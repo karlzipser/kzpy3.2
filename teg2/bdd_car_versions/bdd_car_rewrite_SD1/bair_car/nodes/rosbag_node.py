ff129ba97d9956d84daec480d6dc10e26237e0c1
Karl Zipser <karlzipser@berkeley.edu>
2017-06-23 13:32:39 -0700

    gacp

--- teg2/bdd_car_versions/bdd_car_rewrite_SD1/bair_car/nodes/rosbag_node.py
+++ teg2/bdd_car_versions/bdd_car_rewrite_SD1/bair_car/nodes/rosbag_node.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from kzpy3.utils2 import *
+from kzpy3.utils import *
 import os, sys, shutil, subprocess, time
 import rospy
 import std_msgs.msg
@@ -15,26 +15,21 @@ if __name__ == '__main__':
     save_pub = rospy.Publisher('data_saving', std_msgs.msg.Int32, queue_size=100)
     
 
-    fl = gg('/home/ubuntu/catkin_ws/src/bair_car/rosbags/*')
+    fl = gg(opjh('catkin_ws/src/bair_car/rosbags/*'))
 
     for f in fl:
          os.remove(f)
 
     assert(len(sys.argv) >= 3)
 
-    #bag_rec_folder = opjh('catkin_ws/src/bair_car/rosbags') #sys.argv[1] # '/home/ubuntu/catkin_ws/src/bair_car/rosbags'
-    bag_rec_folder = opj(media_path,'rosbags')#sys.argv[2] # '/media/ubuntu/3131-3031/rosbags'
-    bag_rec_folder = opj(bag_rec_folder,foldername)
+    bag_rec_folder = sys.argv[1] # '/home/ubuntu/catkin_ws/src/bair_car/rosbags'
+    bag_mv_folder = sys.argv[2] # '/media/ubuntu/3131-3031/rosbags'
+    bag_mv_folder = opj(bag_mv_folder,foldername)
 
-    unix('mkdir '+bag_rec_folder)
-    #unix('mkdir  '+opj(bag_mv_folder,'.caf'))
-    #unix('mkdir  '+opj(bag_mv_folder,'.bair_car'))
-
-    #unix('scp -r /home/ubuntu/catkin_ws/src/bair_car ' + opj(bag_mv_folder,'.bair_car'))
-    #unix('scp /home/ubuntu/kzpy3/caf3/z2/z2.caffemodel ' + opj(bag_mv_folder,'.caf'))
+    unix('mkdir '+bag_mv_folder)
     
     assert(os.path.exists(bag_rec_folder))
-    #assert(os.path.exists(bag_mv_folder))
+    assert(os.path.exists(bag_mv_folder))
 
     rate = rospy.Rate(2.0)
 
@@ -47,15 +42,14 @@ if __name__ == '__main__':
                 if '.bag' != os.path.splitext(f)[1]:
                     continue
                 save_pub.publish(std_msgs.msg.Int32(1) )
-                if False:
-                    print('Moving {0}'.format(f))
-                    f_rec = os.path.join(bag_rec_folder, f)
-                    f_mv = os.path.join(bag_mv_folder, f)
-                    # shutil.copy(f_rec, f_mv)
-                    start = time.time()
-                    subprocess.call(['mv', f_rec, f_mv])
+                print('Moving {0}'.format(f))
+                f_rec = os.path.join(bag_rec_folder, f)
+                f_mv = os.path.join(bag_mv_folder, f)
+                # shutil.copy(f_rec, f_mv)
+                start = time.time()
+                subprocess.call(['mv', f_rec, f_mv])
                 elapsed = time.time() - start
-                #unix('rm '+opj(bag_rec_folder,'*.bag')) # 27 Nov 2016, to remove untransferred bags
+                unix('rm '+opj(bag_rec_folder,'*.bag')) # 27 Nov 2016, to remove untransferred bags
                 print('Done in {0} secs\n'.format(elapsed))
                 save_pub.publish(std_msgs.msg.Int32(0))
                 
