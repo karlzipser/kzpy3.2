31b537cacfe7f4fa541a892866beeeec8a4abac1
Karl Zipser <karlzipser@berkeley.edu>
2017-07-05 21:31:06 -0700

    gacp

--- teg2/bdd_car_versions/bdd_car_rewrite_SD1/bair_car/nodes/get_ros_images_live2_z2_color_TX1.py
+++ teg2/bdd_car_versions/bdd_car_rewrite_SD1/bair_car/nodes/get_ros_images_live2_z2_color_TX1.py
@@ -35,7 +35,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from nets.squeezenet import SqueezeNet
-weight_file_path = opjh('pytorch_models','epoch6goodnet') #'save_file.weights')#)
+weight_file_path = '/media/nvidia/rosbags/save_file05Jul17_21h26m01s.weights'#opjh('pytorch_models','epoch6goodnet') #'save_file.weights')#)
 
 def static_vars(**kwargs):
     def decorate(func):
