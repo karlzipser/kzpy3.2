cdf6d71733668b17bfdb7969ebc750ce34b5f19e
Karl Zipser <karlzipser@berkeley.edu>
2017-07-05 20:05:48 -0700

    gacp

--- pytorch2/Train_SqueezeNet/Train.py
+++ pytorch2/Train_SqueezeNet/Train.py
@@ -69,7 +69,7 @@ while True:
             if P.epoch_timer.check():
                 pd2s('\tmode =',mode,'ctr =',DD[mode]['ctr'],dp(100.0*DD[mode]['ctr']/(1.0*len(DD[mode]['all_steer']))),'%')
                 P.epoch_timer.reset()
-                figure('loss');clf()
+                figure('loss');clf();ylim(0,0.02)
                 loss_record['train']['plot']({'c':'b'})
                 loss_record['val']['plot']({'c':'r'})
 
