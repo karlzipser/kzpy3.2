0327849e16cbf6577345191b07dc12658d0c8fa2
Sauhaarda Chowdhuri <sauhaarda@gmail.com>
2017-07-09 16:45:37 -0700

    Initial refactoring of Train.py
    Haven't yet:
    1. Run lint checking
    2. Implemented sorting of loss data
    3. Tested

--- pytorch4/Train_SqueezeNet/Train.py
+++ pytorch4/Train_SqueezeNet/Train.py
@@ -1,8 +1,4 @@
-if '__file__' not in locals():
-    __file__ = 'Train.py'
 from kzpy3.utils2 import *
-cprint('****************** '+__file__+' ******************','yellow')
-pythonpaths(['kzpy3','kzpy3/pytorch3/nets3','kzpy3/pytorch3/Train_SqueezeNet'])
 from vis2 import *
 import torch
 import Data
@@ -10,29 +6,27 @@ import Batch
 import Utils
 import Parameters as P
 
-
-
+# Set Up PyTorch Environment
 torch.set_default_tensor_type('torch.FloatTensor') 
 torch.cuda.set_device(P.GPU)
 torch.cuda.device(P.GPU)
 
-from nets2.SqueezeNet import SqueezeNet
+from SqueezeNet import SqueezeNet
 net = SqueezeNet().cuda()
 criterion = torch.nn.MSELoss().cuda()
 optimizer = torch.optim.Adadelta(net.parameters())
 
-if P.RESUME:
-    cprint(d2s('Resuming with',P.weights_file_path),'yellow')
-    save_data = torch.load(P.weights_file_path)
+if args.resume_path is not None:
+    cprint('Resuming w/ ' + args.resume_path, 'yellow')
+    save_data = torch.load(args.resume_path)
     net.load_state_dict(save_data)
-    time.sleep(4)
-    loss_record_loaded = zload_obj({'path':opjD('loss_record')})
-    loss_record = {}
-    for mode in ['train','val']:
-        loss_record[mode] = Utils.Loss_Record()
-        for k in loss_record_loaded[mode].keys():
-            if not callable(loss_record[mode][k]):
-                loss_record[mode][k] = loss_record_loaded[mode][k]
+    # loss_record_loaded = zload_obj({'path':opjD('loss_record')})
+    # loss_record = {}
+    # for mode in ['train','val']:
+    #     loss_record[mode] = Utils.Loss_Record()
+    #     for k in loss_record_loaded[mode].keys():
+    #         if not callable(loss_record[mode][k]):
+    #             loss_record[mode][k] = loss_record_loaded[mode][k]
 else:
     loss_record = {}
     loss_record['train'] = Utils.Loss_Record()
@@ -40,69 +34,64 @@ else:
 
 rate_counter = Utils.Rate_Counter()
 
-DD=Data.Data()
+data = Data.Data()
 
 timer = {}
 timer['train'] = Timer(60*30)
 timer['val'] = Timer(60*3)
+print_timer = Timer(args.print_time)
+
 trial_loss_record = {}
 
-batch = Batch.Batch({'net':net,'batch_size':P.BATCH_SIZE})
-
+batch = Batch.Batch(net)
 
 while True:
-    for mode in ['train','val']:
-
+    for mode, data_index in [('train', data.train_index), 
+                             ('val', data.val_index)]:
         timer[mode].reset()
-
         while not timer[mode].check():
 
-            batch['fill']({'Data':DD,'mode':mode})
-            
-            batch['forward']({'optimizer':optimizer,'criterion':criterion,'trial_loss_record':trial_loss_record})
+            batch.fill(data, data_index)  # Get batches ready
+            batch.forward(optimizer, criterion, trial_loss_record) # Run net
 
-            if mode == 'train':
-                batch['backward']({'optimizer':optimizer})
+            if mode == 'train':  # Backpropagate
+                batch.backward(optimizer)
 
-            loss_record[mode]['add']({'loss':batch['loss'].data.cpu().numpy()[0]})
+            loss_record[mode].add(batch.loss.data[0])
+            Utils.save_net(net, loss_record)
+            rate_counter.step()
 
-            Utils.save_net({'net':net,'loss_record':loss_record,'weights_folder_path':P.WEIGHTS_FOLDER_PATH})
+            if print_timer.check():
+                batch = Batch.Batch(net)  # Reinitialiize batch
 
-            batch['display']({})
+                print('mode=' + mode)
+                print('ctr=' + str(data.ctr))
+                print('epoch progress=' + str(100 * data_index.ctr /
+                                              len(data_index.all_steer)) + '%')
 
+                batch.display()
 
-            rate_counter['step']({'batch_size':batch['batch_size']})
+                plt.figure('loss')
+                plt.clf()  # clears figure
+                plt.ylim(0.004, 0.015) # range
+                loss_record['train']['plot']('b')  # plot with blue color
+                loss_record['val']['plot']('r')  # plot with red color
+                print_timer.reset()
 
-            batch['clear']()
-
-
-            if P.epoch_timer.check():
-                pd2s('\tmode =',mode,'ctr =',DD[mode]['ctr'],dp(100.0*DD[mode]['ctr']/(1.0*len(DD[mode]['all_steer']))),'%')
-                P.epoch_timer.reset()
-                figure('loss');clf();ylim(0.004,0.015)
-                loss_record['train']['plot']({'c':'b'})
-                loss_record['val']['plot']({'c':'r'})
-
-
-
-
-
-import operator
-sorted_trial_loss_record = sorted(trial_loss_record.items(),key=operator.itemgetter(1))
-
-for i in range(-1,-100,-1):
-    l =  sorted_trial_loss_record[i]
-    run_code,seg_num,offset = sorted_trial_loss_record[i][0][0]
-    t = sorted_trial_loss_record[i][0][1]
-    o = sorted_trial_loss_record[i][0][2]
-    data = DD['get_data']({'run_code':run_code,'seg_num':seg_num,'offset':offset})
-    figure(22);clf();ylim(0,1)
-    plot(t,'r.')
-    plot(o,'g.')
-    plot([0,20],[0.5,0.5],'k')
-    mi(data['right'][0,:,:],23,img_title=d2s(l[1]))
-    pause(1)
-
-
+# import operator
+# sorted_trial_loss_record = sorted(trial_loss_record.items(),key=operator.itemgetter(1))
+# 
+# for i in range(-1,-100,-1):
+#     l =  sorted_trial_loss_record[i]
+#     run_code,seg_num,offset = sorted_trial_loss_record[i][0][0]
+#     t = sorted_trial_loss_record[i][0][1]
+#     o = sorted_trial_loss_record[i][0][2]
+#     data = DD['get_data']({'run_code':run_code,'seg_num':seg_num,'offset':offset})
+#     figure(22);clf();ylim(0,1)
+#     plot(t,'r.')
+#     plot(o,'g.')
+#     plot([0,20],[0.5,0.5],'k')
+#     mi(data['right'][0,:,:],23,img_title=d2s(l[1]))
+#     pause(1)
 
 #EOF
