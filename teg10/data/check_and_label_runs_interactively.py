1ad3317b8895b9b18589ef80b6620373b8f4a0d2
Karl Zipser <karlzipser@berkeley.edu>
2017-07-13 13:17:04 -0700

    gacp

--- teg10/data/check_and_label_runs_interactively.py
+++ teg10/data/check_and_label_runs_interactively.py
@@ -2,14 +2,8 @@
 from kzpy3.misc.progress import *
 from kzpy3.vis2 import *
 
-#bair_car_data_path = opjD('bair_car_data_new')
-#bair_car_data_path = '/media/karlzipser/ExtraDrive4/bair_car_data_new_28April2017'
-#bair_car_data_path = opjD('bair_car_data_Main_Dataset')
-#bair_car_data_path = '/Volumes/SSD_2TB/bair_car_data_new_28April2017'
-bair_car_data_path = '/media/karlzipser/ExtraDrive2/bdd_car_data_July2017_path_dataset'
 
-
-doc_str = """
+doc_string = """
 Interactive data viewer and labeler for model car project.
  
 
@@ -70,6 +64,41 @@ These runs need to be processed correctly:
 """
 
 
+Names = ['name','dic','argument_dictionary','translation_dic','accepted_states','bair_car_data_path']
+for l in Names:
+	exec(d2n(l,'=',"'",l,"'"))
+Values = {}
+V = Values
+
+V[translation_dic] = {'src':bair_car_data_path}
+if __name__ == "__main__" and '__file__' in vars():
+    V[argument_dictionary] = args_to_dic({'pargs':sys.argv[1:]})
+else:
+    print('Running this within interactive python.')
+    V[argument_dictionary] = args_to_dic({
+    	'pargs':"-src /media/karlzipser/ExtraDrive2/bdd_car_data_July2017_path_dataset"})
+V[argument_dictionary] = translate_args(
+    {argument_dictionary:V[argument_dictionary],
+    translation_dic:V[translation_dic]})
+if len(V[argument_dictionary]) == 0:
+	pd2s(doc_string)
+	nice_print_dic({dic:V[translation_dic],name:'arguments'})
+	exit()
+
+
+nice_print_dic( dic,V[argument_dictionary],  name,argument_dictionary )
+
+
+#V['bair_car_data_path'] = opjD('bair_car_data_new')
+#V['bair_car_data_path'] = '/media/karlzipser/ExtraDrive4/bair_car_data_new_28April2017'
+#V['bair_car_data_path'] = opjD('bair_car_data_Main_Dataset')
+#V['bair_car_data_path'] = '/Volumes/SSD_2TB/bair_car_data_new_28April2017'
+V[bair_car_data_path] = V[argument_dictionary][bair_car_data_path]
+
+
+
+
+
 
 
 
@@ -119,7 +148,7 @@ def function_help():
 			HE
 			get help.
 	"""
-	cprint(doc_str,'y')
+	cprint(doc_string,'yellow')
 	cprint('INTERACTIVE FUNCTIONS:')
 	for f in i_functions:
 		exec('print('+f+'.__doc__)')
@@ -135,11 +164,12 @@ HE() # do this here so that we are reminded of this feature
 
 
 
-def function_set_paths(p=opj(bair_car_data_path)):
+def function_set_paths(p=opj(V[bair_car_data_path])):
 	"""
-	function_set_paths(p=opj(bair_car_data_path))
+	function_set_paths(p=opj(V['bair_car_data_path']))
 		SP
 	"""
+	V[bair_car_data_path] = p
 	global I
 	I[meta_path] = opj(p,'meta')
 	I[rgb_1to4_path] = opj(p,'rgb_1to4')
@@ -206,7 +236,7 @@ def function_list_runs(rng=None,auto_direct_labelling=False):
 	"""
 	cprint(I[meta_path])
 	try:
-		run_labels_path = most_recent_file_in_folder(opj(bair_car_data_path,'run_labels'),['run_labels'])
+		run_labels_path = most_recent_file_in_folder(opj(V['bair_car_data_path'],'run_labels'),['run_labels'])
 		I[run_labels] = load_obj(run_labels_path)
 	except:
 		cprint('Unable to load run_labels!!!!! Initalizing to empty dict')
@@ -262,7 +292,7 @@ def function_set_label(k,v=True):
 		k = [k]
 	for m in k:
 		I[run_labels][I[run_]][m] = v
-	save_obj(I[run_labels],opj(bair_car_data_path,'run_labels','run_labels_'+time_str()+'.pkl'))
+	save_obj(I[run_labels],opj(V['bair_car_data_path'],'run_labels','run_labels_'+time_str()+'.pkl'))
 SL = function_set_label
 
 
@@ -385,7 +415,7 @@ def load_images(bag_file_path,color_mode="rgb8",include_flip=True):
 
 
 
-def function_visualize_run(j=None,do_load_images=False,do_CA=False):
+def function_visualize_run(j=None,img_load=False,do_CA=False):
 	"""
 	function_visualize_run()
 		VR
@@ -460,7 +490,7 @@ def function_visualize_run(j=None,do_load_images=False,do_CA=False):
 	plt.ylim(0,0.001*bins[0].max())
 	plt.pause(0.01)
 
-	if do_load_images:
+	if img_load:
 		left_images_ = {}
 		right_images_ = {}
 		steer_ = {}
@@ -519,7 +549,8 @@ def function_visualize_run(j=None,do_load_images=False,do_CA=False):
 		pause(0.01)
 VR = function_visualize_run
 
-
+def VRi(j=None,img_load=True,do_CA=False):
+	VR(j,img_load,do_CA)
 
 
 def function_animate(t0,t1):
@@ -579,309 +610,28 @@ def function_run_loop():
 		RL
 	"""
 	print('')
+	SR(0)
 	while True:
-		try:
-			CR()
+		if True:#try:
+			#
+			#CR()
 			command = raw_input(I[run_] + ' >> ')
 			if command in ['q','quit','outta-here!']:
 				break
 			eval(command)
-		except Exception as e:
+		else:#except Exception as e:
 			cprint("********** Exception ***********************",'red')
 			print(e.message, e.args)
 
 
 
 
-RL = function_run_loop
-
-if __name__ == '__main__':
-	pass #RL()
 
 
+if __name__ == '__main__' and '__file__' in locals():
+	function_run_loop()
 
 
-def function_save_hdf5(run_num=None,dst_path=opj(bair_car_data_path,'hdf5/runs'),flip=False):
-	if run_num != None:
-		CA()
-		SR(run_num)
-		VR()
-	min_seg_len = 30
-	seg_lens = []
-	B = I[B_]
-	L = B['left_image_bound_to_data']
-	sos=B['data']['state_one_steps']
-
-	segment_list = []
-
-	in_segment = False
-
-	for i in range(len(sos)):
-		t = B['data']['raw_timestamps'][i]
-		if sos[i] > 0 and t in I[left_images] and t in I[right_images]:
-			if not in_segment:
-				in_segment = True
-				segment_list.append([])
-			segment_list[-1].append(B['data']['raw_timestamps'][i])
-		else:
-			in_segment = False
-
-
-	segment_list_with_min_len = []
-	for s in segment_list:
-		if len(s) >= min_seg_len:
-			segment_list_with_min_len.append(s)
-
-	for s in segment_list_with_min_len:
-		seg_lens.append(len(s))
-
-	
-	if not flip:
-		rn = opj(I[run_])
-	else:
-		rn = opj('flip_'+I[run_])
-
-	unix('mkdir -p '+dst_path)
-
-	F = h5py.File(opjD(dst_path,rn+'.hdf5'))
-
-	glabels = F.create_group('labels')
-	gsegments = F.create_group('segments')
-
-	if flip:
-		glabels['flip'] = np.array([1])
-	else:
-		glabels['flip'] = np.array([0])	
-	for l in i_labels:
-		if l in I[run_labels][I[run_]]:
-			if I[run_labels][I[run_]][l]:
-				glabels[l] = np.array([1])
-			else:
-				glabels[l] = np.array([0])
-		else:
-			glabels[l] = np.array([0])
-	for i in range(len(segment_list_with_min_len)):
-		segment = segment_list_with_min_len[i]
-		left_image_list = []
-		right_image_list = []
-		steer_list = []
-		motor_list = []
-		state_list = []
-		for j in range(len(segment)):
-			t = segment[j]
-			limg = I[left_images][t]
-			rimg = I[right_images][t]
-			st = I[steer][t]
-			if flip:
-				st -= 49
-				st *= -1.0
-				st += 49
-				left_image_list.append(scipy.fliplr(rimg))
-				right_image_list.append(scipy.fliplr(limg))
-			else:
-				left_image_list.append(limg)
-				right_image_list.append(rimg)
-			steer_list.append(st)
-			motor_list.append(I[motor][t])
-			state_list.append(I[state][t])
-		gsegments[opj(str(i),'left_timestamp')] = segment
-		gsegments[opj(str(i),'left')] = np.array(left_image_list)
-		gsegments[opj(str(i),'right')] = np.array(right_image_list)
-		gsegments[opj(str(i),'steer')] = np.array(steer_list)
-		gsegments[opj(str(i),'motor')] = np.array(motor_list)
-		gsegments[opj(str(i),'state')] = np.array(state_list)
-	F.close()
-S5 = function_save_hdf5
-
-
-
-
-
-
-def mi_or_cv2(img,cv=True,delay=30,title='animate'):
-	if cv:
-		cv2.imshow(title,cv2.cvtColor(img,cv2.COLOR_RGB2BGR))
-		if cv2.waitKey(delay) & 0xFF == ord('q'):
-			pass
-	else:
-		mi(img,title)
-		pause(0.0001)
-
-
-
-
-
-def function_load_hdf5(path):
-	F = h5py.File(path)
-	Lb = F['labels']
-	S = F['segments']
-	return Lb,S
-
-
-
-
-def start_at(t):
-	while time.time() < t:
-		time.sleep(0.1)
-		print t-time.time()
-
-def load_animate_hdf5(path,start_at_time=0):
-	start_at(start_at_time)
-	l,s=function_load_hdf5(path)
-	img = False
-	for h in range(len(s)):
-		if type(img) != bool:
-			img *= 0
-			img += 128
-			mi_or_cv2(img)
-		pause(0.5)
-		n = str(h)
-		for i in range(len(s[n]['left'])):
-			img = s[n]['left'][i]
-			#print s[n][state][i]
-			bar_color = [0,0,0]
-			
-			if s[n][state][i] == 1:
-				bar_color = [0,0,255]
-			elif s[n][state][i] == 6:
-				bar_color = [255,0,0]
-			elif s[n][state][i] == 5:
-				bar_color = [255,255,0]
-			elif s[n][state][i] == 7:
-				bar_color = [255,0,255]
-			else:
-				bar_color = [0,0,0]
-			if i < 2:
-				smooth_steer = s[n][steer][i]
-			else:
-				smooth_steer = (s[n][steer][i] + 0.5*s[n][steer][i-1] + 0.25*s[n][steer][i-2])/1.75
-			#print smooth_steer
-			apply_rect_to_img(img,smooth_steer,0,99,bar_color,bar_color,0.9,0.1,center=True,reverse=True,horizontal=True)
-			apply_rect_to_img(img,s[n][motor][i],0,99,bar_color,bar_color,0.9,0.1,center=True,reverse=True,horizontal=False)
-			mi_or_cv2(img)
-A5 = load_animate_hdf5
-
-
-# filter out left and out in files
-
-def load_hdf5_steer_hist(path,dst_path):
-	if len(gg(opj(dst_path,fname(path).replace('hdf5','state_hist_list.pkl')))) == 1:
-		print(opj(dst_path,fname(path).replace('hdf5','state_hist_list.pkl'))+' exists')
-		return
-	try:
-		print path
-		unix('mkdir -p '+dst_path)
-		low_steer = []
-		high_steer = []
-		l,s=function_load_hdf5(path)
-		pb = ProgressBar(len(s))
-		state_hist_list = []
-		for h in range(len(s)):
-			pb.animate(h)
-			state_hist = np.zeros(8)
-			n = str(h)
-			for i in range(len(s[n][left])):
-				state_hist[int(s[n][state][i])] += 1
-				if i < 2:
-					smooth_steer = s[n][steer][i]
-				else:
-					smooth_steer = (s[n][steer][i] + 0.5*s[n][steer][i-1] + 0.25*s[n][steer][i-2])/1.75
-				if smooth_steer < 43 or smooth_steer > 55:
-					high_steer.append([h,i,int(round(smooth_steer))])
-				else:
-					low_steer.append([h,i,int(round(smooth_steer))])
-			state_hist_list.append(state_hist)
-		pb.animate(h)
-		assert(len(high_steer)>0)
-		assert(len(low_steer)>0)
-
-		save_obj(high_steer,opj(dst_path,fname(path).replace('hdf5','high_steer.pkl')))
-		save_obj(low_steer,opj(dst_path,fname(path).replace('hdf5','low_steer.pkl')))
-		save_obj(state_hist_list,opj(dst_path,fname(path).replace('hdf5','state_hist_list.pkl')))
-	except Exception as e:
-		cprint("********** load_hdf5_steer_hist Exception ***********************",'red')
-		print(e.message, e.args)
-
-
-
-
-
-
-if False:
-	if False:
-		CS_("Goal: create normal and flip hdf5 segements.")
-		for i in range(len(I[runs])):
-			try:
-				VR(i,do_load_images=False)
-				r = I[runs][i]
-				ks = sorted(I[run_labels][r])
-				labeled = False
-				for k in ks:
-					if I[run_labels][r][k]:
-						labeled = True
-				if labeled and I[run_labels][I[runs][i]][reject_run] == False:
-					cprint(d2s(i,') accept',r),'yellow')
-					S5(i,flip=False)
-					S5(flip=True)
-				else:
-					cprint(d2s(i,') reject',r),'red')
-
-			except Exception as e:
-				print("********** Exception ***********************")
-				print(e.message, e.args)
-				cprint('Failure in for i in range(len(I[runs])): loop','blue')
-
-
-	if False:
-		CS_("Goal: create steer hist lists.")
-		hdf5s = sgg(opj(bair_car_data_path,'hdf5/runs/*.hdf5'))
-		ctr = 0
-		for h in hdf5s:
-			ctr += 1
-			print ctr
-			load_hdf5_steer_hist(h,opj(bair_car_data_path,'hdf5','segment_metadata'))
-
-
-
-	if False:
-
-		if False:
-			CS_("Goal: compile run codes.")
-			run_codes = {}
-			steer_hists = sgg(opj(bair_car_data_path,'hdf5/segment_metadata/*.state_hist_list.pkl'))
-			ctr = 0
-			combined = []
-			for s in steer_hists:
-				o = load_obj(s)
-				run_codes[ctr] = fname(s).replace('.state_hist_list.pkl','')
-				print ctr,run_codes[ctr]
-				#for j in range(len(o)):
-				#	o[j][3] = ctr
-				#	combined.append(o[j])
-				ctr += 1
-			#save_obj(combined,opjD('combined'))
-			save_obj(run_codes,opj(bair_car_data_path,'hdf5/segment_metadata/run_codes'))
-
-
-		if False:
-			CS_("Goal: compile low_steer and high_steer.")
-			low_steer = []
-			high_steer = []
-			low_steer_files = sgg(opj(bair_car_data_path,'hdf5/segment_metadata/*.low_steer.pkl'))
-			ctr = 0
-			for s in low_steer_files:
-				print (ctr,s)
-				q = load_obj(s)
-				for i in range(len(q)):
-					q[i].append(ctr)
-				low_steer += q
-				q = load_obj(s.replace('.low_steer.','.high_steer.'))
-				for i in range(len(q)):
-					q[i].append(ctr)
-				high_steer += q
-				ctr += 1
-			save_obj(low_steer,opj(bair_car_data_path,'hdf5/segment_metadata/low_steer'))
-			save_obj(high_steer,opj(bair_car_data_path,'hdf5/segment_metadata/high_steer'))
 
 
 
