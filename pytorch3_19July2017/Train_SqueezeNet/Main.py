0ae999164f4bd872423dd672404b9847961d4fc3
Karl Zipser <karlzipser@berkeley.edu>
2017-07-20 13:16:50 -0700

    gacp

---
+++ pytorch3_19July2017/Train_SqueezeNet/Main.py
@@ -0,0 +1,55 @@
+###############################
+#  for interactive terminal
+import __main__ as main
+if not hasattr(main,'__file__'):
+	from kzpy3.utils2 import *
+	pythonpaths(['kzpy3','kzpy3/pytorch3/Train_SqueezeNet','kzpy3/teg9'])
+#
+###############################
+from Parameters_Module import *
+import Data_Module
+import Batch_Module
+import Network_Module
+exec(identify_file_str)
+
+_ = dictionary_access
+
+for a in Args.keys():
+    _(P,a,equals,_(Args,a)) #P[a] = Args[a]
+
+
+
+trial_loss_record = {} # get this into Pytorch_Network
+
+
+
+
+
+
+
+Network = Network_Module.Pytorch_Network()
+
+Training_data = Data_Module.Training_Data()
+
+Batch = Batch_Module.Batch(net,Network[net], batch_size,P[BATCH_SIZE])
+
+timer = Timer(0)
+
+while True:
+	timer.reset()
+	Batch[clear]()
+	Batch[fill]('Data',Training_data, mode,train)
+	Batch[forward]({'optimizer':Network[optimizer],'criterion':Network[criterion],'trial_loss_record':trial_loss_record})
+	Batch[display]({'print_now':True})
+	Batch[backward]({'optimizer':Network[optimizer]})
+	print timer.time()
+	
+
+	#Batch['clear']()
+
+
+
+
+
+
+#EOF
---
+++ pytorch3_19July2017/Train_SqueezeNet/Network_Module.py
@@ -0,0 +1,30 @@
+from Parameters_Module import *
+import torch
+from SqueezeNet import SqueezeNet
+exec(identify_file_str)
+
+
+_ = dictionary_access
+
+
+torch.set_default_tensor_type('torch.FloatTensor') 
+torch.cuda.set_device(_(P,GPU))
+torch.cuda.device(_(P,GPU))
+
+
+
+
+def Pytorch_Network():
+    D = {}
+    True
+    _(D,dic_type,equals,'Pytorch_Network')
+    _(D,purpose,equals,d2s(inspect.stack()[0][3],':','Object network.'))
+    _(D,net,equals,SqueezeNet().cuda())
+    _(D,criterion,equals,torch.nn.MSELoss().cuda())
+    _(D,optimizer,equals,torch.optim.Adadelta(D[net].parameters()))
+
+
+    return D
+
+#
+#EOF
