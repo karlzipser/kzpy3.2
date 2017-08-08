9c9bc71a47ea7984c7a2ea6a77926a4591d63d4f
Karl Zipser <karlzipser@berkeley.edu>
2017-08-08 09:48:51 -0700

    gacp

--- Cars/robot_car_6Aug2017/nodes/ard_MSE.py
+++ Cars/robot_car_6Aug2017/nodes/ard_MSE.py
@@ -348,7 +348,7 @@ def run_loop(Arduinos,M,BUTTON_DELTA=50,):
 			M['potential_collision'] = 0
 
 			#print M['caffe_motor']
-			print(dp(M['caffe_motor']), dp(M['motor_freeze_threshold']),dp(np.array(M['encoder_lst'][0:20]).mean()),dp(np.array(M['encoder_lst'][-20:]).mean()))
+			#print(dp(M['caffe_motor']), dp(M['motor_freeze_threshold']),dp(np.array(M['encoder_lst'][0:20]).mean()),dp(np.array(M['encoder_lst'][-20:]).mean()))
 			if M['caffe_motor'] > M['motor_freeze_threshold'] and np.array(M['encoder_lst'][0:20]).mean() > 1 and np.array(M['encoder_lst'][-20:]).mean()<0.1 and M['current_state'].state_transition_timer.time() > 1:
 				print("caffe_motor freeze")
 				M['potential_collision'] = 1
