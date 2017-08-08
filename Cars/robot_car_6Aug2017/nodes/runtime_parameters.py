3e4ac0a661ba7cdaf6b26fbb060c6fbf534fe62e
Karl Zipser <karlzipser@berkeley.edu>
2017-08-08 08:24:20 -0700

    gacp

--- Cars/robot_car_6Aug2017/nodes/runtime_parameters.py
+++ Cars/robot_car_6Aug2017/nodes/runtime_parameters.py
@@ -39,6 +39,7 @@ use_caffe = True
 NETWORK = 111
 I_ROBOT = 222
 who_is_in_charge = I_ROBOT
+
 robot_steer = 49
 robot_motor = 49
 
@@ -64,7 +65,7 @@ acc_freeze_threshold_z = 2
 motor_freeze_threshold = 55
 n_avg_IMU = 10
 
-robot_acc2rd_threshold = 9
+robot_acc2rd_threshold = 8
 robot_acc_y_exit_threshold = 0
 robot_steer =  -999 # negative value means not used
 robot_motor = 58
