1fef94d9313adb4988b7b189039f6633e6b673c3
Karl Zipser <karlzipser@berkeley.edu>
2017-08-07 19:08:41 -0700

    gacp

--- teg2/bdd_car_versions/bdd_car_6Aug2017/bair_car/nodes/network_node.py
+++ teg2/bdd_car_versions/bdd_car_6Aug2017/bair_car/nodes/network_node.py
@@ -108,7 +108,7 @@ while not rospy.is_shutdown():
 			if len(left_list) > nframes + 2:
 
 				if  not network_ignore_potential_collision.check() or ((potential_collision_ == 0) and (not frozen_)): #or network_ignore_potential_collision.check():
-
+					assert(network_ignore_potential_collision.check()==False)
 					camera_data = format_camera_data(left_list, right_list)
 					metadata = format_metadata((rp.Racing, 0, rp.Follow, rp.Direct, rp.Play, rp.Furtive))
 
