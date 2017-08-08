40b46dce2584fe9a7a21822c6b329d826102cb17
Karl Zipser <karlzipser@berkeley.edu>
2017-08-07 19:21:35 -0700

    gacp

--- teg2/bdd_car_versions/bdd_car_6Aug2017/bair_car/nodes/network_node.py
+++ teg2/bdd_car_versions/bdd_car_6Aug2017/bair_car/nodes/network_node.py
@@ -99,6 +99,7 @@ while not rospy.is_shutdown():
 			network_ignore_potential_collision.reset()
 			frozen_ = False
 		if not network_enter_timer.check() or potential_collision_ == 2:
+			frozen_ = False
 			print "waiting before entering network mode..."
 			steer_cmd_pub.publish(std_msgs.msg.Int32(49))
 			motor_cmd_pub.publish(std_msgs.msg.Int32(49))
