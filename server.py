19d883976399c114f93542b69b3afa955a453691
Karl Zipser <karlzipser@berkeley.edu>
2017-08-16 12:04:17 -0700

    gacp

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
+"""
 import socket
 
 
-TCP_IP = '127.0.0.1'
+TCP_IP = '192.168.1.10'
 TCP_PORT = 5005
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
@@ -19,3 +20,38 @@ while 1:
     print "received data:", data
     conn.send(data)  # echo
 conn.close()
+"""
+
+'''
+    Simple socket server using threads
+'''
+ 
+import socket
+import sys
+ 
+HOST = ''   # Symbolic name, meaning all available interfaces
+PORT = 8888 # Arbitrary non-privileged port
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+ 
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+ 
+#Start listening on socket
+s.listen(10)
+print 'Socket now listening'
+ 
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+     
+s.close()
