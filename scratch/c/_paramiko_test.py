eeee943b61a13f7af98eab7a61c6cb67e76136b6
Karl Zipser <karlzipser@berkeley.edu>
2017-08-17 15:16:26 -0700

    gacp

--- scratch/c/_paramiko_test.py
+++ scratch/c/_paramiko_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 from kzpy3.utils2 import *
 Car_ips = {'Mr_Black':'192.168.1.20','Mr_Silver':'192.168.1.10'}
 this_car = os.environ["COMPUTER_NAME"]
@@ -13,7 +13,7 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(Car_ips[other_car], username='nvidia',password='nvidia')
 timer = Timer(0)
 for i in range(1000):
-	ssh.exec_command(d2n("echo '(1.4,5.2,",i,")' > ~/Desktop/",other_car,".txt "))
+	ssh.exec_command(d2n("echo '(1.4,5.2,",i,")' > ~/Desktop/",this_car,".txt "))
 	print(i,dp(i/timer.time()))
 
 
