10202bad1c5700cb2c92055eb210293cbaf7ee0f
Karl Zipser <karlzipser@berkeley.edu>
2017-08-17 18:20:37 -0700

    gacp

--- scratch/c/_plot_aruco_paramiko.py
+++ scratch/c/_plot_aruco_paramiko.py
@@ -4,11 +4,11 @@ from kzpy3.vis2 import *
 #clf();plt_square();xysqlim(2.1);
 img = zeros((440,440,3),np.uint8)
 while True:
-	if True:#try:
+	try:
 		l = txt_file_to_list_of_strings(opjD('Mr_Black.txt'))
 		exec('pose = '+l[0])
 		if len(pose) == 4:
 			img[210+int(100*pose[0]),210+int(100*pose[1]),0]=255;
 		mci(img,delay=5)
-	else:#except:
+	except:
 		print 'oops'
