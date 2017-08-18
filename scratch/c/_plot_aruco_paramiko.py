171de31e32413e05eb120147ba68389772f5d1c5
Karl Zipser <karlzipser@berkeley.edu>
2017-08-17 18:11:59 -0700

    gacp

--- scratch/c/_plot_aruco_paramiko.py
+++ scratch/c/_plot_aruco_paramiko.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 from kzpy3.vis2 import *
 
-
+clf();plt_square();xysqlim(2.1);
 
 while True:
-	l = txt_file_to_list_of_strings(opjD('Mr_Black.txt'))
-	exec('pose = '+l[0])
-	clf();plt_square();xysqlim(2.1);plot(pose[0],pose[1],'ro');pause(0.01)
+	try:
+		l = txt_file_to_list_of_strings(opjD('Mr_Black.txt'))
+		exec('pose = '+l[0])
+		plot(pose[0],pose[1],'ro');spause()
+	except:
+		print 'oops'
