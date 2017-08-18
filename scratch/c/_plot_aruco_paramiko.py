62e541c58f5e413b1f126e538765beb171a59a93
Karl Zipser <karlzipser@berkeley.edu>
2017-08-17 18:18:01 -0700

    gacp

--- scratch/c/_plot_aruco_paramiko.py
+++ scratch/c/_plot_aruco_paramiko.py
@@ -7,7 +7,7 @@ while True:
 	if True:#try:
 		l = txt_file_to_list_of_strings(opjD('Mr_Black.txt'))
 		exec('pose = '+l[0])
-		img[pose[0],pose[1],0]=255;
+		img[int(10*pose[0]),int(10*pose[1]),0]=255;
 		mci(img,delay=5)
 	else:#except:
 		print 'oops'
