202136bc85c9ab975895981efd4edfe0ddca9d9f
Karl Zipser <karlzipser@berkeley.edu>
2017-08-19 13:00:48 -0700

    gacp

--- scratch/c/_plot_aruco_paramiko.py
+++ scratch/c/_plot_aruco_paramiko.py
@@ -30,6 +30,7 @@ while not done:
 				Gi[ptsplot](x,[pose[0]],y,[pose[1]],color,(255,0,0))
 				Gi[ptsplot](x,[pose[0]+pose[2]],y,[pose[1]+pose[3]],color,(0,255,0))
 				for xxyy in xy:
+					print xxyy
 					Gi[img][xxyy[0],[xxyy[1]],2] = 255 
 				heading_floats = np.array(heading_floats)
 				Gi[ptsplot]( x,heading_floats[:,0], y,heading_floats[:,1], color,(255,255,255), NO_REVERSE,False)
