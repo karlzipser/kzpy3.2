7a5ef4225b1424af0f7e022e8ebff80a688da606
Karl Zipser <karlzipser@berkeley.edu>
2017-08-19 17:02:54 -0700

    gacp

--- Cars/robot_car_6Aug2017_temp/nodes/network_node.py
+++ Cars/robot_car_6Aug2017_temp/nodes/network_node.py
@@ -113,7 +113,7 @@ hw = half_wall_length
 tmp = Graph_Image(xmin,-hw, xmax,hw, ymin,-hw, ymax,hw, xsize,25, ysize,25)
 #tmp[img] = cv2.blur(lo(opjD('Potential_graph_img')),(rp.potential_graph_blur,rp.potential_graph_blur))
 tmp[img] = lo(opjD('Potential_graph_img'))
-tmp[img][19:22,10:15] = 1.0
+tmp[img][18:21,10:15] = 1.0
 tmp[img] = cv2.blur(tmp[img],(rp.potential_graph_blur,rp.potential_graph_blur))
 
 Potential_graph = Graph_Image(xmin,-hw, xmax,hw, ymin,-hw, ymax,hw, xsize,25, ysize,25, Img,tmp)
