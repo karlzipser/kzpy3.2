2b5db780d75b4fdf5a303ad6d2511eecb7b439cd
Karl Zipser <karlzipser@berkeley.edu>
2017-08-20 18:36:45 -0700

    gacp

--- Cars/robot_car_6Aug2017_temp/nodes/ard_MSE.py
+++ Cars/robot_car_6Aug2017_temp/nodes/ard_MSE.py
@@ -349,7 +349,7 @@ def run_loop(Arduinos,M,BUTTON_DELTA=50,):
 
 			en = int(M['n_lst_steps']/4)
 
-			print 'temp encoder print',np.array(M['encoder_lst'][0:en]).mean(),np.array(M['encoder_lst'][-en:]).mean(),M['caffe_motor']
+			#print 'temp encoder print',np.array(M['encoder_lst'][0:en]).mean(),np.array(M['encoder_lst'][-en:]).mean(),M['caffe_motor']
 			#print M['caffe_motor']
 			#print(dp(M['caffe_motor']), dp(M['motor_freeze_threshold']),dp(np.array(M['encoder_lst'][0:20]).mean()),dp(np.array(M['encoder_lst'][-20:]).mean()))
 #			if M['caffe_motor'] > M['motor_freeze_threshold'] and np.array(M['encoder_lst'][0:en]).mean() > 0.5 and np.array(M['encoder_lst'][-en:]).mean()<0.1 and M['current_state'].state_transition_timer.time() > 1:
