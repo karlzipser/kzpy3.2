ba118c0d5c26fb405052a753082c77386182ad1d
Karl Zipser <karlzipser@hiMac.lan>
2017-08-21 15:17:10 -0700

    gacp

--- Localization_app/Parameters_Module.py
+++ Localization_app/Parameters_Module.py
@@ -5,7 +5,7 @@ exec(identify_file_str)
 _ = dictionary_access
 
 #from aruco_home_4x4_markers import Marker_xy_dic
-from arco_whole_room_markers import Marker_xy_dic
+from aruco_whole_room_markers import Marker_xy_dic
 P = {}
 P[VERBOSE] = True
 P[GRAPHICS] = False
