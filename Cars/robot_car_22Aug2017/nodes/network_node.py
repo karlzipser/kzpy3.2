4ef465df266e6212557c34395672e2b78dd260b5
Karl Zipser <karlzipser@hiMac.lan>
2017-08-23 08:42:20 -0700

    gacp

--- Cars/robot_car_22Aug2017/nodes/network_node.py
+++ Cars/robot_car_22Aug2017/nodes/network_node.py
@@ -175,6 +175,7 @@ def get_best_heading(x_pos,y_pos,heading,radius):
 			min_potential = p
 			min_potential_index = i
 		if not heading_pause:
+			print headings
 			if np.abs(headings[i]) < rp.heading_pause_threshold:
 				print('A')
 				if p > rp.heading_float_pause_threshold:
