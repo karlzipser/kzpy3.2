971287821bb1a837e2e8f19d905c7878442fdafb
Karl Zipser <karlzipser@Karls-MacBook.local>
2017-08-24 13:21:59 -0700

    gacp

--- Cars/robot_car_23Aug2017/nodes/runtime_parameters.py
+++ Cars/robot_car_23Aug2017/nodes/runtime_parameters.py
@@ -20,12 +20,12 @@ except KeyError:
    """
 
 
-Car_IP_dic = {Mr_Blue:'192.168.1.101',
-	Mr_Black:'192.168.1.102',
-	Mr_Orange_Blue:'192.168.1.103',
-	Mr_Yellow_Blue:'192.168.1.104',
-	Mr_Lt_Blue:'192.168.1.105',
-	Mr_Purple:'.168.1.106'}
+Car_IP_dic = {'Mr_Blue':'192.168.1.101',
+	'Mr_Black':'192.168.1.102',
+	'Mr_Orange_Blue':'192.168.1.103',
+	'Mr_Yellow_Blue':'192.168.1.104',
+	'Mr_Lt_Blue':'192.168.1.105',
+	'Mr_Purple':'.168.1.106'}
 ####################### general car settings ################
 #
 for i in range(1):
