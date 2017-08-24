5d5a82475669f3d79c359b649c0d9656394dff26
Karl Zipser <karlzipser@Karls-MacBook.lan>
2017-08-24 14:37:30 -0700

    gacp

--- scratch/c/_plot_aruco_paramiko2.py
+++ scratch/c/_plot_aruco_paramiko2.py
@@ -25,7 +25,9 @@ Gi = Graph_Image(xmin,x_min,
 for i in range(3):
 	Gi[img][:,:,i] = potential_image.copy()
 
-Colors = {'Mr_Black':(0,0,0),'Mr_Blue':(0,0,255),'Mr_Lt_Blue':(100,230,255)}
+mi(potential_image,'potential_image')
+
+Colors = {'Mr_Black':(0,0,0),'Mr_Blue':(0,0,255),'Mr_Lt_Blue':(50,200,255)}
 
 done = False
 while not done:
