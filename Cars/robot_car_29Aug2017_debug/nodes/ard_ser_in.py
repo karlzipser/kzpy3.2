44471e08d46d99f4a39de2e9b111b6da8a9df067
Karl Zipser <karlzipser@hiMac.local>
2017-08-29 08:36:19 -0700

    gacp

---
+++ Cars/robot_car_29Aug2017_debug/nodes/ard_ser_in.py
@@ -0,0 +1,57 @@
+from kzpy3.utils2 import *
+
+import runtime_parameters as rp
+# original
+
+def get_arduino_serial_connections(baudrate, timeout):
+    sers = []
+    ACM_ports = [os.path.join('/dev', p) for p in os.listdir('/dev') if 'ttyACM' in p]
+    for ACM_port in ACM_ports:
+        try:
+            sers.append(serial.Serial(ACM_port, baudrate=baudrate, timeout=timeout))
+            print('Opened {0}'.format(ACM_port))
+        except:
+            pass
+    return sers
+
+def assign_serial_connections(sers):
+    spd2s('Looking for Arduinos . . .')
+    Arduinos = {}
+    ser_timer = Timer(15)
+    while not ser_timer.check() or not len(sers) == 0:
+        for ser in sers:
+            try:
+                ser_str = ser.readline()
+                exec('ser_tuple = list({0})'.format(ser_str))
+                if ser_tuple[0] in ['mse']:
+                    spd2s('Port',ser.port,'is the MSE:',ser_str,ser_timer.time())
+                    Arduinos['MSE'] = ser
+                    sers.remove(ser)
+                    break
+                elif ser_tuple[0] in ['acc','gyro','head']:
+                    spd2s('Port',ser.port,'is the IMU:',ser_str,ser_timer.time())
+                    Arduinos['IMU'] = ser
+                    sers.remove(ser)
+                    break
+                elif ser_tuple[0] in ['GPS2']:
+                    spd2s('Port',ser.port,'is the SIG:',ser_str,ser_timer.time())
+                    Arduinos['SIG'] = ser
+                    sers.remove(ser)
+                    break
+            except:
+                pass
+    spd2s('Done looking for Arduinos.')
+    if rp.require_Arudinos_MSE:
+        if 'MSE' not in Arduinos:
+            srpd2s('Fatal error\nArduino MSE not found!',
+                #'\nUnable to identify port {0}'.format(ser.port),
+                '\nIs transmitter turned on?',
+                '\nIs MSE battery plugged in?')
+            stop_ros()
+    else:
+        spd2s('Using dummy Arduino MSE.')
+        Arduinos['MSE'] = 'Dummy MSE'
+
+    return Arduinos
+
+
