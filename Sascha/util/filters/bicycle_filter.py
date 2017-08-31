9339ba34c7b69e293f9eabcd35d8495c6969ed2b
Sascha Hornauer <sascha.hornauer@uni-oldenburg.de>
2017-08-30 21:24:53 -0700

    ..

--- Sascha/util/filters/bicycle_filter.py
+++ Sascha/util/filters/bicycle_filter.py
@@ -9,24 +9,27 @@ import os
 import sys
 from os.path import expanduser
 import time
-from pykalman import KalmanFilter
+from kzpy3.Sascha.util.filters.third_party.pykalman.pykalman import KalmanFilter
 import pylab as pl
 
 class Simple_Kalman_Estimator():
 
-    number_of_observations = 40
-    #kf = KalmanFilter(initial_state_mean=0, n_dim_obs=2)
+    number_of_observations = 5
+    
     kf = KalmanFilter(transition_matrices=np.array([[1, 1], [0, 1]]),
                   transition_covariance=0.01 * np.eye(2))
     observations = []
 
-    def get_xy_position(self,position_xy,heading,steering_angle):
+    def get_xy_position(self, position_xy, heading, steering_angle):
         
-        self.observations = np.append(self.observations,position_xy)
+        self.observations = np.append(self.observations, position_xy)
         
         if len(self.observations) == 1:
-            self.observations = np.append(self.observations,position_xy)
-
+            self.observations = np.append(self.observations, position_xy)
+            
+        if len(self.observations) > self.number_of_observations:
+            self.observations = np.delete(self.observations,0)
+            
         states_pred = self.kf.em(self.observations).smooth(self.observations)[0]
         
         return states_pred[:, 0]
@@ -35,38 +38,36 @@ if __name__ == '__main__':
     
     filter = Simple_Kalman_Estimator()
     
-    rnd = np.random.RandomState(0)
-
-    # generate a noisy sine wave to act as our fake observations
-    n_timesteps = 100
-    x = np.linspace(0, 3 * np.pi, n_timesteps)
-    src_observations = 20 * (np.sin(x) + 0.5 * rnd.randn(n_timesteps))
     
     pl.ion()
     
-    for i in range(0,len(src_observations)):
-        
-        observations = src_observations[0:i+2]
-        
-        states_pred = filter.get_xy_position(src_observations[i], np.pi/.2, np.pi/6.)
-        
-        x = np.linspace(0, 3 * np.pi, i+2)
-        fig = pl.figure(figsize=(16, 6))
-        
-        
-        obs_scatter = pl.scatter(x, observations, marker='x', color='b',
-                                 label='observations')
-        position_line = pl.plot(x, states_pred,
-                                linestyle='-', marker='o', color='r',
-                                label='position est.')
-        #velocity_line = pl.plot(x, states_pred[:, 1],
-        #                        linestyle='-', marker='o', color='g',
-        #                        label='velocity est.')
-        pl.legend(loc='lower right')
-        pl.xlim(xmin=0, xmax=x.max())
-        pl.xlabel('time')
-        pl.show()
-        pl.pause(1)
-        pl.close()
+   
+
+    lower_bound = max(0,i-4)
+    upper_bound = i+2
+    
+    observations = src_observations[lower_bound:upper_bound]
+    
+    states_pred = filter.get_xy_position(src_observations[i], np.pi / .2, np.pi / 6.)
+    print len(states_pred)
+    print len(observations)
+    x = np.linspace(lower_bound, 3 * np.pi, upper_bound)
+    print len(x)
+    fig = pl.figure(figsize=(16, 6))
+            
+    obs_scatter = pl.scatter(x, observations, marker='x', color='b',
+                             label='observations')
+    position_line = pl.plot(x, states_pred,
+                            linestyle='-', marker='o', color='r',
+                            label='position est.')
+    # velocity_line = pl.plot(x, states_pred[:, 1],
+    #                        linestyle='-', marker='o', color='g',
+    #                        label='velocity est.')
+    pl.legend(loc='lower right')
+    pl.xlim(xmin=0, xmax=x.max())
+    pl.xlabel('time')
+    pl.show()
+    pl.pause(1)
+    pl.close()
             
     
