64dcb06f87a9a33582cfd838f4a9868c6e849200
Sascha Hornauer <sascha.hornauer@uni-oldenburg.de>
2017-08-30 23:19:09 -0700

    ...

--- Sascha/util/filters/simple_kalman.py
+++ Sascha/util/filters/simple_kalman.py
@@ -14,14 +14,15 @@ import pylab as plt
 
 class Simple_Kalman_Estimator():
 
-    number_of_observations = 10
-    
     kf_x = KalmanFilter(transition_matrices=np.array([[1, 1], [0, 1]]),
                   transition_covariance=0.01 * np.eye(2))
     kf_y = KalmanFilter(transition_matrices=np.array([[1, 1], [0, 1]]),
                   transition_covariance=0.01 * np.eye(2))
     observations_x = []
     observations_y = []
+    
+    def __init__(self, number_of_observations = 10):
+        self.number_of_observations = number_of_observations
 
     def get_xy_position(self, position_xy, heading, steering_angle):
         
@@ -47,38 +48,22 @@ class Simple_Kalman_Estimator():
         states_pred_y = self.kf_y.em(self.observations_y).smooth(self.observations_y)[0]
         
         
-        return (states_pred_x[:, 0],states_pred_y[:,0])
+        return (states_pred_x[:, 0][-1],states_pred_y[:,0][-1])
 
-if __name__ == '__main__':
-    
-    filter = Simple_Kalman_Estimator()
-
-    rnd = np.random.RandomState(0)
-    plt.ion()
-
-    
-    # generate a noisy sine wave to act as our fake observations
-    n_timesteps = 100
-    x = np.linspace(0, 3 * np.pi, n_timesteps)
-    observations = 20 * (np.sin(x) + 0.5 * rnd.randn(n_timesteps))
-    actual_values = 20 * (np.sin(x) + 0.5)
-    for i in range(0,len(observations)):
-        #plt.figure(figsize=(16, 6))
-        result = filter.get_xy_position((observations[i],observations[i]), np.pi / .2, np.pi / 6.)
-        #print result
-        #print len(result)
-        #x = np.arange(0,len(result))
-        #print x
-        #plt.plot(x,result[0], color='b')
-        #plt.plot(x,actual_values[max(0,i-9):i+1], color='r')
-        #plt.plot(x,observations[max(0,i-9):i+1],color='g')
-        #plt.show()
-        #plt.pause(0.1)
-        #plt.close()
-        print "--"
-        print np.mean(observations[max(0,i-9):i+1]-result[0])
-        print "--"
-        time.sleep(1)
-        
-        
-    
+# if __name__ == '__main__':
+#     
+#     filter = Simple_Kalman_Estimator()
+# 
+#     rnd = np.random.RandomState(0)
+# 
+#     n_timesteps = 100
+#     x = np.linspace(0, 3 * np.pi, n_timesteps)
+#     observations = 20 * (np.sin(x) + 0.5 * rnd.randn(n_timesteps))
+#     actual_values = 20 * (np.sin(x) + 0.5)
+#     for i in range(0,len(observations)):
+#         
+#         result = filter.get_xy_position((observations[i],observations[i]), np.pi / .2, np.pi / 6.)
+# 
+#         
+#         
+#     
--- Sascha/util/position_filter_interface.py
+++ Sascha/util/position_filter_interface.py
@@ -3,13 +3,23 @@ Created on Aug 30, 2017
 
 @author: Sascha Hornauer
 '''
-from kzpy3.Sascha.util.filters.simple_kalman import get_xy_position
+from kzpy3.Sascha.util.filters.simple_kalman import Simple_Kalman_Estimator
 
-def get_xy_position(position_xy,heading,steering_angle):
-    return get_xy_position(position_xy,heading,steering_angle)
+class Position_Filter():
+
+    filter = Simple_Kalman_Estimator()
+
+    def get_xy_position(self,position_xy,heading,steering_angle):
+        return filter.get_xy_position(position_xy,heading,steering_angle)
 
 if __name__ == '__main__':
-    pass
+    
+    
+    
+    test_filter = Position_Filter()
+    
+    
+    
 
 
 
