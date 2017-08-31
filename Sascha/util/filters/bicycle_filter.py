5fa063d391fa79d8be25bdf432c7e2fa6edf8113
Sascha Hornauer <sascha.hornauer@uni-oldenburg.de>
2017-08-30 20:49:51 -0700

    ...

--- Sascha/util/filters/bicycle_filter.py
+++ Sascha/util/filters/bicycle_filter.py
@@ -9,76 +9,69 @@ import os
 import sys
 from os.path import expanduser
 import time
+from pykalman import KalmanFilter
+import pylab as pl
 
-# The third party module filterpy is added with full source so it is not necessary
-# to install it on all cars. This is just an experiment, maybe another filter is used
-# in the future.
-if not 'filterpy' is sys.path:
-    filterpath = str(os.path.join(expanduser("~"),'kzpy3','Sascha','util','filters','third_party','filterpy')) 
-    sys.path.append(filterpath)
-else:
-    print "Filterpy was in path" 
-    
-from kzpy3.Sascha.util.filters.third_party.filterpy.filterpy.kalman import KalmanFilter
-from kzpy3.Sascha.util.filters.third_party.filterpy.filterpy.common import Q_discrete_white_noise
+class Simple_Kalman_Estimator():
 
-def get_xy_position(position_xy,heading,steering_angle):
-    
-    my_filter = KalmanFilter(dim_x=2, dim_z=2)
-    
-    dt = 0.5
-    
-    my_filter.x = np.array([[2.],
-                            [0.]])       # initial state (location and velocity)
+    number_of_observations = 40
+    kf = KalmanFilter(initial_state_mean=0, n_dim_obs=2)
+    observations = []
 
-    my_filter.F = np.array([[1.,1.],
-                    [0.,1.]])    # state transition matrix
-    
-    my_filter.H = np.array([[1.,1.]])    # Measurement function
-    my_filter.P *= 1000.                 # covariance matrix
-    my_filter.R = 5                      # state uncertainty
-    my_filter.Q = Q_discrete_white_noise(2, dt, .1) # process uncertainty
-     
-    i = 0.
-    '''
-        ----------
-    x : numpy.array(dim_x, 1)
-        State estimate vector
-
-    P : numpy.array(dim_x, dim_x)
-        Covariance matrix
-
-    R : numpy.array(dim_z, dim_z)
-        Measurement noise matrix
-
-    Q : numpy.array(dim_x, dim_x)
-        Process noise matrix
-
-    F : numpy.array()
-        State Transition matrix
-
-    H : numpy.array(dim_x, dim_x)
-        Measurement function
-    '''
-     
-    while True:
+    def get_xy_position(self,position_xy,heading,steering_angle):
         
-        i += 1.
+        self.observations = np.append(self.observations,position_xy)
         
-        time.sleep(1)
-        my_filter.predict()
-        my_filter.update(np.array([2.*i]))
-     
-        # do something with the output
-        x = my_filter.x
-        print x
+        if len(self.observations == 1):
+            self.observations = np.append(self.observations,position_xy)
         
-        if i > 20:
-            break
-    
+        
+        
+        # create a Kalman Filter by hinting at the size of the state and observation
+        # space.  If you already have good guesses for the initial parameters, put them
+        # in here.  The Kalman Filter will try to learn the values of all variables.
+        kf = KalmanFilter(transition_matrices=np.array([[1, 1], [0, 1]]),
+                          transition_covariance=0.01 * np.eye(2))
+        
+        states_pred = kf.em(observations).smooth(observations)[0]
+        
+        return states_pred
 
 if __name__ == '__main__':
     
-    print get_xy_position((23.0,4.0), np.pi/.2, np.pi/6.)
+    filter = Simple_Kalman_Estimator()
     
+    rnd = np.random.RandomState(0)
+        
+    # generate a noisy sine wave to act as our fake observations
+    n_timesteps = 100
+    x = np.linspace(0, 3 * np.pi, n_timesteps)
+    src_observations = 20 * (np.sin(x) + 0.5 * rnd.randn(n_timesteps))
+    
+    observations = src_observations[0]
+    
+    pl.ion()
+    
+    for i in range(1,len(src_observations)):
+    
+        states_pred = filter.get_xy_position(src_observations[i], np.pi/.2, np.pi/6.)
+        
+        x = np.linspace(0, 3 * np.pi, i+1)
+        fig = pl.figure(figsize=(16, 6))
+        
+        obs_scatter = pl.scatter(x, observations, marker='x', color='b',
+                                 label='observations')
+        position_line = pl.plot(x, states_pred[:, 0],
+                                linestyle='-', marker='o', color='r',
+                                label='position est.')
+        velocity_line = pl.plot(x, states_pred[:, 1],
+                                linestyle='-', marker='o', color='g',
+                                label='velocity est.')
+        pl.legend(loc='lower right')
+        pl.xlim(xmin=0, xmax=x.max())
+        pl.xlabel('time')
+        pl.show()
+        pl.pause(1)
+        pl.close()
+            
     
