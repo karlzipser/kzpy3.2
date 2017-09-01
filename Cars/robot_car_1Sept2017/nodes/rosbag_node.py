94307d7642b73e4a6e3840a32604b97ccd5fe7a7
Karl Zipser <karlzipser@hiMac.lan>
2017-09-01 08:12:43 -0700

    gacp

--- Cars/robot_car_1Sept2017/nodes/rosbag_node.py
+++ Cars/robot_car_1Sept2017/nodes/rosbag_node.py
@@ -13,8 +13,8 @@ try:
     existing_bag_files = sgg('/media/nvidia/rosbags/*.bag')
     folder_name = existing_bag_files[0].replace('bair_car',rp.computer_name)
     folder_name = '_'.join(folder_name.split('_')[:-1])
-    print('mkdir '+folder_name)
-    print('mv /media/nvidia/rosbags/*.bag '+folder_name)
+    unix('mkdir '+folder_name)
+    unix('mv /media/nvidia/rosbags/*.bag '+folder_name)
 except Exception as e:
     print("********** rosbag_node.py Exception ***********************")
     print(e.message, e.args)
