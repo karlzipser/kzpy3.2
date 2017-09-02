719dbe22a8fffbaaaa01275fa2f343f5a25c13b9
Karl Zipser <karlzipser@berkeley.edu>
2017-09-01 17:36:41 -0700

    gacp

--- scratch/c/_color_xterms.py
+++ scratch/c/_color_xterms.py
@@ -3,14 +3,14 @@ import kzpy3.Cars.robot_car_1Sept2017.nodes.runtime_parameters as rp
 
 Car_xterm_colors_dic = {'Mr_Blue':('white','blue'),
 	'Mr_Black':('white','black'),
-	'Mr_Orange':('black','orange'),
+	'Mr_Orange':('white','red'),
 	'Mr_Yellow':('black','yellow'),
 	'Mr_Lt_Blue':('black','cyan'),
 	'Mr_Purple':('white','purple'),
 }
-
-for k in Car_xterm_colors_dic:
-	fg = Car_xterm_colors_dic[k][0]
-	bg = Car_xterm_colors_dic[k][1]
-	ssh = d2n('ssh nvidia@',rp.Car_IP_dic[k])
-	os.system(d2s('xterm -hold -bg',bg,'-fg',fg,'-e ',ssh,'&'))
+for geo in ['80x10','20x10']:
+	for k in Car_xterm_colors_dic:
+		fg = Car_xterm_colors_dic[k][0]
+		bg = Car_xterm_colors_dic[k][1]
+		ssh = d2n('ssh nvidia@',rp.Car_IP_dic[k])
+		os.system(d2s('xterm -hold -geometry',geo,'-bg',bg,'-fg',fg,'-e ',ssh,'&'))
