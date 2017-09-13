d2fefa90f5f212d8e5b26bc3b7732d04b5b5483d
Karl Zipser <karlzipser@hiMac.lan>
2017-09-13 13:17:31 -0700

    gacp

--- Cars/robot_car_1Sept2017/nodes/network_node.py
+++ Cars/robot_car_1Sept2017/nodes/network_node.py
@@ -315,7 +315,7 @@ def paramiko_connection_thread():
 						pd2s('ssh connection to',k,'failed')
 						pass
 		time.sleep(5)
-#threading.Thread(target=paramiko_connection_thread).start()
+threading.Thread(target=paramiko_connection_thread).start()
 
 ssh_command_str = ''
 
@@ -342,7 +342,7 @@ def paramiko_command_thread():
 				time.sleep(0.1-t) #one_over_sixty - t)
 		else:
 			time.sleep(0.2)
-#threading.Thread(target=paramiko_command_thread).start()
+threading.Thread(target=paramiko_command_thread).start()
 #
 ###################################################################
 
