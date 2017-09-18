41aeb70e75115fbbadfb5e452c1a76f9671f6e86
Karl Zipser <karlzipser@Karls-MacBook.lan>
2017-09-18 11:42:41 -0700

    gacp

--- Cars/aruco_driver_17Sept2017_MSE_only/nodes/network.py
+++ Cars/aruco_driver_17Sept2017_MSE_only/nodes/network.py
@@ -299,7 +299,7 @@ while not rospy.is_shutdown():
 
 				forward_motor, forward_steer = run_model(camera_data, metadata)
 
-				if forward_motor < 54:
+				if 'Back' not in rp.computer_name and forward_motor < 54:
 					torch_motor = 99 - back_motor
 					torch_steer = 99 - back_steer
 				else:
