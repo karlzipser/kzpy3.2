c0e97fa1029687b5fa9c620f653b58b8620309f0
Karl Zipser <karlzipser@berkeley.edu>
2017-09-30 18:19:22 -0700

    gacp

--- Train_app/Train_SqueezeNet_IMU1/Data_Packer_Module.py
+++ Train_app/Train_SqueezeNet_IMU1/Data_Packer_Module.py
@@ -6,7 +6,8 @@ O = h5r(opjD('bdd_car_data_Sept2017_aruco_demo/h5py/Mr_Black_2017-09-02-13-42-50
 
 def Data_Packer(**Args):
 
-	
+
+
 
 	for we_are in ["the setup section"]:
 
@@ -15,27 +16,38 @@ def Data_Packer(**Args):
 
 	for we_are in ['function definitions']:
 
+
+
+
+
 		def _next():
+
+			L,O = _next_files()
 
 			name='name'
 
-			if _[CTR] >= len(L['ts'])-7*P[N_FRAMES]:
+			if _[CTR] >= len(L['ts'])-7*P[N_FRAMES]: # note constant, 7
+				L.close()
+				O.close()
 				_[CTR] = 0
+				return _next()
+			else:
+				list_of_images = []
 
-			list_of_images = []
+				for i in range(6*P[N_FRAMES]):
+					img = O['left_image']['vals'][_[CTR]][:,:,1]	# i.e., (94, 168)
+					img = np.expand_dims(img,axis=2)				# i.e., (94, 168, 1)
+					list_of_images.append( img )
+					_[CTR] += 1
 
-			for i in range(6*P[N_FRAMES]):
-				img = O['left_image']['vals'][_[CTR]][:,:,1]# (94, 168)
-				img = np.expand_dims(img,axis=2)			# (94, 168, 1)
-				list_of_images.append( img )
-				_[CTR] += 1
+				list_of_meta_data_floats_or_arrays=[0.0]
+				steer = L['steer'][_[CTR]:_[CTR]+P[N_STEPS]]/99.
+				motor = L['motor'][_[CTR]:_[CTR]+P[N_STEPS]]/99.
+				list_of_target_floats_or_lists = list(steer) + list(motor)
+				return name,list_of_images,list_of_meta_data_floats_or_arrays,list_of_target_floats_or_lists
 
 
-			list_of_meta_data_floats_or_arrays=[0.0]
-			steer = L['steer'][_[CTR]:_[CTR]+P[N_STEPS]]/99.
-			motor = L['motor'][_[CTR]:_[CTR]+P[N_STEPS]]/99.
-			list_of_target_floats_or_lists = list(steer) + list(motor)
-			return name,list_of_images,list_of_meta_data_floats_or_arrays,list_of_target_floats_or_lists
+
 
 	for we_are in ['the place where we name the functions']:
 
