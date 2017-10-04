d29589976054b57b43265a4b24028103630eb9e1
Karl Zipser <karlzipser@hiMac.lan>
2017-10-04 09:00:16 -0700

    gacp

--- Cars/robot_car_4Oct2017/nodes/ard_MSE.py
+++ Cars/robot_car_4Oct2017/nodes/ard_MSE.py
@@ -89,7 +89,7 @@ class Net_Steer_Net_Motor(Computer_Control):
 
 
 def buttons_to_state(Arduinos,M,BUTTON_DELTA):
-	print button_pwm_lst[-10:]
+	print M['button_pwm_lst'][-10:]
 	if np.abs(M['button_pwm_lst'][-1] - M['state_four'].button_pwm_peak) < BUTTON_DELTA:
 		if M['current_state'] == None:
 			M['current_state'] = M['state_four']
