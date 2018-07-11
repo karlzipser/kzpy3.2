c050950fe3e0301e3704f7c81ea749cfbb93ec82
Karl Zipser <karlzipser@Karls-MacBook.lan>
2018-07-11 11:44:09 -0700

    gacp

--- Cars/car_3July2018/nodes/network_node.py
+++ Cars/car_3July2018/nodes/network_node.py
@@ -70,7 +70,7 @@ while main_timer.check() == False:
                 current_steer = (1.0-s)*torch_steer + s*current_steer
                 current_motor = (1.0-s)*torch_steer + s*current_motor
 
-            adjusted_motor = rp.motor_gain*(current_motor - 49)+rp.motor_offset+49
+            adjusted_motor = rp.motor_gain*(current_motor-49) + rp.motor_offset + 49
 
             steer_cmd_pub.publish(std_msgs.msg.Int32(current_steer))
             motor_cmd_pub.publish(std_msgs.msg.Int32(adjusted_motor))
