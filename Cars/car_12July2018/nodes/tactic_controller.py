a8c9c8e81bdadc5f375ac2c07af5a48fb0a80966
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-07-12 14:43:12 -0700

    gacp

--- Cars/car_12July2018/nodes/tactic_controller.py
+++ Cars/car_12July2018/nodes/tactic_controller.py
@@ -3,6 +3,7 @@ from kzpy3.utils2 import *
 import threading
 
 P = {}
+P['SMOOTHING_PARAMETER_1'] = 0.75
 P['ABORT'] = False
 
 def get_arduino_serial_connections(baudrate, timeout):
@@ -58,13 +59,32 @@ def assign_serial_connections(sers):
         spd2s('MSE not found: Is transmitter turned on? Is MSE battery plugged in?')
         #P['ABORT'] = True
     return Arduinos
+def pwm_to_percent(null_pwm,current_pwm,max_pwm,min_pwm):
+    current_pwm -= null_pwm
+    max_pwm -= null_pwm
+    min_pwm -= null_pwm
+    if current_pwm >= 0:
+        p = 99*(1.0 + current_pwm/max_pwm)/2.0
+    else:
+        p = 99*(1.0 - current_pwm/min_pwm)/2.0
+    if p > 99:
+        p = 99
+    if p < 0:
+        p = 0      
+    return p
+def percent_to_pwm(percent,null_pwm,max_pwm,min_pwm):
+    if percent >= 49:
+        p = (percent-50)/50.0 * (max_pwm-null_pwm) + null_pwm
+    else:
+        p = (percent-50)/50.0 * (null_pwm-min_pwm) + null_pwm
+    return p
 
 
 
 
 
 def _TACTIC_RC_controller_run_loop(D,P):
-    print('_run_loop')
+    print('_TACTIC_RC_controller_run_loop')
     time.sleep(0.1)
     D['arduino'].flushInput()
     time.sleep(0.1)
@@ -72,7 +92,7 @@ def _TACTIC_RC_controller_run_loop(D,P):
     flush_seconds = 0.25
     flush_timer = Timer(flush_seconds)
     ctr_timer = Timer()
-    print_timer = Timer(0.01)
+    print_timer = Timer(1)
     while P['ABORT'] == False:
         if 'Brief sleep to allow other threads to process...':
             time.sleep(0.0001)
@@ -118,7 +138,6 @@ def _TACTIC_RC_controller_run_loop(D,P):
             print e
             pass            
     print 'end _TACTIC_RC_controller_run_loop.'
-
 def TACTIC_RC_controller(arduino,P):
     D = {}
     D['ctr'] = 0
@@ -132,6 +151,61 @@ def TACTIC_RC_controller(arduino,P):
 # python kzpy3/Cars/car_12July2018/nodes/tactic_controller.py
 
 
+def _calibrate_run_loop(D,RC,P):
+    print "_calibrate_run_loop"
+    print_timer = Timer(0.1)
+    D['calibrated'] = False
+    while P['ABORT'] == False:
+        if 'Brief sleep to allow other threads to process...':
+            time.sleep(0.0001)
+        if D['calibrated'] == True:
+            D['servo_percent'] = pwm_to_percent(
+                D['servo_pwm_null'],RC['servo_pwm'],D['servo_pwm_max'],D['servo_pwm_min'])
+            D['motor_percent'] = pwm_to_percent(
+                D['motor_pwm_null'],RC['motor_pwm'],D['motor_pwm_max'],D['motor_pwm_min'])
+        if True:#try:
+            if RC['button_number'] != 4:
+                #time.sleep(0.1)
+                continue
+            if RC['button_time'] < 0.1:
+                D['calibrated'] = False
+                D['servo_pwm_null'] = RC['servo_pwm']
+                D['motor_pwm_null'] = RC['motor_pwm']
+            elif RC['button_time'] < 1.0:
+                s = P['SMOOTHING_PARAMETER_1']
+                D['servo_pwm_null'] = (1.0-s)*RC['servo_pwm'] + s*D['servo_pwm_null']
+                D['motor_pwm_null'] = (1.0-s)*RC['motor_pwm'] + s*D['motor_pwm_null']
+                D['servo_pwm_min'] = D['servo_pwm_null']
+                D['servo_pwm_max'] = D['servo_pwm_null']
+                D['motor_pwm_min'] = D['motor_pwm_null']
+                D['motor_pwm_max'] = D['motor_pwm_null']
+            else:
+                if D['servo_pwm_max'] < RC['servo_pwm']:
+                    D['servo_pwm_max'] = RC['servo_pwm']
+                if D['servo_pwm_min'] > RC['servo_pwm']:
+                    D['servo_pwm_min'] = RC['servo_pwm']
+                if D['motor_pwm_max'] < RC['motor_pwm']:
+                    D['motor_pwm_max'] = RC['motor_pwm']
+                if D['motor_pwm_min'] > RC['motor_pwm']:
+                    D['motor_pwm_min'] = RC['motor_pwm']
+                if D['servo_pwm_max'] - D['servo_pwm_min'] > 300:
+                    if D['motor_pwm_max'] - D['motor_pwm_min'] > 300:
+                        D['calibrated'] = True
+        else:#except Exception as e:
+            print e
+            pass 
+        if print_timer.check():
+            pprint(D)
+            print_timer.reset()           
+    print 'end _calibrate_run_loop.'
+def Calibration_Mode(rc_controller,P):
+    D = {}
+    threading.Thread(target=_calibrate_run_loop,args=[D,rc_controller,P]).start()
+    return D
+
+
+
+
 P['USE_MSE'] = True
 
 if 'Start Arduino threads...':
@@ -142,6 +216,7 @@ if 'Start Arduino threads...':
 
     if P['USE_MSE'] and 'MSE' in Arduinos.keys():
         Tactic_RC_controller = TACTIC_RC_controller(Arduinos['MSE'],P)
+        Calibration_mode = Calibration_Mode(Tactic_RC_controller,P)
     else:
         spd2s("!!!!!!!!!! 'MSE' not in Arduinos[] !!!!!!!!!!!")    
 
