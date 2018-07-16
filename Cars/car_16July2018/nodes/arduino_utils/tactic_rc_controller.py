8ced238ed153585d190f843cbf3b100f829cd4ec
Karl Zipser <karlzipser@Karls-MacBook.lan>
2018-07-16 13:30:31 -0700

    gacp

--- Cars/car_16July2018/nodes/arduino_utils/tactic_rc_controller.py
+++ Cars/car_16July2018/nodes/arduino_utils/tactic_rc_controller.py
@@ -80,7 +80,7 @@ def _TACTIC_RC_controller_run_loop(P):
                 if P['agent_choice'] == 'human':
                     write_str = d2n( '(', int(P['servo_pwm_smooth']), ',', int(P['motor_pwm_smooth']+10000), ')')
                 elif P['agent_choice'] == 'network':
-                    print_timer.message(d2s(P['time_since_button_4']))
+                    print_timer.message(d2s(P['time_since_button_4'].time()))
                     if P['time_since_button_4'] > 1:
                         _servo_pwm = percent_to_pwm(P['network']['servo_percent'],P['servo_pwm_null'],P['servo_pwm_max'],P['servo_pwm_min'])
                         _motor_pwm = percent_to_pwm(P['network']['motor_percent'],P['motor_pwm_null'],P['motor_pwm_max'],P['motor_pwm_min'])
