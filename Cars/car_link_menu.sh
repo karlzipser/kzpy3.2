c50aaf42a6f1ff9ca29fedf8a12c8d46c9d982f3
Karl Zipser <karlzipser@Karls-MacBook.lan>
2018-07-16 11:29:18 -0700

    gacp

--- Cars/car_link_menu.sh
+++ Cars/car_link_menu.sh
@@ -2,7 +2,7 @@
 
 OPTIONS="exit ls bdd_car_rewrite_SD2 bdd_car_rewrite_SD2_LCR bdd_car_rewrite_SD2_LCR_net 
   robot_car_1Sept2017 robot_car_observer1 robot_car_4Oct2017
-  aruco_driver_16Sept2017 aruco_driver_17Sept2017_MSE_only car_12July2018 car_3July2018"
+  aruco_driver_16Sept2017 aruco_driver_17Sept2017_MSE_only car_12July2018 car_16July2018"
 COLUMNS=12
 echo 'car_link_menu:'
 select opt in $OPTIONS; do
@@ -19,9 +19,9 @@ select opt in $OPTIONS; do
     ls -al ~/catkin_ws/src/bair_car
 
 
-   elif [ "$opt" = "car_3July2018" ]; then
+   elif [ "$opt" = "car_16July2018" ]; then
     rm ~/catkin_ws/src/bair_car
-    ln -s ~/kzpy3/Cars/car_3July2018 ~/catkin_ws/src/bair_car
+    ln -s ~/kzpy3/Cars/car_16July2018 ~/catkin_ws/src/bair_car
     ls -al ~/catkin_ws/src/bair_car
 
    elif [ "$opt" = "aruco_driver_16Sept2017" ]; then
