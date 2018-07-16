286997cb1f3f887133d378f1a2f708f241a3aa30
Karl Zipser <karlzipser@Karls-MacBook.lan>
2018-07-16 13:40:36 -0700

    gacp

--- Cars/car_16July2018/nodes/arduino_utils/tactic_rc_controller.py
+++ Cars/car_16July2018/nodes/arduino_utils/tactic_rc_controller.py
@@ -80,12 +80,16 @@ def _TACTIC_RC_controller_run_loop(P):
                 if P['agent_choice'] == 'human':
                     write_str = d2n( '(', int(P['servo_pwm_smooth']), ',', int(P['motor_pwm_smooth']+10000), ')')
                 elif P['agent_choice'] == 'network':
-                    print_timer.message(d2s(P['time_since_button_4'].time()))
-                    if P['time_since_button_4'].time() > 1:
+                    if np.abs(P['human']['servo_percent']-49) > 4 or np.abs(P['human']['motor_percent']-49) > 4:
+                        write_str = d2n( '(', int(P['servo_pwm_smooth']), ',', int(P['motor_pwm_smooth']+10000), ')')
+                        P['time_since_button_4'].reset()
+                        print_timer.message('Temporary human control control...')
+                    elif P['time_since_button_4'].time() > 2.0:
                         _servo_pwm = percent_to_pwm(P['network']['servo_percent'],P['servo_pwm_null'],P['servo_pwm_max'],P['servo_pwm_min'])
                         _motor_pwm = percent_to_pwm(P['network']['motor_percent'],P['motor_pwm_null'],P['motor_pwm_max'],P['motor_pwm_min'])
                         write_str = d2n( '(', int(_servo_pwm), ',', int(_motor_pwm+10000), ')')
                     else:
+                        print_timer.message('Waiting before giving network control...')
                         write_str = d2n( '(',49,',',49+10000,')')
                 if P['button_number'] != 4:
                     if P['calibrated']:
