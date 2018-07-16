2ca2320256bad11423a11a89359b768b58faef88
Karl Zipser <karlzipser@Karls-MacBook.lan>
2018-07-16 13:53:30 -0700

    gacp

--- Cars/car_16July2018/nodes/arduino_utils/tactic_rc_controller.py
+++ Cars/car_16July2018/nodes/arduino_utils/tactic_rc_controller.py
@@ -83,7 +83,7 @@ def _TACTIC_RC_controller_run_loop(P):
                     if np.abs(P['human']['servo_percent']-49) > 4 or np.abs(P['human']['motor_percent']-49) > 4:
                         write_str = d2n( '(', int(P['servo_pwm_smooth']), ',', int(P['motor_pwm_smooth']+10000), ')')
                         #P['time_since_button_4'].reset()
-                        print_timer.message(d2s('Temporary human control control...',P['human']['servo_percent'],P['human']['motor_percent'])
+                        print_timer.message(d2s('Temporary human control control...',P['human']['servo_percent'],P['human']['motor_percent']))
                     elif P['time_since_button_4'].time() > 2.0:
                         print_timer.message('here')
                         _servo_pwm = percent_to_pwm(P['network']['servo_percent'],P['servo_pwm_null'],P['servo_pwm_max'],P['servo_pwm_min'])
