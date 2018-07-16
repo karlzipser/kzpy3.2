111b949877a755eb1d9b317f0186b40c2e6a1d76
Karl Zipser <karlzipser@Karls-MacBook.lan>
2018-07-16 11:20:25 -0700

    gacp

--- scratch/c/_temp4.py
+++ scratch/c/_temp4.py
@@ -3,7 +3,7 @@ import curses
 
 def pbar(window):
     for i in range(10):
-        window.addstr(10, 10, "[" + ("=" * i) + ">" + (" " * (10 - i )) + "]")
+        window.addstr(1, 10, "[" + ("=" * i) + ">" + (" " * (10 - i )) + "]")
         window.refresh()
         time.sleep(0.5)
 
