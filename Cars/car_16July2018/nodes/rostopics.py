0f95113395568c7b4500cc497ca4199c886b8329
Karl Zipser <karlzipser@Karls-MacBook.lan>
2018-07-17 10:11:35 -0700

    gacp

--- Cars/car_16July2018/nodes/rostopics.py
+++ Cars/car_16July2018/nodes/rostopics.py
@@ -69,10 +69,10 @@ if using_linux():
     P['ABORT'] = False
     timer = Timer(0.001)
     def printer_thread():
-        while not P['ABORT']:
-        for k in Rostopics.keys():
-            t = k.replace('/bair_car','')
-            timer.message(d2s(t,"=\t",P[k]))
+            while not P['ABORT']:
+            for k in Rostopics.keys():
+                t = k.replace('/bair_car','')
+                timer.message(d2s(t,"=\t",P[k]))
     threading.Thread(target=printer_thread,args=[]).start()
 
     q = '_'
