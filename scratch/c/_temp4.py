5b553c8e81bf1cfca36f7098b449f945dbae55f7
Karl Zipser <karlzipser@Karls-MacBook.lan>
2018-07-17 00:14:59 -0700

    gacp

--- scratch/c/_temp4.py
+++ scratch/c/_temp4.py
@@ -12,6 +12,7 @@ if False:
 		for j in range(100):
 		    for i in range(10):
 		        window.addstr(i, 10, d2s('xyz:',dp(np.random.rand(1),2)))
+		        #stdscr.addstr(0, 0, whstr, curses.color_pair(1))
 		        window.refresh()
 		    time.sleep(0.1)
 	curses.wrapper(pbar)
@@ -129,6 +130,110 @@ if False:
 	    main()
 
 
+if False:
+	import curses 
 
+	screen = curses.initscr() 
+	#curses.noecho() 
+	curses.curs_set(0) 
+	screen.keypad(1) 
+	curses.mousemask(1)
 
+	screen.addstr("This is a Sample Curses Script\n\n") 
 
+	while True:
+	    event = screen.getch() 
+	    if event == ord("q"): break 
+	    if event == curses.KEY_MOUSE:
+	    	#print 'here'
+	        _, mx, my, _, _ = curses.getmouse()
+	        y, x = screen.getyx()
+	        print x,y
+	        screen.addstr(y, x, screen.instr(my, mx, 5))
+
+	curses.endwin()
+
+if True:
+	#!/usr/bin/env python2                                                       
+
+	import curses                                                                
+	from curses import panel                                                     
+
+	class Menu(object):                                                          
+
+	    def __init__(self, items, stdscreen):                                    
+	        self.window = stdscreen.subwin(0,0)                                  
+	        self.window.keypad(1)                                                
+	        self.panel = panel.new_panel(self.window)                            
+	        self.panel.hide()                                                    
+	        panel.update_panels()                                                
+
+	        self.position = 0                                                    
+	        self.items = items                                                   
+	        self.items.append(('exit','exit'))                                   
+
+	    def navigate(self, n):                                                   
+	        self.position += n                                                   
+	        if self.position < 0:                                                
+	            self.position = 0                                                
+	        elif self.position >= len(self.items):                               
+	            self.position = len(self.items)-1                                
+
+	    def display(self):                                                       
+	        self.panel.top()                                                     
+	        self.panel.show()                                                    
+	        self.window.clear()                                                  
+
+	        while True:                                                          
+	            self.window.refresh()                                            
+	            curses.doupdate()                                                
+	            for index, item in enumerate(self.items):                        
+	                if index == self.position:                                   
+	                    mode = curses.A_REVERSE                                  
+	                else:                                                        
+	                    mode = curses.A_NORMAL                                   
+
+	                msg = '%d. %s' % (index, item[0])                            
+	                self.window.addstr(1+index, 1, msg, mode)                    
+
+	            key = self.window.getch()                                        
+
+	            if key in [curses.KEY_ENTER, ord('\n')]:                         
+	                if self.position == len(self.items)-1:                       
+	                    break                                                    
+	                else:                                                        
+	                    self.items[self.position][1]()                           
+
+	            elif key == curses.KEY_UP:                                       
+	                self.navigate(-1)                                            
+
+	            elif key == curses.KEY_DOWN:                                     
+	                self.navigate(1)                                             
+
+	        self.window.clear()                                                  
+	        self.panel.hide()                                                    
+	        panel.update_panels()                                                
+	        curses.doupdate()
+
+	class MyApp(object):                                                         
+
+	    def __init__(self, stdscreen):                                           
+	        self.screen = stdscreen                                              
+	        curses.curs_set(0)                                                   
+
+	        submenu_items = [                                                    
+	                ('beep', curses.beep),                                       
+	                ('flash', curses.flash)                                      
+	                ]                                                            
+	        submenu = Menu(submenu_items, self.screen)                           
+
+	        main_menu_items = [                                                  
+	                ('beep', curses.beep),                                       
+	                ('flash', curses.flash),                                     
+	                ('submenu', submenu.display)                                 
+	                ]                                                            
+	        main_menu = Menu(main_menu_items, self.screen)                       
+	        main_menu.display()                                                  
+
+	if __name__ == '__main__':                                                       
+	    curses.wrapper(MyApp)   
