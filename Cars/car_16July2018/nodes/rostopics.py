94468aea037da9d2144390b7614de028ebc5ecca
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-07-18 16:55:09 -0700

    gacp

--- Cars/car_16July2018/nodes/rostopics.py
+++ Cars/car_16July2018/nodes/rostopics.py
@@ -63,7 +63,7 @@ def get_ros_publisher_strs(Rostopics_publish,P):
         rtype = topic[1]
         pub_name = get_safe_name(name)+'_pub'
         pub_setup_strs.append(d2n(pub_name," = rospy.Publisher('",name,"', ",rtype,", queue_size=100)"))
-        pub_publish_strs.append(d2n(pub_name,".publish(",rtype,"(",P[B+name],")"))
+        pub_publish_strs.append(d2n(pub_name,".publish(",rtype,"(",P[B+name],"))"))
     return pub_setup_strs,pub_publish_strs
 
 
