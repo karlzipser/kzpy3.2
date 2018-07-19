886f65e368ced6af5920bc2f03cc7a31f12f7002
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-07-18 23:01:36 -0700

    gacp

--- Cars/car_16July2018/nodes/default_values.py
+++ Cars/car_16July2018/nodes/default_values.py
@@ -3,4 +3,4 @@ Default['network_output_sample'] = 4 # >= 0, <= 9
 Default['network_steer_gain'] = 3.0
 Default['network_motor_gain'] = 1.0
 Default['network_motor_offset'] = -2
-Default['network_smoothing_parameter'] = 0.0
+Default['network_smoothing_parameter'] = 0.33
