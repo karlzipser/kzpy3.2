7485d056d0c2f49ddbb5857650c1a0699b8b24fb
Karl Zipser <karlzipser@Karls-MacBook.lan>
2018-07-18 21:32:12 -0700

    gacp

--- Cars/car_16July2018/nodes/network_node.py
+++ Cars/car_16July2018/nodes/network_node.py
@@ -112,7 +112,7 @@ net_utils.init_model(N)
 
 while not main_timer.check():
     time.sleep(0.0001)
-    print_timer(d2s("N['network_steer_gain'] =",N['network_steer_gain']))
+    print_timer.message(d2s("N['network_steer_gain'] =",N['network_steer_gain']))
     Hz = frequency_timer.freq(name='Hz_network',do_print=True)
     if is_number(Hz):
         Hz_network_pub.publish(std_msgs.msg.Float32(Hz))
