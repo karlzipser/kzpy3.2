54f7ad569203725e2f1f0461b038b52e1c703481
Karl Zipser <karlzipser@Karls-MacBook.lan>
2018-07-20 12:42:36 -0700

    gacp

--- Cars/car_16July2018/nodes/network_node.py
+++ Cars/car_16July2018/nodes/network_node.py
@@ -109,7 +109,7 @@ low_frequency_pub_timer = Timer(0.5)
 
 net_utils.init_model(N)
 
-while not drive_mode_callback < 0:
+while not drive_mode < 0:
     time.sleep(0.0001)
     #print_timer.message(d2s("N['network_steer_gain'] =",N['network_steer_gain']))
     Hz = frequency_timer.freq(name='Hz_network',do_print=False)
