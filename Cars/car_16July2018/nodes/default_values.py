ea6f02f94074f9d18880007b970c72036eeb0a47
Karl Zipser <karlzipser@Karls-MacBook.lan>
2018-07-20 12:54:24 -0700

    gacp

--- Cars/car_16July2018/nodes/default_values.py
+++ Cars/car_16July2018/nodes/default_values.py
@@ -3,8 +3,8 @@ from kzpy3.utils2 import *
 Network = {}
 Network['network_output_sample'] = 4 # >= 0, <= 9
 Network['network_steer_gain'] = 3.0
-Network['network_motor_gain'] = 1.0
-Network['network_motor_offset'] = -2
+Network['network_motor_gain'] = 0.75
+Network['network_motor_offset'] = 0
 Network['network_smoothing_parameter'] = 0.33
 Network['weight_file_path'] = opjh('pytorch_models','net.infer')
 
