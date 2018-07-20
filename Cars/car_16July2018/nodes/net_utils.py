0a2e029ebd15f3f27669ac67b32567dab372dd36
Karl Zipser <karlzipser@Karls-MacBook.lan>
2018-07-20 12:01:32 -0700

    gacp

--- Cars/car_16July2018/nodes/net_utils.py
+++ Cars/car_16July2018/nodes/net_utils.py
@@ -119,7 +119,7 @@ def format_camera_data(left_list, right_list):
     for i in range(nframes):
         for side in (left_list, right_list):
             if GREY_OUT_TOP_OF_IMAGE:
-                if side[-i - 1][:188,:,:] = 128  #*= 0 ####################!!!!!!!!!!!!!!!!!!!!!!!!!
+                side[-i - 1][:188,:,:] = 128  #*= 0 ####################!!!!!!!!!!!!!!!!!!!!!!!!!
             listoftensors.append(torch.from_numpy(side[-i - 1]))
     camera_data = torch.cat(listoftensors, 2)
    
