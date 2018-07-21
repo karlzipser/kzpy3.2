28fa26150345ce674809a54b2ead04a46118fd50
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-07-21 10:13:20 -0700

    gacp

--- Cars/car_16July2018_stable/nodes/rosbag_node.py
+++ Cars/car_16July2018_stable/nodes/rosbag_node.py
@@ -11,59 +11,62 @@ for k in default_values.Record.keys():
 
 if not R['RECORD_DATA']:
     spd2s('rosbag_node.py::Not recording data')
-else:
-    foldername_prefix = os.environ["COMPUTER_NAME"] #'run_'
+    time.sleep(3600*24)
+    assert(False)
 
-    foldername = d2n(foldername_prefix,'_',time_str())
-    time.sleep(3)
 
-    if __name__ == '__main__':
-        rospy.init_node('rosbag_node', anonymous=True)
-        save_pub = rospy.Publisher('data_saving', std_msgs.msg.Int32, queue_size=100)
-        
-        fl = gg(opjh('catkin_ws/src/bair_car/rosbags/*'))
+foldername_prefix = os.environ["COMPUTER_NAME"] #'run_'
 
-        for f in fl:
-             os.remove(f)
+foldername = d2n(foldername_prefix,'_',time_str())
+time.sleep(3)
 
-        assert(len(sys.argv) >= 3)
+if __name__ == '__main__':
+    rospy.init_node('rosbag_node', anonymous=True)
+    save_pub = rospy.Publisher('data_saving', std_msgs.msg.Int32, queue_size=100)
+    
+    fl = gg(opjh('catkin_ws/src/bair_car/rosbags/*'))
 
-        bag_rec_folder = sys.argv[1] # '/home/ubuntu/catkin_ws/src/bair_car/rosbags'
-        bag_mv_folder = sys.argv[2] # '/media/ubuntu/3131-3031/rosbags'
-        bag_mv_folder = opj(bag_mv_folder,foldername)
+    for f in fl:
+         os.remove(f)
 
-        unix('mkdir '+bag_mv_folder)
-        
-        assert(os.path.exists(bag_rec_folder))
-        assert(os.path.exists(bag_mv_folder))
+    assert(len(sys.argv) >= 3)
 
-        rate = rospy.Rate(2.0)
+    bag_rec_folder = sys.argv[1] # '/home/ubuntu/catkin_ws/src/bair_car/rosbags'
+    bag_mv_folder = sys.argv[2] # '/media/ubuntu/3131-3031/rosbags'
+    bag_mv_folder = opj(bag_mv_folder,foldername)
 
-        try:
-            while not rospy.is_shutdown():
+    unix('mkdir '+bag_mv_folder)
+    
+    assert(os.path.exists(bag_rec_folder))
+    assert(os.path.exists(bag_mv_folder))
+
+    rate = rospy.Rate(2.0)
+
+    try:
+        while not rospy.is_shutdown():
+            save_pub.publish(std_msgs.msg.Int32(0))
+            for f in os.listdir(bag_rec_folder):
+                if '.bag' != os.path.splitext(f)[1]:
+                    continue
+                save_pub.publish(std_msgs.msg.Int32(1) )
+                print('Moving {0}'.format(f))
+                f_rec = os.path.join(bag_rec_folder, f)
+                f_mv = os.path.join(bag_mv_folder, f)
+                # shutil.copy(f_rec, f_mv)
+                start = time.time()
+                subprocess.call(['mv', f_rec, f_mv])
+                elapsed = time.time() - start
+                unix('rm '+opj(bag_rec_folder,'*.bag')) # 27 Nov 2016, to remove untransferred bags
+                print('Done in {0} secs\n'.format(elapsed))
                 save_pub.publish(std_msgs.msg.Int32(0))
-                for f in os.listdir(bag_rec_folder):
-                    if '.bag' != os.path.splitext(f)[1]:
-                        continue
-                    save_pub.publish(std_msgs.msg.Int32(1) )
-                    print('Moving {0}'.format(f))
-                    f_rec = os.path.join(bag_rec_folder, f)
-                    f_mv = os.path.join(bag_mv_folder, f)
-                    # shutil.copy(f_rec, f_mv)
-                    start = time.time()
-                    subprocess.call(['mv', f_rec, f_mv])
-                    elapsed = time.time() - start
-                    unix('rm '+opj(bag_rec_folder,'*.bag')) # 27 Nov 2016, to remove untransferred bags
-                    print('Done in {0} secs\n'.format(elapsed))
-                    save_pub.publish(std_msgs.msg.Int32(0))
-                    
-                rate.sleep()
-        except Exception as e:
-            print("********** Exception ***********************")
-            print(e.message, e.args)
-            print "rosbag_node.py Exception doing... unix(opjh('kzpy3/scripts/kill_ros.sh'))"
-            unix(opjh('kzpy3/scripts/kill_ros.sh'))
-
-        print "rosbag_node.py end doing... unix(opjh('kzpy3/scripts/kill_ros.sh'))"
+                
+            rate.sleep()
+    except Exception as e:
+        print("********** Exception ***********************")
+        print(e.message, e.args)
+        print "rosbag_node.py Exception doing... unix(opjh('kzpy3/scripts/kill_ros.sh'))"
         unix(opjh('kzpy3/scripts/kill_ros.sh'))
+
+    print "rosbag_node.py end doing... unix(opjh('kzpy3/scripts/kill_ros.sh'))"
+    unix(opjh('kzpy3/scripts/kill_ros.sh'))
 
