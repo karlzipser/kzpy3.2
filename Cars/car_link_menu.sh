a10a6e3203249452ea99edde9de16ef16d25cd6c
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-07-21 09:34:33 -0700

    gacp

--- Cars/car_link_menu.sh
+++ Cars/car_link_menu.sh
@@ -5,17 +5,14 @@ COLUMNS=12
 echo 'car_link_menu:'
 select opt in $OPTIONS; do
    COLUMNS=12
-
    if [ "$opt" = "car_21July2018" ]; then
     rm ~/catkin_ws/src/bair_car
     ln -s ~/kzpy3/Cars/car_21July2018 ~/catkin_ws/src/bair_car
     ls -al ~/catkin_ws/src/bair_car
-
    elif [ "$opt" = "car_16July2018_stable" ]; then
     rm ~/catkin_ws/src/bair_car
     ln -s ~/kzpy3/Cars/car_16July2018_stable ~/catkin_ws/src/bair_car
     ls -al ~/catkin_ws/src/bair_car
-    
    elif [ "$opt" = "ls" ]; then
     ls -al ~/kzpy3/Cars 
    elif [ "$opt" = "exit" ]; then
