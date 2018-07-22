b9349e0b94c69f1a015e28f0be125e6a85c2d53b
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-07-21 17:00:09 -0700

    gacp

--- Cars/car_16July2018_stable/nodes/_rosbag_node.py
+++ Cars/car_16July2018_stable/nodes/_rosbag_node.py
@@ -3,24 +3,19 @@ from kzpy3.utils2 import *
 import os, sys, shutil, subprocess, time
 import rospy
 import std_msgs.msg
-import runtime_parameters as rp
 
-os.environ['STOP'] = 'False'
 
-#exec_str = "from kzpy3.teg2.bdd_car_versions.bdd_car_rewrite_SD2.runtime_params import foldername"
-#print("from rosbag_node.py doing: '"+exec_str+"'")
-#exec(exec_str)
-#from runtime_parameters import foldername
-#import runtime_parameters as rp
 
-spd2s(rp.foldername)
+
+foldername_prefix = os.environ["COMPUTER_NAME"] #'run_'
+
+foldername = d2n(foldername_prefix,'_',time_str())
 time.sleep(3)
 
 if __name__ == '__main__':
     rospy.init_node('rosbag_node', anonymous=True)
     save_pub = rospy.Publisher('data_saving', std_msgs.msg.Int32, queue_size=100)
     
-
     fl = gg(opjh('catkin_ws/src/bair_car/rosbags/*'))
 
     for f in fl:
@@ -30,14 +25,9 @@ if __name__ == '__main__':
 
     bag_rec_folder = sys.argv[1] # '/home/ubuntu/catkin_ws/src/bair_car/rosbags'
     bag_mv_folder = sys.argv[2] # '/media/ubuntu/3131-3031/rosbags'
-    bag_mv_folder = opj(bag_mv_folder,rp.foldername)
+    bag_mv_folder = opj(bag_mv_folder,foldername)
 
     unix('mkdir '+bag_mv_folder)
-    #unix('mkdir  '+opj(bag_mv_folder,'.caf'))
-    #unix('mkdir  '+opj(bag_mv_folder,'.bair_car'))
-
-    #unix('scp -r /home/ubuntu/catkin_ws/src/bair_car ' + opj(bag_mv_folder,'.bair_car'))
-    #unix('scp /home/ubuntu/kzpy3/caf3/z2/z2.caffemodel ' + opj(bag_mv_folder,'.caf'))
     
     assert(os.path.exists(bag_rec_folder))
     assert(os.path.exists(bag_mv_folder))
@@ -45,9 +35,6 @@ if __name__ == '__main__':
     rate = rospy.Rate(2.0)
 
     try:
-        if os.environ['STOP'] == 'True':
-            stop_ros()
-            assert(False)
         while not rospy.is_shutdown():
             save_pub.publish(std_msgs.msg.Int32(0))
             for f in os.listdir(bag_rec_folder):
@@ -69,8 +56,9 @@ if __name__ == '__main__':
     except Exception as e:
         print("********** Exception ***********************")
         print(e.message, e.args)
-        os.environ['STOP'] = 'True'
-        rospy.signal_shutdown(d2s(e.message,e.args))
-        stop_ros()
-    stop_ros()
+        print "rosbag_node.py Exception doing... unix(opjh('kzpy3/scripts/kill_ros.sh'))"
+        unix(opjh('kzpy3/scripts/kill_ros.sh'))
+
+    print "rosbag_node.py end doing... unix(opjh('kzpy3/scripts/kill_ros.sh'))"
+    unix(opjh('kzpy3/scripts/kill_ros.sh'))
 
