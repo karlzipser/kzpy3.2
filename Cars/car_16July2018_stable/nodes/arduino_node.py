f5f7375d5e5995a06ddd5cc8e01bae41f3f3708c
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-07-22 21:54:54 -0700

    gacp

--- Cars/car_16July2018_stable/nodes/arduino_node.py
+++ Cars/car_16July2018_stable/nodes/arduino_node.py
@@ -149,9 +149,7 @@ if 'Start Arduino threads...':
     else:
         spd2s("!!!!!!!!!! 'IMU' not in Arduinos[] or not using 'IMU' !!!!!!!!!!!")
 
-while True:
-    time.sleep(5)
-"""    
+  
 if 'Main loop...':
     print 'main loop'
     q = '_'
@@ -168,5 +166,5 @@ if 'Main loop...':
         print "doing... unix(opjh('kzpy3/scripts/kill_ros.sh'))"
         time.sleep(0.5)
         unix(opjh('kzpy3/scripts/kill_ros.sh'))
-"""
+
 #EOF
