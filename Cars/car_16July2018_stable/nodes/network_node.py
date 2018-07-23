d5cac48babd7dca888627bf7cb2a2275fc66c90a
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-07-22 22:49:37 -0700

    gacp

--- Cars/car_16July2018_stable/nodes/network_node.py
+++ Cars/car_16July2018_stable/nodes/network_node.py
@@ -192,7 +192,10 @@ while True:
 
         if low_frequency_pub_timer2.check():
             mi(np.random.random((100,100)));spause()
+            Net_activity = Activity_Module.Net_Activity('batch_num',0, 'activiations',Torch_network['solver'].A)
+            Net_activity['view']('moment_index',0,'delay',1, 'scales',{'camera_input':4,'pre_metadata_features':0,'pre_metadata_features_metadata':1,'post_metadata_features':2})
             pd2s("N['weight_file_path'] =",N['weight_file_path'])
+            mi(np.random.random((100,100)));spause()
             #spd2s(adjusted_steer,adjusted_motor,drive_mode, human_agent, behavioral_mode)
             low_frequency_pub_timer2.reset()
     else:
