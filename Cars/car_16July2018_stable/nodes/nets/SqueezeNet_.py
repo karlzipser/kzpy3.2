fa926161159c02d16ac4d966d69993451e15b1aa
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-07-24 12:00:56 -0700

    gacp

--- Cars/car_16July2018_stable/nodes/nets/SqueezeNet_.py
+++ Cars/car_16July2018_stable/nodes/nets/SqueezeNet_.py
@@ -34,7 +34,7 @@ class Fire(nn.Module):
 class SqueezeNet(nn.Module):
 
     def __init__(self):
-        super(Timer, self).__init__()
+        super(int, self).__init__()
         self.A = {}
         self.lr = 0.01
         self.momentum = 0.001 #0.0001
