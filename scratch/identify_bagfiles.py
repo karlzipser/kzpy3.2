1b2357a66e324737552a891c6fa11ce9d92aa099
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-07-26 18:00:08 -0700

    gacp

--- scratch/identify_bagfiles.py
+++ scratch/identify_bagfiles.py
@@ -1,15 +1,16 @@
 
 from kzpy3.utils2 import *
 
-def identify_bagfiles(volume_name=''):
-	Bagfiles = {}
-	for dirpath, dirnames, filenames in os.walk(opjm(volume_name)):
+def identify_bagfiles(volume=''):
+	Bags = {}
+	for dirpath, dirnames, filenames in os.walk(opjm(volume)):
 	    for filename in [f for f in filenames if f.endswith(".bag")]:
-	    	if not (filename in Bagfiles):
-	    		Bagfiles[filename] = []
-	    	new_dirpath = dirpath.replace(opjm(volume_name),'')
-	    	Bagfiles[filename].append(opj(new_dirpath,filename))
-	    	print Bagfiles[filename][-1]
+	    	if not (filename in Bags):
+	    		Bags[filename] = []
+	    	new_dirpath = dirpath.replace(opjm(volume),'')
+	    	Bags[filename].append(opj(new_dirpath,filename))
+	    	print Bags[filename][-1]
+	Bagfiles = {'bags':Bags,'volume':volume}
 	return Bagfiles
 
-B = identify_bagfiles(volume_name='model_car_data_A1')
+B = identify_bagfiles(volume='model_car_data_A1')
