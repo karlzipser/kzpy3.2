0a6a8e7cdfca74555d4621ec99e7a0dede033f47
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-07-29 13:47:16 -0700

    gacp

--- Data_app/get_data_moments.py
+++ Data_app/get_data_moments.py
@@ -110,7 +110,7 @@ def get_data_moments__left_right_center_dataset_version(dataset_path,location,be
 
 	for i in range(len(ts)-num_steps):
 		timer.percent_message(i,len(ts)-num_steps)
-		r = is_this_a_good_data_moment(L=L,index=i,steps=num_steps,time_proportion_tolerance=0.2,state_proportion_tolerance=0.4,min_initial_steps=15,accepted_states=accepted_states,motor_threshold=52)
+		r = is_this_a_good_data_moment__left_right_center_version(drive_state=drive_state,L=L,index=i,steps=num_steps,time_proportion_tolerance=0.2,state_proportion_tolerance=0.4,min_initial_steps=15,accepted_states=accepted_states,motor_threshold=52)
 		results.append(r)
 		
 		if r:
@@ -142,7 +142,7 @@ def get_data_moments__left_right_center_dataset_version(dataset_path,location,be
 			except:
 				pd2s('failed data_moments')
 	figure('data moments');clf();ylim(-0.5,5.0)
-	plot(state+0.02) 
+	plot(drive_state+0.02) 
 	plot(L['left_ts_deltas'][:]*10)
 	plot(results)
 	plt.title(run_name)
@@ -207,7 +207,25 @@ def is_this_a_good_data_moment(L=None,index=0,steps=0,time_proportion_tolerance=
 	if state_1s / (1.0*steps) < state_proportion_tolerance:
 		return 0
 	return 1
-
+def is_this_a_good_data_moment__left_right_center_version(drive_state=None,L=None,index=0,steps=0,time_proportion_tolerance=0,state_proportion_tolerance=0,min_initial_steps=0,accepted_states=[],motor_threshold=0):
+	start_time = L['ts'][index]
+	end_time = L['ts'][index+steps-1]
+	d_time = end_time - start_time
+	expected_time = steps * 1/30.0
+	if abs(expected_time - d_time) > time_proportion_tolerance * expected_time:
+		return 0
+	state_1s = 0
+	for i in range(index,index + steps):
+		if intr(drive_state[i]) == 1: ########## This is the difference from the version above
+			state_1s += 1
+		else:
+			if i-index < min_initial_steps:
+				return 0
+		if L['motor'][i] < motor_threshold:
+			return 0
+	if state_1s / (1.0*steps) < state_proportion_tolerance:
+		return 0
+	return 1
 
 
 
