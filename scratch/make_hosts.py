38698512b0c8356ab9e1090fe19f737dc598dad9
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-08-02 11:40:45 -0700

    gacp

--- scratch/make_hosts.py
+++ scratch/make_hosts.py
@@ -2,15 +2,16 @@
 
 from kzpy3.utils import *
 
+base_ip = '192.168.1.'
 host_ip_dic = {
-	'Mr_Plain':101,
-	'Mr_New':103
+	'Mr_Plain':base_ip+'101',
+	'Mr_New':base_ip+'103'
 }
 
 host = os.environ('COMPUTER_NAME')
 
 host_list = [
-	(127.0.0.1,'localhost'),
+	('127.0.0.1','localhost'),
 	(host_ip_dic[host],host)
 	]
 
--- scripts/make_hosts.py
+++ scripts/make_hosts.py
@@ -2,18 +2,21 @@
 
 from kzpy3.utils import *
 
+base_ip = '192.168.1.'
 host_ip_dic = {
-	'Mr_Plain':101,
-	'Mr_New':103
+	'Mr_Plain':base_ip+'101',
+	'Mr_New':base_ip+'103'
 }
 
-host = 'Mr_Plain'
+host = os.environ('COMPUTER_NAME')
 
 host_list = [
-	(127.0.0.1,'localhost'),
+	('127.0.0.1','localhost'),
 	(host_ip_dic[host],host)
 	]
 
 for h in sorted(host_ip_dic.keys()):
 	if h != host:
 		host_list.append(host_ip_dic[host],host)
+
+pprint host_list
