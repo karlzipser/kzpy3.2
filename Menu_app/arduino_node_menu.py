e4104e0cd375cef8a460b66ab5d1d85c093edb56
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-08-17 14:29:55 -0700

    gacp

--- Menu_app/arduino_node_menu.py
+++ Menu_app/arduino_node_menu.py
@@ -4,14 +4,12 @@ from kzpy3.Menu_app.menu import *
 menu_path = opjh('.menu','network_node')
 
 Topics = [
-    ('network_smoothing_parameter',0.75,Float),
-    ('network_output_sample',4,Int),
-    ('network_motor_offset',3,Int),
-    ('network_steer_gain',8.1,Float),
-    ('network_motor_gain',1.4,Float),
-    ('servo_pwm_smooth_manual_offset',169,Int)
+    ('servo_feedback_left',Int),
+    ('servo_feedback_right',Int),
+    ('servo_feedback_center',Int)
 ]
 
 if __name__ == '__main__':
     menu(Topics,menu_path)
+    
 #EOF
