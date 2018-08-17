78ef81bb11ad512f7c4336c2631a3467e2206c3f
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-08-17 13:26:00 -0700

    gacp

--- Menu_app/subscriber.py
+++ Menu_app/subscriber.py
@@ -12,10 +12,27 @@ subscription_strs = get_ros_subscriber_strs(Topics)
 for c in [rosimport_str,rospyinit_str]+subscription_strs:
     if using_linux(): exec(c)
 
+R_avg = {}
+ctr = 0
+for t in R.keys():
+    R_avg[t] = 0
 
-for i in range(1000):
-    print R
-    time.sleep(0.1)
+
+
+raw_enter('Eeady to start? Hit enter...')
+time.sleep(0.5)
+print('recording...')
+
+timer = Timer(1.0)
+while not timer.check():
+    for t in R.keys():
+        R_avg[t] + = R[t]
+    ctr += 1
+for t in R.keys():
+    R_avg[t] /= (1.0*ctr)
+
+print('done.')
+pprint(R)
 
 raw_enter()
 
