fd3e83ae0e437597c36f0576be9797802f854f17
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-08-19 11:47:05 -0700

    gacp

---
+++ Cars/car_24July2018_2/nodes/arduino_utils/_serial_init.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+from kzpy3.utils2 import *
+exec(identify_file_str)
+from default_values import flex_names
+
+
+
+
+def get_arduino_serial_connections(baudrate, timeout):
+    if using_linux():
+        arduino_serial_prefix = 'ttyACM'
+    elif using_osx():
+        arduino_serial_prefix = 'tty.usbmodem141'
+    sers = []
+    ACM_ports = [opj('/dev', p) for p in os.listdir('/dev') if arduino_serial_prefix in p]
+    for ACM_port in ACM_ports:
+        try:
+            sers.append(serial.Serial(ACM_port, baudrate=baudrate, timeout=timeout))
+            print('Opened {0}'.format(ACM_port))
+        except:
+            pass
+    return sers
+
+def assign_serial_connections(P,sers):
+    P['Arduinos'] = {}
+    for ser in sers:
+        for _ in xrange(100):
+            try:
+                ser_str = ser.readline()
+                print ser_str
+                exec('ser_tuple = list({0})'.format(ser_str))
+                if ser_tuple[0] in ['mse']:
+                    print(d2s('Port',ser.port,'is the MSE:',ser_str))
+                    if 'Keep this up to date...':
+                        print("\tusing 'motor_servo_minimal_3July2018_test.ino' NOT")
+                    P['Arduinos']['MSE'] = ser
+                    P['Arduinos']['SIG'] = P['Arduinos']['MSE']
+                    break
+                elif ser_tuple[0] in ['acc','gyro','head']:
+                    print(d2s('Port',ser.port,'is the IMU:',ser_str))
+                    if 'Keep this up to date...':
+                        print("\tusing 'acc.ino'")
+                    P['Arduinos']['IMU'] = ser
+                    break
+                #################
+                #
+                elif ser_tuple[0] in flex_names:
+                    print(d2s('Port',ser.port,'is the FLEX:',ser_str))
+                    P['Arduinos']['FLEX'] = ser
+                    break
+                #
+                #################
+
+
+            except:
+                pass
+        else:
+            CS_('Unable to identify port {0}'.format(ser.port))
+    print 'Finished scanning serial ports.'
+    if 'MSE' not in P['Arduinos'].keys():
+        spd2s('MSE not found: Is transmitter turned on? Is MSE battery plugged in?')
+    #return Arduinos
+
+
+
+
+
+
