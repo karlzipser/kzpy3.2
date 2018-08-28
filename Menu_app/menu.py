c0565abd1b30548adcc444487bf8082414cd340f
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-08-28 11:49:02 -0700

    gacp

--- Menu_app/menu.py
+++ Menu_app/menu.py
@@ -1,15 +1,14 @@
 from kzpy3.utils3 import *
 
-def clear_screen():
-    print(chr(27) + "[2J")
-
-def menu(Topics,menu_path):
+def menu(Topics,path):
 
     message = False
     choice_number = 0
     Number_name_binding = {}
     Name_number_binding = {}
+
     while choice_number != 1:
+
         try:
             
             clear_screen()
@@ -21,12 +20,17 @@ def menu(Topics,menu_path):
             Number_name_binding[ctr]='add';print d2n(' ',ctr,') ',Number_name_binding[ctr])
 
             first = True
-            for name in Topics.keys():
+            if 'to_expose' in Topics:
+                names_to_use = Topics['to_expose']
+            else:
+                names_to_use = Topics.keys()
+
+            for name in names_to_use:
 
                 ctr += 1
 
                 Number_name_binding[ctr] = name
-                # Number_name_binding[ctr]
+
                 if first:
                     q = ' '
                     first = False
@@ -34,16 +38,13 @@ def menu(Topics,menu_path):
                     q = ''
                 print d2n(q,ctr,') ',name,': ',Topics[name],'  '),
                 cprint(type(Topics[name]).__name__,'grey')
-                #print d2n(ctr,') ',name,': ',dp(Topics[Number_name_binding[ctr]],2))
 
             for n in Number_name_binding.keys():
                 Name_number_binding[Number_name_binding[n]] = n
-            #pprint(Number_name_binding)
-            #pprint(Name_number_binding)
-            #raw_enter()
+
             if message:
-                #print message
-                CS_(message)#
+
+                CS_(message)
 
             choice_number = input('#? ')
 
@@ -55,9 +56,8 @@ def menu(Topics,menu_path):
 
             elif choice_number == Name_number_binding['load']:
                 message = 'file not loaded'
-                files = get_files_sorted_by_mtime(opj(menu_path,'*.pkl'))
-                #print files
-                #print type(files)
+                files = get_files_sorted_by_mtime(opj(path,'*.pkl'))
+
                 files.reverse()
                 filenames = []
                 ctr2 = 1
@@ -66,13 +66,13 @@ def menu(Topics,menu_path):
                         filenames.append(fname(f[0]))
                         print d2n('\t',ctr2,') ',filenames[-1])
                         ctr2 += 1
-                #file_choice_number = input('\t#? ')
+
                 filename = filenames[input('\tload #? ')-1]
-                #Topics_loaded = lo(opj(menu_path,filenames[file_choice_number-1]))
-                Topics_loaded = lo(opj(menu_path,filename))
+
+                Topics_loaded = lo(opj(path,filename))
                 for t in Topics_loaded:
                     Topics[t] = Topics_loaded[t]
-                save_topics(Topics,menu_path)
+                save_topics(Topics,path)
                 message = d2s('loaded',filename)
 
             elif choice_number == Name_number_binding['save']:
@@ -80,7 +80,7 @@ def menu(Topics,menu_path):
                 description = get_safe_name(raw_input('\tshort description #? '))
                 assert(len(description)>0)
                 filename = d2n('Topics.',description,'.pkl')
-                so(Topics,opj(menu_path,filename))
+                so(Topics,opj(path,filename))
                 message = d2s('saved',filename)
 
 
@@ -98,7 +98,7 @@ def menu(Topics,menu_path):
                 message = name+' value not changed'
                 current_val = Topics[name]
                 Topics[name] = input(d2n(name,'(',current_val,') new value > '))
-                save_topics(Topics,menu_path)
+                save_topics(Topics,path)
                 message = 'changed '+name
 
         except Exception as e:
@@ -107,39 +107,45 @@ def menu(Topics,menu_path):
             exec(EXCEPT_STR)
             #raw_enter()
 
-    #clear_screen()
 
-def save_topics(Topics,menu_path):
-    unix(d2n('rm ',opj(menu_path,'ready')))
-    unix(d2n('rm ',opj(menu_path,'Topics.pkl')))
-    so(Topics,opj(menu_path,'Topics.pkl'))
-    unix('touch '+opj(menu_path,'ready'))
+def save_topics(Topics,path):
+    unix(d2n('rm ',opj(path,'ready')))
+    unix(d2n('rm ',opj(path,'Topics.pkl')))
+    so(Topics,opj(path,'Topics.pkl'))
+    unix('touch '+opj(path,'ready'))
 
-def load_Topics(menu_path,first_load=False):
-    if first_load:
-        r = ['ready']
-    else:
-        r = sggo(menu_path,'ready')
+def load_Topics(path,first_load=False):
+    r = sggo(path,'ready')
     if len(r) > 1:
-        CS_('Warning, more than one ready in '+menu_path)
-    if len(r) == 1:
-        Topics = lo(opjh(menu_path,'Topics.pkl'))
-        unix(d2n('rm ',opj(menu_path,'ready')))
+        CS_('Warning, more than one ready in '+path)
+    if len(r) == 1 or first_load:
+        Topics = lo(opjh(path,'Topics.pkl'))
+        if len(r) == 1:
+            unix(d2n('rm ',opj(path,'ready')))
         return Topics
     else:
-        #pd2s('Could not load from',menu_path)
         return None
 
-def load_menu_data(menu_path,Parameters,first_load=False):
+def load_menu_data(path,Parameters,first_load=False):
     timer = Timer(0.5)
     while Parameters['ABORT'] == False:
         if timer.check():
-            Topics = load_Topics(menu_path,first_load)
+            Topics = load_Topics(path,first_load)
             if type(Topics) == dict:
                 for t in Topics.keys():
                     Parameters[t] = Topics[t]
             timer.reset()
         else:
             time.sleep(0.1)
+
+"""
+e.g.
+python kzpy3/Menu_app/menu.py path ~/kzpy3/Train_app/Train_Z1dconvnet0/__local__/
+"""
+
+if __name__ == '__main__':
+    path = Arguments['path']
+    Topics = load_Topics(path,first_load=True)
+    menu(Topics,path)
 
 #EOF
