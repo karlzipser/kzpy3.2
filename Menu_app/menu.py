5cc9bf87f93eb65f634aa4fb175838899f807c03
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-08-30 15:02:51 -0700

    gacp

--- Menu_app/menu.py
+++ Menu_app/menu.py
@@ -180,7 +180,7 @@ python kzpy3/Menu_app/menu.py path ~/kzpy3/Train_app/Train_Z1dconvnet0/__local__
 if __name__ == '__main__':
     path = Arguments['path']
     if 'default' in Arguments.keys():
-        import kzpy3.Train_app.Train_Z1dconvnet0.default_values as default_values
+        import kzpy3.Cars.car_24July2018.nodes.default_values.py as default_values
         Topics = default_values.P
     else:
         try:
