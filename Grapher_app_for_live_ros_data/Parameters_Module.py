a0a7316077346027072e413a0212e6a438892cfa
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-09-24 11:57:37 -0700

    gacp

--- Grapher_app_for_live_ros_data/Parameters_Module.py
+++ Grapher_app_for_live_ros_data/Parameters_Module.py
@@ -10,7 +10,7 @@ P['USE_IMAGES'] = False
 
 flex_names = []
 for fb in ['F']:
-    for lr in ['L','R','C']:
+    for lr in ['L','C','R']:
         for i in [0,1,2,3]:
             flex_names.append(d2n(fb,lr,i))
 #flex_names.append('xan0')
@@ -32,35 +32,42 @@ P[TOPICS] = {
 	'cmd/motor':{maxval:80,		minval:20,		baseline:49.0,	color:(0,255,255)},
 	motor:{maxval:80,		minval:49,		baseline:49.0,	color:(0,0,255)},
 	state:{maxval:6,		minval:-10,		baseline:0,		color:(128,128,128)},
-	encoder:{maxval:4,		minval:0,		baseline:0,		color:(255,128,128)},
-	acc_x:{maxval:10,		minval:-10,		baseline:0,		color:acc_color},
-	acc_y:{maxval:10,		minval:-10,		baseline:0,		color:acc_color},
-	acc_z:{maxval:10-9.80,	minval:-10,		baseline:0,		color:acc_color},
-	gyro_x:{maxval:60,		minval:-60,	baseline:0,		color:gyro_color},
-	gyro_y:{maxval:60,		minval:-60,	baseline:0,		color:gyro_color},
-	gyro_z:{maxval:60,		minval:-60,	baseline:0,		color:gyro_color},
+	encoder:{maxval:4,		minval:0,		baseline:0,		color:(255,255,255)},
+	acc_x:{maxval:3,		minval:-3,		baseline:0,		color:acc_color},
+	acc_y:{maxval:3,		minval:-3,		baseline:0,		color:acc_color},
+	acc_z:{maxval:3-9.80,	minval:-3,		baseline:0,		color:acc_color},
+	gyro_x:{maxval:30,		minval:-30,	baseline:0,		color:gyro_color},
+	gyro_y:{maxval:30,		minval:-30,	baseline:0,		color:gyro_color},
+	gyro_z:{maxval:30,		minval:-30,	baseline:0,		color:gyro_color},
 	gyro_heading_x:{maxval:360,minval:-180,	baseline:0,		color:(255,200,200)},
 	left_ts_deltas:{maxval:0.1,minval:0,	baseline:0,		color:(0,0,255)},
 }
 ctr = 0
 for f in flex_names:
+	if 'C' in f:
+		b = 0
+	elif:
+		'R' in f:
+		b = 128
+	elif 'L' in f:
+		b = 255
 	c = bound_value(20.0*ctr,0,255)
 	#col = (255*z2o(na([c,255-c,127]))).astype(int)
-	col = [c,255-c,127]
+	col = [c,255-c,b]
 	P[TOPICS][f] = {maxval:3000,		minval:-100,	baseline:0,		color:col}
 	ctr += 1
 ##}
 P['topic_keys_sorted'] = [
+	steer,
+	motor,
+	'cmd/motor',
+	'cmd/steer',
 	acc_x,
 	acc_y,
 	acc_z,
 	gyro_x,
 	gyro_y,
 	gyro_z,
-	'cmd/motor',
-	'cmd/steer',
-	steer,
-	motor,
 	#gyro_heading_x,
 	#left_ts_deltas,
 	encoder,
