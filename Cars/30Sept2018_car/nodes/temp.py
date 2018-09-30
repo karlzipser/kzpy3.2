f03d3a9c59b550396696facd5d970ccaa6f60a8e
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-09-30 13:50:49 -0700

    gacp

---
+++ Cars/30Sept2018_car/nodes/temp.py
@@ -0,0 +1,19 @@
+###start
+from kzpy3.utils3 import *
+
+import default_values
+
+Parameters = default_values.Parameters
+
+import kzpy3.Menu_app.menu
+menu_path = Parameters['The menu path.']
+unix('mkdir -p '+menu_path)
+unix(d2s('rm',opj(menu_path,'ready')))
+threading.Thread(target=kzpy3.Menu_app.menu.load_menu_data,args=[menu_path,Parameters]).start()
+
+
+while Parameters['ABORT'] == False:
+	print memory()
+	time.sleep(1)
+
+#EOF
