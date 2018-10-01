073c045b4deef46fb0b134a073a5ae3f3c7773be
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-09-30 17:01:59 -0700

    gacp

--- Cars/30Sept2018_car/nodes/arduino_utils/tactic_rc_controller.py
+++ Cars/30Sept2018_car/nodes/arduino_utils/tactic_rc_controller.py
@@ -181,14 +181,14 @@ def _TACTIC_RC_controller_run_loop(P):
     print 'end _TACTIC_RC_controller_run_loop.'
 
 def get_write_str(servo_pwm,camera_pwm,motor_pwm,P):
-    print 'get_write_str'
+    #print 'get_write_str'
     ws = d2n( '(',
         int(P['servo_pwm_smooth_manual_offset']+servo_pwm),',',
         int(P['camera_pwm_manual_offset']+camera_pwm+5000),',',
         int(motor_pwm+10000),',',
         int(P['Arduinos']['SIG/write']),
         ')' )
-    cs(ws)
+    #cs(ws)
     return ws
 
 def pwm_to_percent(null_pwm,current_pwm,max_pwm,min_pwm):
