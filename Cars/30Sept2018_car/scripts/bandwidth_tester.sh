c0e5d07fc005a4a807b8f833ae84de6f757d4832
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-10-02 11:06:34 -0700

    gacp

--- Cars/30Sept2018_car/scripts/bandwidth_tester.sh
+++ Cars/30Sept2018_car/scripts/bandwidth_tester.sh
@@ -2,4 +2,5 @@ rm ~/Desktop/bw.txt
 rostopic bw /bair_car/zed/left/image_rect_color > ~/Desktop/bw.txt &
 sleep 3
 killall rostopic
-mv bw.txt left_image_rect_color_bw.txt
+mv ~/Desktop/bw.txt ~/Desktop/left_image_rect_color_bw.txt
+rm ~/Desktop/bw.txt
