7269a95d3cf2e940dba3dbb8c248125abb35f918
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-10-02 12:28:20 -0700

    gacp

--- Cars/30Sept2018_car/nodes/arduino_utils/calibration_mode.py
+++ Cars/30Sept2018_car/nodes/arduino_utils/calibration_mode.py
@@ -54,20 +54,21 @@ def _calibrate_run_loop(P):
             print 'left_image_rect_color_bw.txt',zed_left_bw
             print 'os1_node_points_bw.txt',os1_points_bw
             if len(zed_left_bw) == 0:
-                for i in range(10):
+                for i in range(4):
                     CS('No ZED!',exception=True)
                     P['Arduinos']['SOUND'].write("60")
                     time.sleep(2)
                 time.sleep(2)
             else:
-                CS('No LIDAR points!',exception=True)
                 P['Arduinos']['SOUND'].write("30")
                 time.sleep(1)
             if len(os1_points_bw) == 0:
-                    print """P['Arduinos']['SOUND'].write("61")"""
+                for i in range(2):
+                    CS('No LIDAR!',exception=True)
+                    P['Arduinos']['SOUND'].write("61")
                     time.sleep(2)
+                time.sleep(2)
             else:
-                print """P['Arduinos']['SOUND'].write("31")"""
                 P['Arduinos']['SOUND'].write("31")
                 time.sleep(1)
             bandwidth_check_timer.reset()
