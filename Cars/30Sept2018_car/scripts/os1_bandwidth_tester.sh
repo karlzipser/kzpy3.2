3147fc7db1c9b315d2286ea6813161366c56e780
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-10-02 15:20:23 -0700

    gacp

--- Cars/30Sept2018_car/scripts/os1_bandwidth_tester.sh
+++ Cars/30Sept2018_car/scripts/os1_bandwidth_tester.sh
@@ -1,7 +1,7 @@
 rm ~/Desktop/os1_node_points_bw.txt
 rostopic bw /os1_node/points > ~/Desktop/os1_node_points_bw.txt &
 sleep 3
-killall rostopic
+#killall rostopic
 
 
 
--- Cars/30Sept2018_car/scripts/zed_bandwidth_tester.sh
+++ Cars/30Sept2018_car/scripts/zed_bandwidth_tester.sh
@@ -2,7 +2,7 @@
 rm ~/Desktop/left_image_rect_color_bw.txt
 rostopic bw /bair_car/zed/left/image_rect_color > ~/Desktop/left_image_rect_color_bw.txt &
 sleep 3
-killall rostopic
+#killall rostopic
 
 
 
