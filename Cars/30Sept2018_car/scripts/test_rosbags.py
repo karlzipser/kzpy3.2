f0c065000b3177d4875440c21897a7dd31f9aea6
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-10-02 10:04:46 -0700

    gacp

---
+++ Cars/30Sept2018_car/scripts/test_rosbags.py
@@ -0,0 +1,22 @@
+from kzpy3.utils3 import *
+
+current_bag_number = 0
+
+def get_bag_info():
+    try:
+        latest_rosbag_folder = most_recent_file_in_folder(opjm('rosbags'))
+        latest_rosbag = most_recent_file_in_folder(latest_rosbag_folder)
+        bag_num = int(fname(latest_rosbag).split('_')[-1].split('.')[0])
+        bag_size = os.path.getsize(latest_rosbag)
+        bag_size = dp(bag_size/1000000000.)
+        if (bag_num == current_bag_number+1) and bag_size > 1.0:
+        	current_bag_number += 1
+        	return True
+        else:
+        	return False
+    except:
+        return False
+
+while True:
+	time.sleep(1)
+	print get_bag_info()
