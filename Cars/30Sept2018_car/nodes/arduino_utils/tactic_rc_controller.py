69fa6fb0f11cacf09103d342286eeae39e4e5609
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-10-05 16:29:26 -0700

    gacp

--- Cars/30Sept2018_car/nodes/arduino_utils/tactic_rc_controller.py
+++ Cars/30Sept2018_car/nodes/arduino_utils/tactic_rc_controller.py
@@ -166,7 +166,7 @@ def _TACTIC_RC_controller_run_loop(P):
                             P['Arduinos']['MSE'].write(write_str)
             else:
                 write_str = get_write_str(P['servo_pwm_null'],P['servo_pwm_null'],P['motor_pwm_null'],P)
-                P['Arduinos']['MSE'].write(write_str)
+                #P['Arduinos']['MSE'].write(write_str)
                         
             #print write_str
 
