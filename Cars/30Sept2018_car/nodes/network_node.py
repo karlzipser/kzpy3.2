0e880877a4d08be36824f356947facbdc65f475a
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-10-05 11:57:53 -0700

    gacp

--- Cars/30Sept2018_car/nodes/network_node.py
+++ Cars/30Sept2018_car/nodes/network_node.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 from kzpy3.utils3 import *
 
+import rospy
+
+rospy.init_node('network_node',anonymous=True,disable_signals=True)
+
+
 while not rospy.is_shutdown():
     time.sleep(5)
