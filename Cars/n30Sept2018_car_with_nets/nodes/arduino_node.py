f8700562746fd55e7e93928c553ac5411ed1dec5
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-10-10 21:57:06 -0700

    gacp

--- Cars/n30Sept2018_car_with_nets/nodes/arduino_node.py
+++ Cars/n30Sept2018_car_with_nets/nodes/arduino_node.py
@@ -159,7 +159,9 @@ if 'Main loop...':
         Default_values.arduino.default_values.EXIT(restart=False,shutdown=False,kill_ros=True,_file_=__file__)
     except Exception as e:
         CS_(d2s('Main loop exception',e))
+
 P['Arduinos']['SOUND'].write("(22)")
+
 CS_('End arduino_node.py main loop.')
 
 #EOF
