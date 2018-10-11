c6e41ee8524dbd8048656721028c34c74a55cfda
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-10-10 21:34:30 -0700

    gacp

--- Cars/n30Sept2018_car_with_nets/nodes/arduino_node.py
+++ Cars/n30Sept2018_car_with_nets/nodes/arduino_node.py
@@ -112,8 +112,9 @@ def _publish_MSE_data(P):
             P['human_agent_pub'].publish(std_msgs.msg.Int32(1))
         elif P['agent_choice'] == 'network':
             P['human_agent_pub'].publish(std_msgs.msg.Int32(0))
-        else assert False
-        
+        else:
+            assert False
+
         if P['button_number'] == 4:
             drive_mode = 0
         else:
