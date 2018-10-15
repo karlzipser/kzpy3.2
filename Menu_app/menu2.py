7df5e32c0266817ef0e43b1ca267db9542ab896a
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-10-15 12:20:47 -0700

    gacp

--- Menu_app/menu2.py
+++ Menu_app/menu2.py
@@ -191,13 +191,13 @@ def menu2(Topics,path):
 def save_topics(Topics,path):
     for customer in Topics['customers']:
         c = get_safe_name(customer)
-        try:
+        if True:#try:
             os.remove(opj(path,'__local__','ready.'+c))
-        except:
+        else:#except:
             pass
-        try:
+        if True:#try:
             os.remove(opj(path,'__local__','Topics.'+c+'.pkl'))
-        except:
+        else:#except:
             pass
         so(Topics,opjh(path,'__local__','Topics.'+c+'.pkl'))
         text_to_file(opjh(path,'__local__','ready.'+c),'')
