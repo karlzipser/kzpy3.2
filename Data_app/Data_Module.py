fde9a9bfc5bd21cb66e489f2788c8e1fb90d0f49
Karl Zipser <karlzipser@hiMac.local>
2018-10-18 11:59:34 -0700

    gacp

--- Data_app/Data_Module.py
+++ Data_app/Data_Module.py
@@ -95,6 +95,8 @@ def Original_Timestamp_Data(bag_folder_path=None, h5py_path=None):
 					# https://answers.ros.org/question/240491/point_cloud2read_points-and-then/
 					valv = list(sensor_msgs.point_cloud2.read_points(m_[1],skip_nans=True,field_names=("x","y","z")))
 					valv = nv(valv)
+					from kzpy3.vis3 import *
+					figure(1);clf();plt_square();pts_plot(valv);raw_enter()
 				except  Exception as e:
 				    exc_type, exc_obj, exc_tb = sys.exc_info()
 				    file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
