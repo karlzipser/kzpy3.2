7529a9c2e27525f02893439e791103962131d56a
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-11-08 11:20:29 -0800

    gacp

--- Data_app/lidar/older/python_pointclouds6h_durations.py
+++ Data_app/lidar/older/python_pointclouds6h_durations.py
@@ -88,7 +88,7 @@ def points__callback(msg):
     ary1 = A[field_names[1]]
     
     for point in pc2.read_points(msg,skip_nans=False,field_names=field_names):
-        if ctr == 128:
+        if ctr >= 512:
             break
         if ctr2 == 1:
             if ctr3 >= height-1:
