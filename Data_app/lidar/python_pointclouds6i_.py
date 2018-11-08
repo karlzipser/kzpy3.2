8e6633a512ca51672ecec28d9a846c23c5d98557
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-11-08 11:35:37 -0800

    gacp

--- Data_app/lidar/python_pointclouds6i_.py
+++ Data_app/lidar/python_pointclouds6i_.py
@@ -3,7 +3,6 @@
 from kzpy3.vis3 import *
 
 
-
 import sensor_msgs
 from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
@@ -17,6 +16,16 @@ if 'TX1' in Arguments:
         TX1 = False
 
 Output = {}
+
+
+Durations = {}
+durations = ['points__callback','process_callback_data','pointcloud_thread',]
+for d in durations:
+    Durations[d] = {}
+    Durations[d]['timer'] = Timer()
+    Durations[d]['list'] = []
+
+
 
 A = {}
 A['ABORT'] = False
@@ -39,7 +48,7 @@ if TX1:
     field_names = ['reflectivity','intensity']
     width = 1024/2
 else:
-    field_names = ['y','intensity']
+    field_names = ['y','intensity']#,'reflectivity']
     width = 1024
 
 height = 16
@@ -47,7 +56,6 @@ width_times_height = width * height
 
 for f in field_names:
     A[f] = zeros((width,height))
-
 ######################################
 #
 Y = {}
@@ -69,17 +77,21 @@ Y[np.nan] = 0
 #
 ######################################
 
+
 def points__callback(msg):
     global calls
     #calls += 1
-    
+    dname = 'points__callback'
+    Durations[dname]['timer'].reset()
+
     ctr = 0
     ctr2 = 0
     ctr3 = 0
     
     ary0 = A[field_names[0]]
     ary1 = A[field_names[1]]
-    
+    #ary2 = A[field_names[2]]
+
     for point in pc2.read_points(msg,skip_nans=False,field_names=field_names):
 
         if ctr2 == 1:
@@ -95,20 +107,31 @@ def points__callback(msg):
                 ctr = 0
             ary0[ctr,ctr3] = point[0]  
             ary1[ctr,ctr3] = point[1]
+            #ary1[ctr,ctr3] = point[2]
         ctr2 += 1
         if ctr2 >= 4:
             ctr2 = 0
 
+
+
     calls += 1
+
+    Durations[dname]['list'].append(1000.0*Durations[dname]['timer'].time())
+
 
 
 
 
 d2_prev = None
 
+
+
+
 def process_callback_data():
 
     global d2_prev
+    dname = 'process_callback_data'
+    Durations[dname]['timer'].reset()
 
     if TX1:
         b = A['reflectivity']
@@ -118,6 +141,7 @@ def process_callback_data():
         b = A['y']
         y = (b[:,8]*1000).astype(int)
         b2 = A['intensity']
+
 
     indicies = [Y[v] for v in y]
 
@@ -141,17 +165,49 @@ def process_callback_data():
     """
     d2_prev = d2.copy()
 
+    Durations[dname]['list'].append(1000.0*Durations[dname]['timer'].time())
+
 #    return e
     return d2
-
 
 
 net_input_width = 168
 net_input_height = 94
 
+
+
+
+
+
+"""
+w/[0,1,2]
+points__callback = 66.91 ms
+process_callback_data = 4.02 ms
+pointcloud_thread = 46.43 ms
+
+w/[0]
+points__callback : 60.5 ms
+process_callback_data : 3.6 ms
+pointcloud_thread : 8.5 ms
+
+w/skipping processing of data from point cloud in callback
+points__callback : 47.6 ms
+
+
+w/[1]
+points__callback : 61.3 ms
+process_callback_data : 3.4 ms
+pointcloud_thread : 8.0 ms
+
+with three pointcloud fields instead of two as above
+points__callback : 71.4 ms
+
+"""
+
+
 def pointcloud_thread():
     global calls_prev, calls_skip
-
+    dname = 'pointcloud_thread'
     cg("\nStarting pointcloud_thread()\n")
     print_Arguments()
 
@@ -160,7 +216,6 @@ def pointcloud_thread():
         time.sleep(0.01)
 
     if 'time' in A:
-        cr("timer = Timer(A['time'])")
         timer = Timer(A['time'])
 
 
@@ -173,6 +228,8 @@ def pointcloud_thread():
 
                 freq_timer.freq("LIDAR")
         
+                Durations[dname]['timer'].reset()
+
                 #Output['e'] = process_callback_data()
                 Output['d2'] = process_callback_data()
 
@@ -194,7 +251,7 @@ def pointcloud_thread():
                 # can be moved up if width known
 
                 e = []
-                for i in [0,1,2]:
+                for i in [1]:#,1,2]:
                     half_width = half_widths[i]
                     f = cv2.resize(
                             d2[width/2 - half_width:width/2 + half_width, :],
@@ -209,22 +266,28 @@ def pointcloud_thread():
 
 
 
-
+                Durations[dname]['list'].append(1000.0*Durations[dname]['timer'].time())
 
 
                 if A['use_images']:
                     if A['calls_skip'] == calls_skip:
                         calls_skip = 0
-                        mci(
-                            (z2o(Output['e'].transpose(1,0))*255).astype(np.uint8),
-                            scale=2.0,
-                            color_mode=cv2.COLOR_GRAY2BGR,
-                        )
+                        #print type(Output['e'])
+                        #print shape(Output['e'])
+                        for i in [1]:
+                            mci(
+                                (z2o(Output['e'][i])*255).astype(np.uint8),
+                                scale=2.0,
+                                color_mode=cv2.COLOR_GRAY2BGR,
+                                title=d2n("Output['e'][",i,"]")
+                            )
                     else:
                         calls_skip += 1
 
                 calls_prev = calls_
                 
+                
+
             if 'time' in A:
                 if timer.check():
                     cr("timer.check()")
@@ -237,6 +300,7 @@ def pointcloud_thread():
             CS_('Exception!',emphasis=True)
             CS_(d2s(exc_type,file_name,exc_tb.tb_lineno),emphasis=False)
     CA();time.sleep(1)
+    A['ABORT'] = True
     cg('\npointcloud_thread() exiting.\n\n')
 
 
@@ -244,7 +308,16 @@ if __name__ == '__main__':
     rospy.init_node('receive_pointclouds')
     rospy.Subscriber('/os1_node/points', PointCloud2, points__callback)
     threading.Thread(target=pointcloud_thread,args=[]).start()
-
+    show_durations = Timer(5)
+    A['ABORT'] = False
+    while A['ABORT'] == False:
+        if show_durations.check():
+            for d in durations:
+                #figure(d);clf()
+                #hist(Durations[d]['list'])
+                #spause()
+                cg(d,':',dp(np.median(Durations[d]['list']),1),'ms')
+                show_durations.reset()
 
 
 
