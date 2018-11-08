410308c158ed5b9f34fb8c4813f38bd2d879c0e7
Karl Zipser <karlzipser@berkeley.edu>
2018-11-07 18:52:41 -0800

    gacp

---
+++ scratch/Template/default_values.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from kzpy3.utils3 import *
+
+P = {}
+P['ABORT'] = False
+P['test'] = 1
+P['To Expose'] = ['a','b','c']
+P['a'] = 1
+P['b'] = np.pi
+P['c'] = [1,2,3]
+P['d'] = 'ddd'
+
+#EOF
