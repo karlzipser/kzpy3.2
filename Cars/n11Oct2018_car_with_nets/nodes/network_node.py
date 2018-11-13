90c560691591533661a7364a429a3df5369fc2b5
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-11-12 20:24:03 -0800

    gacp

--- Cars/n11Oct2018_car_with_nets/nodes/network_node.py
+++ Cars/n11Oct2018_car_with_nets/nodes/network_node.py
@@ -388,7 +388,7 @@ while not rospy.is_shutdown():
                             #Durations[dname]['list'].append(1000.0*Durations[dname]['timer'].time())
                             #Durations[dname]['timer'].reset()
                             #cr('D')
-                    if False:#'show_net_input' in Arguments:                   
+                    if True:#'show_net_input' in Arguments:                   
                         if True:#'show_net_input' in ppc.A:
                             if True:#ppc.A['show_net_input']:
                                 if even:
