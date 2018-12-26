0f996a3a83d935a077d6170492cb1f1ac49ea9a5
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2018-12-26 10:15:55 -0800

    gacp

--- s7.py
+++ s7.py
@@ -1300,4 +1300,61 @@ A = load_net(opjD('initial_weights.infer'))
 B = load_net(opjD('Networks/net_24Dec2018_12imgs_projections/weights/net_25Dec18_20h12m56s.infer'))
 
 
+
+
+
+######################
+#
+"""
+http://www.jonwitts.co.uk/archives/896
+"""
+
+import sys, termios, tty, os, time
+ 
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+ 
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+ 
+button_delay = 0.2
+ 
+while True:
+    char = getch()
+ 
+    if (char == "p"):
+        print("Stop!")
+        exit(0)
+ 
+    if (char == "a"):
+        print("Left pressed")
+        time.sleep(button_delay)
+ 
+    elif (char == "d"):
+        print("Right pressed")
+        time.sleep(button_delay)
+ 
+    elif (char == "w"):
+        print("Up pressed")
+        time.sleep(button_delay)
+ 
+    elif (char == "s"):
+        print("Down pressed")
+        time.sleep(button_delay)
+ 
+    elif (char == "1"):
+        print("Number 1 pressed")
+        time.sleep(button_delay)
+#
+######################
+
+
+
+
+
 #EOF
