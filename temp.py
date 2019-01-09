18e41ec22b40e705f03fd58892aad5b758f19ea7
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2019-01-09 14:19:57 -0800

    gacp

--- temp.py
+++ temp.py
@@ -1,20 +1,20 @@
+from kzpy3.vis3 import *
 
-
+if 'color_index' not in locals():
+    color_list = ['b','g','r','c','m','y','k',]
+    color_index = 0
 
 ########################################################################################
 ########################################################################################
 ########################################################################################
 ####
-def get_xys(
+def get_float_pixels_from_xys(
             xys = zeros([10,2]),
             img_shape = (100,100,3),
             col = (1,1),
             row = (1,1),
-            box_real = ((0,10.0),(-1.0,1.0)),
-    ):
-
-    #assert len(img_shape) == 2
-
+            box_real = ((0,10.0),(-1.0,1.0))
+):
     xy_img_size = (img_shape[0],img_shape[1])
 
     xy_real_mag = (box_real[0][1] - box_real[0][0],
@@ -25,14 +25,11 @@ def get_xys(
 
     xy_real_to_pixels = (
         xy_img_size[0]/(1.0*xy_real_mag[0])/(1.0*col[1]),
-        xy_img_size[1]/(1.0*xy_real_mag[1])/(1.0*row[1]),
-    )
+        xy_img_size[1]/(1.0*xy_real_mag[1])/(1.0*row[1]))
 
-    xy_pixel_offset = (
-                        col_width*(col[0]-1),
-                        row_height*(row[0]-1),
-    )
-
+    
+    xy_pixel_offset = ( col_width*(col[0]-1),
+                        row_height*(row[0]-1))
 
     xys[:,0] *= xy_real_to_pixels[0]
     xys[:,1] *= xy_real_to_pixels[1]
@@ -41,28 +38,41 @@ def get_xys(
     xys[:,0] += xy_pixel_offset[0]
     xys[:,1] += xy_pixel_offset[1]
 
+    xys[:,1] = xy_img_size[1] - xys[:,1]
+    for i in [0,1]:
+        a = xys[:,i]
+        a[ a<0 ] = 0
+        a[ a>=xy_img_size[i] ] = xy_img_size[i]
+
     return xys
+
+
+def function_pts_plot(img,xys,cs):
+    img[ xys[:,1].astype(int), xys[:,0].astype(int), : ] = cs
+
+def get_blank_image(x,y):
+    return zeros((y,x,3)).astype(np.uint8)
 
 ####
 ########################################################################################
 ########################################################################################
 ########################################################################################
 
+img = get_blank_image(163,369)
 
-
-if 'color_index' not in locals():
-    color_list = ['b','g','r','c','m','y','k',]
-    color_index = 0
-    data = zeros((100,2))
-    data[:,0] = arange(0,100)/10.
+if 'gather data':
+    data = zeros((1000,2))
+    data[:,0] = arange(0,1000)/100.
     data[:,1] = np.sin(data[:,0])
-    img = zeros((200,87,3),np.uint8)
+    cs = z2_255(np.random.randn(len(data),3))
 
-xys = get_xys(data,shape(img))
+xys = get_float_pixels_from_xys(data,shape(img),col=(1,10),row=(1,10))
 
-if color_index >= len(color_list):
-    color_index = 0
-pts_plot(xys,color_list[color_index])
-color_index += 1
+#img[ xys[:,0].astype(int), xys[:,1].astype(int), : ] = cs
+function_pts_plot(img,xys,cs)
+
+mi(img,1);figure(1);spause()
 
 
+
+#EOF
