65dbcc7f278929c9a98e4bd283d6fced3ce85e4f
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2019-01-09 16:48:08 -0800

    gacp

--- scratch/plotter.py
+++ scratch/plotter.py
@@ -6,6 +6,7 @@ if 'color_index' not in locals():
 
 if 'L' not in locals():
     L = h5r("/Users/karlzipser/Desktop/model_car_data_July2018_lrc/locations/local/left_right_center/h5py/Mr_Black_25Jul18_14h44m55s_local_lrc/left_timestamp_metadata_right_ts.h5py")
+
 ########################################################################################
 ########################################################################################
 ########################################################################################
@@ -28,7 +29,6 @@ def get_float_pixels_from_xys(
     xy_real_to_pixels = (
         xy_img_size[0]/(1.0*xy_real_mag[0])/(1.0*col[1]),
         xy_img_size[1]/(1.0*xy_real_mag[1])/(1.0*row[1]))
-
     
     xy_pixel_offset = ( col_width*(col[0]-1),
                         row_height*(row[0]-1))
@@ -49,7 +49,7 @@ def get_float_pixels_from_xys(
     return xys
 
 
-def function_pts_plot(img,xys,cs):
+def img_pts_plot(img,xys,cs):
     img[ xys[:,1].astype(int), xys[:,0].astype(int), : ] = cs
 
 def get_blank_image(x,y):
@@ -62,34 +62,49 @@ def add_image(img,img_new,xy):
 ########################################################################################
 ########################################################################################
 ########################################################################################
-gyro = L['gyro_x'][:]
+DATA = L['steer'][:]
+MX,MN = -2**16,2**16
 for ii in range(0,10000,9):
     if 'fresh image':# == False:
         img = get_blank_image(1000,200)
 
     if 'gather data':
-        #data = zeros((1000,2))
-        #data[:,0] = arange(0,1000)/100.
-        #data[:,1] = np.sin(data[:,0])
+        if False:
+            data = zeros((1000,2))
+            data[:,0] = arange(0,1000)/100.
+            data[:,1] = np.sin(data[:,0])
         data = zeros((1100,2))
         data[:,0] = range(1100)
-        data[:,1] = gyro[ii:ii+1100]
-        #print data[:,1].min(),data[:,1].max()
+        data[:,1] = DATA[ii:ii+1100]
         mn,mx=data[:,1].min(),data[:,1].max()
+        if mn < MN:
+            MN = mn
+        if mx > MX:
+            MX = mx
+        if np.random.randint(100) == 1:
+            MX = 500
+            cr("MX=500")
         cs = z2_255(np.random.randn(len(data),3))
         cs*=0
         for i in range(3):
             cs[:,i] = 255#np.random.randint(255)
 
     for j in [2,3]:
-        #print j,ii
-        xys = get_float_pixels_from_xys(data.copy(),shape(img),col=(1,1),row=(j,4),box=((0,1100.0),(mn,mx)))
-        function_pts_plot(img,xys,cs)
 
-    mci(img)#;spause()
+        xys = get_float_pixels_from_xys(
+            data.copy(),
+            shape(img),
+            col=(1,1),
+            row=(j,4),
+            box=((0,1100.0),
+                (MN,MX)))
 
+        img_pts_plot(img,xys,cs)
 
-# col=(1,1),row=(1,1),box=((0,10.0),(-1.0,1.0)),clr=(128,250,99)
+    mci(img)
+    s = 0.9
+    MX = s*MX + (1-s)*(MX-MN)/2.0
+    MN = s*MN + (1-s)*(MX-MN)/2.0
 
 
 #EOF
