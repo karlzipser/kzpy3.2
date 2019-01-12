48cf91fe2242a8392e20df6e96420cbe758c48e7
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2019-01-12 15:55:57 -0800

    gacp

--- scratch/untitled2.py
+++ scratch/untitled2.py
@@ -8,8 +8,8 @@ if 'color_index' not in locals():
     color_index = 0
 
 if 'L' not in locals():
-    L = h5r(opjD("model_car_data_July2018_lrc/locations/local/left_right_center/h5py",
-        "Mr_Black_25Jul18_14h44m55s_local_lrc/left_timestamp_metadata_right_ts.h5py"))
+    L,O,___ = open_run("Mr_Black_25Jul18_14h44m55s_local_lrc",
+                opjD("model_car_data_July2018_lrc/locations/local/left_right_center/h5py"),want_list=['L','O'])
 
 topics = [
     u'acc_x',
@@ -92,7 +92,10 @@ plot_timer = Tr(2/30)
 message = Tr(3)
 tr = Tr(500)
 Hz=Tr(3)
+
+
 while not tr.c():
+
     #message.message(d2s(P['acc_x/min'],P['acc_x/max']))
     
     if True:#:try:
@@ -101,19 +104,47 @@ while not tr.c():
         if True:#plot_timer.c():
             plot_timer.reset()
             ctr = 0
+            image.place_image_in_image2(img,O['left_image']['vals'][P['index']],row=(2,3),col=(2.05,3)) #########
+            image.place_image_in_image2(img,O['right_image']['vals'][P['index']],row=(2,3),col=(1,3)) #########
+
             for t in topics:
+                cs = na((255,255,255))
+                if '_x' in t:
+                    cs = na((255,0,0))
+                if '_y' in t:
+                    cs = na((0,255,0))
+                    
+                if '_z' in t:
+                    cs = na((0,0,255))
+                    
+                if 'motor' in t:
+                    cs = na((0,0,255))
+                if 'steer' in t:
+                    cs = na((255,0,0))
+                if 'encoder' in t:
+                    cs = na((0,255,255))
+
+                    
                 #print t
                 ctr += 1
-                cs = zeros((len(P[opj(t,'data')]),3))+255
-
+                #cs = zeros((len(P[opj(t,'data')]),3))+255
+                """
+                if 'acc' in t:
+                    r = 1; print r
+                if 'gyro' in t and 'heading' not in t:
+                    r = 2;print r
+                else:
+                """
+                r = len(topics)-ctr+1
                 xys = image.get_float_pixels( #########
                     xys=na(P[opj(t,'data')]),
                     img_shape=shape(img),
                     col=(1,1),
-                    row=(len(topics)-ctr+1,len(topics)),
+                    row=(r,len(topics)),
                     box=((0,len(P[opj(t,'data')])),(P[opj(t,'min')],P[opj(t,'max')]))
                 )
                 image.img_pts_plot(img,xys,cs) #########
+            
             mci(img)
             img *= 0
 
@@ -127,17 +158,9 @@ while not tr.c():
 
 
     """
-    mci(img)
-    img *= 0
-    if imgtimer.check():
-        rr=1+np.random.randint(5)
-        rc=1+np.random.randint(5)
-        imgtimer.reset()
-    image.place_image_in_image2(img,b,row=(rr,5),col=(rc,5)) #########
+
     
-    s = 0.95
-    MX = s*MX + (1-s)*(MX-MN)/2.0
-    MN = s*MN + (1-s)*(MX-MN)/2.0
+
     """
 
 #EOF
