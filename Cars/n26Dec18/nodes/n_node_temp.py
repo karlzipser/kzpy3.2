4a04c30cb05b802463df345aade936b2b816a63f
Karl Zipser <karlzipser@berkeley.edu>
2019-01-19 18:55:22 -0800

    gacp

--- Cars/n26Dec18/nodes/n_node_temp.py
+++ Cars/n26Dec18/nodes/n_node_temp.py
@@ -5,32 +5,29 @@ import camera
 exec(identify_file_str)
 
 if __name__ == '__main__':
-
+    camera.QUIT = False
     rospy.init_node('network_node',anonymous=True,disable_signals=True)
-    
-    Q_list = camera.Q_list
-    
     
     hz = Timer(30)
     wait = Timer()
     wait2 = Timer(30)
-    while not rospy.is_shutdown():
+    while not rospy.is_shutdown() and not camera.QUIT:
         if wait.time() > 10:
             if wait2.check():
                 cr('wait.time() =',int(wait.time()))
                 wait2.reset()
         try:
-            if len(Q_list) > 0:
-                if Q_list[-1].ready:
-                    Q_list[-1].ready = False
-                    Q_list[-1].display(1,1,4)
+            if len(camera.Q_list) > 0:
+                if camera.Q_list[-1].ready:
+                    camera.Q_list[-1].ready = False
+                    camera.Q_list[-1].display(1,1,4)
                     hz.freq(' (main) ')
                     wait.reset()
                     continue
             time.sleep(1./10000.)
     
         except KeyboardInterrupt:
-            QUIT = True
+            camera.QUIT = True
             cr('\n\n*** KeyboardInterrupt ***\n')
             time.sleep(1)
 
@@ -39,9 +36,9 @@ if __name__ == '__main__':
             file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             CS_('Exception!',emphasis=True)
             CS_(d2s(exc_type,file_name,exc_tb.tb_lineno),emphasis=False)
-            #QUIT = True
-            #cr('\n\n*** Exception ***\n')
-            #time.sleep(1)
+            camera.QUIT = True
+            cr('\n\n*** Exception ***\n')
+            time.sleep(1)
 
 #EOF
 
