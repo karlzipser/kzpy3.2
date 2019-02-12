5fea0f9fb082a65c0a06b67188c5d72d954fed63
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2019-02-11 16:17:21 -0800

    gacp

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rla &
+rm -r ~/.ros/log/* &
+roslaunch bair_car bair_car.launch use_zed:=true record:=true &
 python kzpy3/Cars/f6Feb19/nodes/arduino_node.py &
 python kzpy3/Cars/f6Feb19/nodes/network_node.py &
 python kzpy3/Cars/f6Feb19/nodes/flex_network_node.py &
