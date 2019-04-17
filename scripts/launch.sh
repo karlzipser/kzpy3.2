7a0819bd788f1d0ef4685aa0b5c09aa039eec367
Karl Zipser <karlzipser@berkeley.edu>
2019-04-17 11:40:10 -0700

    gacp

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#python kzpy3/Cars/f6Feb19/nodes/arduino_node.py &
+python kzpy3/Cars/f6Feb19/nodes/arduino_node.py &
 
-#python kzpy3/Cars/f6Feb19/nodes/network_node.py &
+python kzpy3/Cars/f6Feb19/nodes/network_node.py &
 
-#python kzpy3/Cars/f6Feb19/nodes/flex_network_node.py &
+python kzpy3/Cars/f6Feb19/nodes/flex_network_node.py &
 
-#python kzpy3/Cars/f6Feb19/nodes/control_node.py &
+python kzpy3/Cars/f6Feb19/nodes/control_node.py &
 
 
-python kzpy3/Cars/a2Apr19_16April2019/nodes/arduino_node.py &
+#python kzpy3/Cars/a2Apr19_16April2019/nodes/arduino_node.py &
 
-python kzpy3/Cars/a2Apr19_16April2019/nodes/network_node.py &
+#python kzpy3/Cars/a2Apr19_16April2019/nodes/network_node.py &
 
-python kzpy3/Cars/a2Apr19_16April2019/nodes/flex_network_node.py &
+#python kzpy3/Cars/a2Apr19_16April2019/nodes/flex_network_node.py &
 
-python kzpy3/Cars/a2Apr19_16April2019/nodes/control_node.py &
+#python kzpy3/Cars/a2Apr19_16April2019/nodes/control_node.py &
