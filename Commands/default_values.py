88d69f29d5294ef1326f821d17dca6eba1f1bada
Karl Zipser <karlzipser@berkeley.edu>
2019-04-19 09:46:37 -0700

    gacp

--- Commands/default_values.py
+++ Commands/default_values.py
@@ -11,6 +11,8 @@ _ = {
                 'control_node':'kzpy3/Cars/a2Apr19_16April2019/nodes/control_node.py',
                 'menu2':'python kzpy3/Menu_app/menu2.py path kzpy3/Cars/a2Apr19_16April2019/nodes dic P', 
                 'VT_net2__5April2019_2__18April2019_for_speed':'python kzpy3/VT_net2__5April2019_2__18April2019_for_speed/main.py',
+                'ldr_img, show_image_from_ros':'show_image_from_ros.py topic ldr_img',
+                'left, show_image_from_ros':'show_image_from_ros.py',
             },
 
         },
