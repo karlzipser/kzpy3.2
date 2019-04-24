0886b476dc2d92690cebf7f3cd2973d4e67da245
Karl Zipser <karlzipser@berkeley.edu>
2019-04-23 19:06:36 -0700

    gacp

--- Cars/a2Apr19_16April2019/nodes/network_node.py
+++ Cars/a2Apr19_16April2019/nodes/network_node.py
@@ -336,6 +336,7 @@ if __name__ == '__main__':
                                 #print("np.abs(N['cmd/camera']-49) > 5")#camera_motion_ldr_modulator_notification_Timer.message("np.abs(N['cmd/camera']-49) > 5")
 
                             elif not camera_motion_ldr_modulator_timer.check():
+                                pass
                                 #print('not camera_motion_ldr_modulator_timer.check()')#camera_motion_ldr_modulator_notification_Timer.message('not camera_motion_ldr_modulator_timer.check()')
 
                             else:
