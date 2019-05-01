1a2a5c7f5dae30285016f553435ff9eec79013c5
Karl Zipser <karlzipser@berkeley.edu>
2019-05-01 10:35:29 -0700

    gacp

--- VT_net2__5April2019_2__18April2019_for_speed/main.py
+++ VT_net2__5April2019_2__18April2019_for_speed/main.py
@@ -102,13 +102,13 @@ if __name__ == '__main__':
 
             camera_heading = (S['cmd/camera']-49) * _['cmd_camera_to_camera_heading_cooeficient']
 
-            if np.abs(S['delta cmd/camera']) < 10:
+            if np.abs(S['delta cmd/camera']) < 3:
                 headings['left'] =      S['headings_left'] + camera_heading
                 headings['direct'] =    S['headings_direct'] + camera_heading
                 headings['right'] =     S['headings_right'] + camera_heading
             else:
                 pass
-            print S['delta cmd/camera'],S['cmd/camera']
+                print S['delta cmd/camera'],S['cmd/camera']
             encoders['left'] =      S['encoders_left']
             encoders['direct'] =    S['encoders_direct']
             encoders['right'] =     S['encoders_right']
