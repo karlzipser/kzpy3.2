346ce1e650e2694da3a3bc8787c9796708c61abe
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2019-05-23 17:27:19 -0700

    gacp

--- VT_landmark__23May2019/main.py
+++ VT_landmark__23May2019/main.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
                     x_origin_in_pixels=None,
                     y_origin_in_pixels=None,
                     use_CV2_plot=True,
-                    use_maplotlib=True,
+                    use_maplotlib=False,
                     do_print=False,
                     clear=True
                 )
