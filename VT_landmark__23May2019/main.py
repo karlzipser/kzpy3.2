387f9d8fad41e84835b47d4cbc6a4b3d4b7c3288
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2019-05-23 17:58:55 -0700

    gacp

--- VT_landmark__23May2019/main.py
+++ VT_landmark__23May2019/main.py
@@ -134,7 +134,7 @@ if __name__ == '__main__':
                 Path_pts2D['show'](
                     height_in_pixels=200,
                     width_in_pixels=200,
-                    pixels_per_unit=60./4./4.,
+                    pixels_per_unit=_['pixels_per_unit'],
                     x_origin_in_pixels=None,
                     y_origin_in_pixels=None,
                     use_CV2_plot=True,
