c15201a71a86a259bdda03390422915752aa2e20
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2019-05-30 19:54:44 -0700

    gacp

--- Commands/Dic_menu.py
+++ Commands/Dic_menu.py
@@ -19,8 +19,9 @@ def Default_Values(Q,project_path,parent_keys=[]):
     def function_save():
         save_C(D['Q'],D['project_path'])
     def function_load():
+        #cm(-1,ra=1)
         load_C(D['Q'],D['project_path'])
-        add_keys(D,['Q']+D['Q'])
+        add_keys(D,D['Q'])
     def function_up():
         if len(D['current_keys']) > 0:
             D['current_keys'].pop()
@@ -51,9 +52,10 @@ def Default_Values(Q,project_path,parent_keys=[]):
         return show_menu(key_access(D,D['current_keys']),message)
 
     def function_menu():
-        #D['load']()
+        
         message = ''
         while True:
+            D['load']()
             items = D['show'](message)
             while True:
                 R = choice(items)
@@ -144,15 +146,20 @@ def add_keys(D,E,keys=[]):
 
 
 def load_C(C,project_path,name='default_values'):
+    #cm(0,ra=1)
     if len(sggo(opj(project_path,'__local__','ready'))) == 0:
         return False
     if len(sggo(opj(project_path,'__local__',name+'.writing.pkl'))) > 0:
         return False
     if len(sggo(opj(project_path,'__local__',name+'.pkl'))) == 0:
         return False
+    #cm(1,ra=1)
     try:
+        #cm(2)
         D = lo(opj(project_path,'__local__',name+'.pkl'))
+        #cm(3)
     except:
+        #cm(4)
         return False
     if len(sggo(opj(project_path,'__local__','ready'))) == 0:
         return False
@@ -194,8 +201,8 @@ def is_meta_key(k):
     else:
         return True
 
-#def clear_screen():
-#    cr("\nclear screen\n")
+def clear_screen():
+    cr("\nclear screen\n")
 
 def show_menu(C,message):
     clear_screen()
@@ -353,103 +360,6 @@ def menu_python():
         file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         message = d2s('Exception!',exc_type,file_name,exc_tb.tb_lineno)
         return message
-
-    """
-    if choice == 0:
-        return ('up')
-
-    elif type(C_[key_choice]) == dict:
-        ('down',key_choice)
-
-    else:
-        cmd_mode = C_[key_choice][0]
-        cmd_str = C_[key_choice][1]
-        
-        if cmd_mode == 'bash':
-            yes_no = 'y'#raw_input(d2s(cmd_str,' ([y]/n) '))
-
-            if yes_no == 'y' or yes_no == '':
-
-                try:
-                    if cmd_str[0] != '@':
-                        os.system(cmd_str)
-                    else:
-                        os.system("gnome-terminal --geometry 50x30+100+200 -x "+cmd_str[1:])
-                        raw_enter()
-                except Exception as e:
-                    exc_type, exc_obj, exc_tb = sys.exc_info()
-                    file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                    CS_('Exception!',emphasis=True)
-                    CS_(d2s(exc_type,file_name,exc_tb.tb_lineno),emphasis=False)
-                    raw_enter()
-
-        elif cmd_mode == 'set value':
-            try:
-                if type(C_[key_choice][1]) == bool:
-                    value = C_[key_choice][1]
-                    yes_no = raw_input(d2n("Toggle '",key_choice,"'? ([y]/n) "))
-                    if yes_no == 'y' or yes_no == '':
-                        value = not value
-                        message = d2n("(set '",key_choice,"' to ",value,')')
-                    if value:
-                        message = d2n("('",key_choice,"' unchanged)")
-                else:
-                    value = input(d2n("Enter value for '",key_choice,"': "))
-                    if type(value) != type(C_[key_choice][1]):
-                        message = d2n("!!! type(",value,") != type(",C_[key_choice][1],") !!!")
-                        return message
-                    message = d2n("(set '",key_choice,"' to ",value,')')
-                print key_choice,value,C_[key_choice]
-                C_[key_choice] = (cmd_mode,value,'edited')
-                save_C(C)
-                time.sleep(1/8.)
-            except Exception as e:
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                CS_('Exception!',emphasis=True)
-                CS_(d2s(exc_type,file_name,exc_tb.tb_lineno),emphasis=False)
-                raw_enter()
-
-        elif cmd_mode == 'active':
-            
-                A = Dic_Loader(C_[key_choice][1])
-                while True:
-                    try:
-                        if A['load']():
-                            clear_screen()
-                            for k in sorted(A['Dic'].keys()):
-                                cw(gr,str(k)+')',yl,A['Dic'][k])
-                            cw(lb,time_str('Pretty'))
-                    except KeyboardInterrupt:
-                        cr('*** KeyboardInterrupt ***')
-                        break
-                    except Exception as e:
-                        exc_type, exc_obj, exc_tb = sys.exc_info()
-                        file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                        CS_('Exception!',emphasis=True)
-                        CS_(d2s(exc_type,file_name,exc_tb.tb_lineno),emphasis=False)
-
-        elif cmd_mode == 'python':
-            code = raw_input(d2n("Enter python code: "))
-            d = code.split(';')
-            if 'print' not in d[-1]:
-                d[-1]=d2s('print(',d[-1],')')
-            code = ';'.join(d)
-            exec(code)
-            raw_enter()
-
-        elif cmd_mode == 'path':
-            c = file_to_text(C_[key_choice][1])
-            C_[key_choice][1]
-            exec(c)
-            normal_dic_to_dic_in_Command_form(Q)
-            assign_path(Q,C_[key_choice][1])
-            Q['_path_'] = ('const',C_[key_choice][1])
-            C_[key_choice] = Q
-            save_C(C)
-            key_list.append(key_choice)
-    """
-
 
 
 
