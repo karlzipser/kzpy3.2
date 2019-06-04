23f99ee13fce026c797fe50becb7eb6735fe544a
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2019-06-04 12:34:18 -0700

    gacp

--- VT_net2__1June2019/main.py
+++ VT_net2__1June2019/main.py
@@ -284,7 +284,7 @@ if __name__ == '__main__':
                     grid=True,
                     scale=1.0,
                     clear=True,
-                    code=0,
+                    code=-1,
                     color=(255,255,255), #(0,127,255),
                     show=True,
                     background_image=S['left_image'],
