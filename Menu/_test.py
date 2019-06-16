60781796b3fd35925e5c27308236e01e7ca15d46
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2019-06-16 08:56:47 -0700

    gacp

--- Menu/_test.py
+++ Menu/_test.py
@@ -1,23 +1,17 @@
 from kzpy3.vis3 import *
 import Menu
 
-dic_project_path = opjk('Menu','A')
-
-Dics = {}
-
 Q = Menu.start_Dic(
-    dic_project_path=dic_project_path,
-    Dics=Dics,
+    dic_project_path=opjk('Menu','A'),
+    Dics={},
     Arguments={
         'menu':False,
         'read_only':False,
     }
 )
 
-P = Q['Q']
-
 while True:
     Q['load']()
-    cg(P['tests'],ra=1)
+    cg(Q['Q']['tests']['a'],Q['Q']['tests']['b'],ra=1,sf=0)
 
 #EOF
