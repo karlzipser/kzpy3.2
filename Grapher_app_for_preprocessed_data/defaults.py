b81290224c72a2e29fd63ba88f1f4e28ce402b43
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2019-06-22 11:49:04 -0700

    gacp

--- Grapher_app_for_preprocessed_data/defaults.py
+++ Grapher_app_for_preprocessed_data/defaults.py
@@ -61,8 +61,8 @@ Q = {
 		'k':("P[START_TIME] -= 100.0*dt_; P[END_TIME] += 100.0*dt_",
 			"Time scale in"),
 		' ':("""
-	'VERTICAL_LINE_PROPORTION'=0.5
-	'START_TIME','END_TIME' = 'START_TIME_INIT','END_TIME_INIT'
+	VERTICAL_LINE_PROPORTION=0.5
+	P[START_TIME],P[END_TIME] = P[START_TIME_INIT],P[END_TIME_INIT]
 	ymin_,ymax_ = ymin_init_,ymax_init_
 	show_menuv = True""",
 			"Reset"),
