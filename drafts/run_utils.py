d46b73031ca501cdbcc7cac6cf7c839b0b007be0
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2019-10-14 13:33:15 -0700

    gacp

--- drafts/run_utils.py
+++ drafts/run_utils.py
@@ -8,3 +8,10 @@ def bagfile_has_topic(bagfile,topic):
 		])
 
 	return topic in ouput_txt
+
+
+def h5py_file_has_topic(h5py_file,topic):
+	F = h5r(h5py_file)
+	answer = topic in F.keys()
+	F.close()
+	return answer
