0c4425da255320fa907f763186a0601b267c387b
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2019-10-14 19:23:50 -0700

    gacp

--- drafts/setup_run_links.py
+++ drafts/setup_run_links.py
@@ -11,9 +11,9 @@ M = find_files_recursively(src,'*.bag',FILES_ONLY=True)
 for k in M['paths'].keys():
 	run_name = k.split('/')[-1]
 	if run_name not in runs_with_points:
-		print run_name,opj(M['src'],k)
-		os.system(d2s('ln -s',opj(M['src'],k),run_name))
+		#print run_name,opj(M['src'],k)
+		print(d2s('ln -s',opj(M['src'],k),run_name))
 	else:
-		print run_name,False
+		#print run_name,False
 
 
