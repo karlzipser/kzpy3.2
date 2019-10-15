71356fc089c9e3cfeaf2a54c3c329bd2cccdc0b3
Karl Zipser <karlzipser@berkeley.edu>
2019-10-15 06:18:54 -0700

    gacp

--- drafts/setup_run_links.py
+++ drafts/setup_run_links.py
@@ -10,7 +10,7 @@ M = find_files_recursively(src,'*.bag',FILES_ONLY=True)
 
 for k in M['paths'].keys():
 	run_name = k.split('/')[-1]
-	if run_name not in runs_with_points:
+	if run_name in runs_with_points:
 		#print run_name,opj(M['src'],k)
 		os.system(d2s('ln -s',opj(M['src'],k),opj(dst,run_name)))
 	else:
@@ -18,3 +18,10 @@ for k in M['paths'].keys():
 		#print run_name,False
 
 
+"""
+python kzpy3/drafts/setup_run_links.py --src /media/karlzipser/A3 --dst ~/Desktop/runs_with_points__links
+
+Data src ~/Desktop/runs_with_points__links dst ~/Desktop/runs_with_points__h5py
+"""
+
+#EOF
