2b3a0e09eb853e8c30fa596b046b9fbf65cea3c0
Karl Zipser <karlzipser@berkeley.edu>
2019-10-15 13:38:47 -0700

    gacp

--- drafts/temp.py
+++ drafts/temp.py
@@ -1,5 +1,6 @@
 #,a
-
+CA()
+try_to_close(D)
 D = h5r(opjD('Depth_images','tegra-ubuntu_01Dec18_16h03m42s.Depth_image.h5py'))
 
 
@@ -9,14 +10,14 @@ for i in range(shape(D['depth'])[0]):
 	a = D['depth'][i,:,:]/5.0*255
 	a[a>255] = 255
 	a[a<0] = 0
-	image[:,:,0] = a
+	#image[:,:,0] = a
 
 	a = D['intensity'][i,:,:]/1700.0*255
 	a[a>255] = 255
 	a[a<0] = 0
 	image[:,:,1] = a
 
-	a = D['reflectance'][i,:,:]
+	a = D['reflectivity'][i,:,:]
 	a[a<500] = 500
 	a[a>5000] = 5000
 	a = 255 * (a-500) / 4500.
@@ -25,5 +26,4 @@ for i in range(shape(D['depth'])[0]):
 	image[:,:,2] = a
 	mci(image,scale=3.0)
 	time.sleep(0.25)
-
 #,b
