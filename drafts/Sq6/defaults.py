66866b2bd07c24b3a2770631bdbd502b7d46bdb1
Karl Zipser <karlzipser@hiMac.local>
2020-01-04 07:38:57 -0800

    gacp

--- drafts/Sq6/defaults.py
+++ drafts/Sq6/defaults.py
@@ -7,7 +7,7 @@ Q = {
         'graphics_timer_time':1,
         'abort':False,
         'graphics_ylim':[-0.1,1.],
-        'meo_num':64,
+        'meo_num':8,
     },
     'network_parameters': {
     },
