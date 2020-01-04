7e16f6bcf33048de8c8b35407694bc9ce7a6db7b
Karl Zipser <karlzipser@Karls-MacBook-2.local>
2020-01-04 09:44:00 -0800

    gacp

--- drafts/Sq6/networks/condecon.py
+++ drafts/Sq6/networks/condecon.py
@@ -7,44 +7,35 @@ class conv_deconv(nn.Module):
 
     def __init__(self):
         super(conv_deconv,self).__init__()
-        #Convolution 1
+
         self.conv1=nn.Conv2d(in_channels=3,out_channels=16, kernel_size=4,stride=1, padding=0)
         nn.init.xavier_uniform(self.conv1.weight) #Xaviers Initialisation
         self.swish1= nn.ReLU()
 
-        #Max Pool 1
         self.maxpool1= nn.MaxPool2d(kernel_size=2,return_indices=True)
 
-        #Convolution 2
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5)
         nn.init.xavier_uniform(self.conv2.weight)
         self.swish2 = nn.ReLU()
 
-        #Max Pool 2
         self.maxpool2 = nn.MaxPool2d(kernel_size=2,return_indices=True)
 
-        #Convolution 3
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
         nn.init.xavier_uniform(self.conv3.weight)
         self.swish3 = nn.ReLU()
 
-        #De Convolution 1
         self.deconv1=nn.ConvTranspose2d(in_channels=64,out_channels=32,kernel_size=3)
         nn.init.xavier_uniform(self.deconv1.weight)
         self.swish4=nn.ReLU()
 
-        #Max UnPool 1
         self.maxunpool1=nn.MaxUnpool2d(kernel_size=2)
 
-        #De Convolution 2
         self.deconv2=nn.ConvTranspose2d(in_channels=32,out_channels=16,kernel_size=5)
         nn.init.xavier_uniform(self.deconv2.weight)
         self.swish5=nn.ReLU()
 
-        #Max UnPool 2
         self.maxunpool2=nn.MaxUnpool2d(kernel_size=2)
 
-        #DeConvolution 3
         self.deconv3=nn.ConvTranspose2d(in_channels=16,out_channels=3,kernel_size=4)
         nn.init.xavier_uniform(self.deconv3.weight)
         self.swish6=nn.ReLU()
