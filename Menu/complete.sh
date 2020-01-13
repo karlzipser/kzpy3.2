95fe7f31d96d984748a03a2ca79b010a95d661f2
Karl Zipser <karlzipser@berkeley.edu>
2020-01-13 09:15:51 -0800

    gacp

--- Menu/complete.sh
+++ Menu/complete.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-complete -W "--menu --read_only --help --path --load --load_timer_time" menu
+# https://stackoverflow.com/questions/45778261/custom-bash-autocomplete-with-file-path-completion
+complete -o default -W "--menu --read_only --help --path --load --load_timer_time" menu
