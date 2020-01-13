2fa20e40252169fcbf53272dda5683c40586e3b5
Karl Zipser <karlzipser@berkeley.edu>
2020-01-13 10:49:35 -0800

    gacp

--- utils3.py
+++ utils3.py
@@ -10,7 +10,7 @@ from utils.more import *
 log_name = get_safe_name(sys.argv[0].replace(opjk(),'')+'.txt')
 log_strs = []
 log_strs.append(' '.join(sys.argv).replace(opjk(),'')+' # '+time_str('Pretty'))
-os.system('mkdir -p '+opjk('__local__'))
-list_of_strings_to_txt_file(opjk('__local__',log_name),log_strs,write_mode="a")
+os.system('mkdir -p '+opjk('__local__','logs'))
+list_of_strings_to_txt_file(opjk('__local__','logs',log_name),log_strs,write_mode="a")
 
 #EOF
