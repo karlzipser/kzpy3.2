06654d92ce122b65277fda6724fdbecfca56e9f5
Karl Zipser <karlzipser@berkeley.edu>
2020-03-23 11:17:22 -0700

    gacp

--- drafts/nf0.py
+++ drafts/nf0.py
@@ -1,16 +1,6 @@
 
 #####################
 # pgp;more kzpy3/drafts/nf0.py
-s = 'Hi Fran!'
-t = 'Can you read this?'
-print s + ' ' + t
-
-s = 1
-t = 2
-print s + ' ' + t
-
-print s + t
-#
 ######################
 #
 D = {}
