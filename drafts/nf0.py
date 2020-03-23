41264b44bbb9fc71d459f2076e7aeda9d0a93d8b
Karl Zipser <karlzipser@berkeley.edu>
2020-03-23 11:28:01 -0700

    gacp

--- drafts/nf0.py
+++ drafts/nf0.py
@@ -1,29 +1,17 @@
 
 #####################
-# pgp;more kzpy3/drafts/nf0.py
-######################
 #
-D = {}
-D['a'] = 1
-D['b'] = 2
+def g(Q):
+	classes = Q.keys()
+	for c in classes:
+		Q[c] = Q[c] + 1
 
-E = {
-	'a':1,
-	'b':2,
-}
 
-print D
-print E
-#
-########################
-#
-def f(a):
-	b = a+1
-	print a,b
-	return a+1
 
-f(0)
+print g(D)
 
-print f(0)
+g(D)
+
+print g(D)
 #
 ######################
