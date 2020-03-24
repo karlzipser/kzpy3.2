e7bf6ac3a14de32839996071da3c5d7a60a90e5f
Karl Zipser <karlzipser@berkeley.edu>
2020-03-23 18:40:35 -0700

    gacp

--- Sqclass/defaults.py
+++ Sqclass/defaults.py
@@ -3,9 +3,9 @@ from kzpy3.utils3 import *
 
 
 Q = {
-    'num_clusters_to_use':10,
+    'num_clusters_to_use':1,
     's':0.5,
-    'num_clusters_to_average':5,
+    'num_clusters_to_average':1,
 }
 
 
