4883afea87987ab725b9d782f2bc4a187e84dd0a
Karl Zipser <karlzipser@berkeley.edu>
2020-04-05 09:40:53 -0700

    gacp

--- Learn/main.py
+++ Learn/main.py
@@ -55,7 +55,7 @@ Net_strs = {
     'pts2d1' : """
         Learn 
             --type ConDecon_Fire_FS,Fire3,pts2d1
-            --resume False 
+            --resume True 
             --batch_size 1
             --save_timer_time 300 
             --target_offset 0 
