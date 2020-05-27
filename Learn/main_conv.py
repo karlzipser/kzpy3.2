25cde606cace1b889c3d9230ea7f2f3d597cc9ef
Karl Zipser <karlzipser@berkeley.edu>
2020-05-27 16:18:24 -0700

    gacp

--- Learn/main_conv.py
+++ Learn/main_conv.py
@@ -172,6 +172,7 @@ def main6():
 
         else:
             s = 1.0
+            cg(GENERATOR.A['output_2'].size(),GENERATOR.A['target'].size())
             GENERATOR.loss = s*GENERATOR.criterion(GENERATOR.A['output_2'],GENERATOR.A['target'])
 
         if Nets[n]['P']['backwards']:
