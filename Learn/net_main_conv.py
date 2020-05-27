4b330778fbbd3489756c80c426958954f9e7c1cd
Karl Zipser <karlzipser@berkeley.edu>
2020-05-27 11:52:52 -0700

    gacp

--- Learn/net_main_conv.py
+++ Learn/net_main_conv.py
@@ -44,7 +44,7 @@ def Net_Main(M=False,sys_str=False,Arguments_=False,P_Runs_saved=None):
         'height':94,
         'show_graphics':True,
     }
-    P['type']= ['ConDecon_Fire_FS','Fire3'] #!!!!!!!!!!!!!!!!!
+    #P['type']= ['conv0'] #!!!!!!!!!!!!!!!!!
     if sys_str != False:
         Arguments_ = parse_to_Arguments(sys_str)
     else:
