de0f4ef377e3a5a27eaa76aa7583f4be5602f0e3
Karl Zipser <karlzipser@berkeley.edu>
2020-05-27 15:52:26 -0700

    gacp

--- Learn/networks/Conv.py
+++ Learn/networks/Conv.py
@@ -151,9 +151,10 @@ class Conv(Net):
 
         
         self.A['output_2'] = self.output_2(x)
+        clp('x',x.size(),'output_2',self.A['output_2'].size())
         self.A['output_2'] = self.A['output_2'].view(self.A['output_2'].size(0), -1)
         
-        clp('x',x.size(),'output_2',self.A['output_2'].size())
+
 
 
 
