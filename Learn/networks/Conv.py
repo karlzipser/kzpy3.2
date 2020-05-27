5e507e94c472d1e0ae59728611b78ddac27459ca
Karl Zipser <karlzipser@berkeley.edu>
2020-05-27 15:20:01 -0700

    gacp

--- Learn/networks/Conv.py
+++ Learn/networks/Conv.py
@@ -44,6 +44,37 @@ class Fire(nn.Module):
 
 
 
+class Smoke(nn.Module):
+    def __init__(
+        self,
+        inplanes,
+        squeeze_planes,
+        expand1x1_planes,
+        expand3x3_planes,
+        name='',
+        A=False
+    ):
+        super(Smoke, self).__init__()
+        self.A = A
+        self.name = name
+        self.inplanes = inplanes
+        self.squeeze = nn.ConvTranspose2d(inplanes, squeeze_planes, kernel_size=1)
+        self.squeeze_activation = nn.ReLU(inplace=True)
+        self.expand1x1 = nn.ConvTranspose2d(squeeze_planes, expand1x1_planes,
+                                   kernel_size=1)
+        self.expand1x1_activation = nn.ReLU(inplace=True)
+        self.expand3x3 = nn.ConvTranspose2d(squeeze_planes, expand3x3_planes,
+                                   kernel_size=3, padding=1)
+        self.expand3x3_activation = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.squeeze_activation(self.squeeze(x))
+        if type(self.A) != type(False):
+            self.A[d2p(self.name,'squeeze_activation')] = x
+        return torch.cat([
+            self.expand1x1_activation(self.expand1x1(x)),
+            self.expand3x3_activation(self.expand3x3(x))
+        ], 1)
 
 aa = 8
 a = 16
