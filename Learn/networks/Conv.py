0f0879dc9a2116ba1d2037e2b31dcd9a24a088aa
Karl Zipser <karlzipser@berkeley.edu>
2020-05-28 11:20:47 -0700

    gacp

--- Learn/networks/Conv.py
+++ Learn/networks/Conv.py
@@ -46,13 +46,17 @@ class Fire(nn.Module):
     def describe(self):
         kprint(self.D,title=self.D['name'],ignore_keys=['name'],r=1)
     def forward(self, x):
+        cg(x.size())
         x = self.squeeze_activation(self.squeeze(x))
         if type(self.A) != type(False):
             self.A[d2p(self.name,'squeeze_activation')] = x
-        return torch.cat([
+        x = torch.cat([
             self.expand1x1_activation(self.expand1x1(x)),
             self.expand3x3_activation(self.expand3x3(x))
         ], 1)
+        cr(x.size(),ra=1)
+
+        return x
 
 
 
