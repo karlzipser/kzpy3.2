f5ed0faa175bb931140138901aa54f9ce2f235fc
Karl Zipser <karlzipser@berkeley.edu>
2020-05-28 14:23:25 -0700

    gacp

--- Learn/networks/Conv.py
+++ Learn/networks/Conv.py
@@ -198,7 +198,7 @@ class MyConv(Net):
 
         self.final_conv = nn.Conv2d(512, P['NUM_OUTPUTS'], kernel_size=1)
         self.drop = nn.Dropout(p=0.5)
-        self.avg = nn.AvgPool2d(kernel_size=2, stride=6)
+        self.avg = nn.AvgPool2d(kernel_size=3, stride=6)
         
 
 
