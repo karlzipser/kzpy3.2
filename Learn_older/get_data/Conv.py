b95891ad38bd16369de8fbe5ef8e33031a4daff9
Karl Zipser <karlzipser@berkeley.edu>
2020-05-28 20:56:07 -0700

    gacp

---
+++ Learn_older/get_data/Conv.py
@@ -0,0 +1,193 @@
+from kzpy3.vis3 import *
+exec(identify_file_str)
+
+# python kzpy3/drafts/Sq7/main.py --RESUME 1 --NET_TYPE ConDecon_test2 --NET_TYPE_SUFFIX 84x47
+#WIDTH,HEIGHT = 168/2,94/2
+
+#from kzpy3.Learn.get_data.runs import All_runs
+
+from runs import All_runs
+
+def setup(P):
+
+    Runs = {}
+
+    for path in sggo(opjD('Data/outer_contours/rotated','*.h5py')):
+        r = fname(path).split('.')[0]
+        if r in All_runs[P['runs']]:
+            if os.path.getsize(path) > 0:
+                if time.time() - os.path.getmtime(path) > 60:
+                    Runs[r] = {}
+                    for k in [
+                        'rotated',
+                        'original_timestamp_data',
+                        'flip_images',
+                        'left_timestamp_metadata_right_ts',
+                        'button_number',
+                        'encoder',
+                    ]:
+                        Runs[r][k] = None
+                    Runs[r]['rotated'] = h5r(path)
+
+    P['good_indicies'] = []
+
+    for r in Runs.keys():
+
+        H = find_files_recursively(opjD('Data'),r,DIRS_ONLY=True)
+
+        if len(H['paths']) == 0:
+            cr(r,'not found.')
+            continue
+        else:
+            cg(r,'found.')
+
+        for k in ['original_timestamp_data','flip_images','left_timestamp_metadata_right_ts']:
+            Runs[r][k] = h5r(opj(opjD('Data'),H['paths'].keys()[0],r,k+'.h5py'))
+
+        if len(sggo(opjD('temp.pkl'))) == 0:
+            for i in rlen(Runs[r]['rotated']['valid']):
+                if Runs[r]['rotated']['valid'][i]:
+                    P['good_indicies'].append( (r,i) )
+    
+
+
+    if len(sggo(opjD('temp.pkl'))) == 1:
+        P['good_indicies'] = lo(opjD('temp.pkl'))
+    else:
+        so(opjD('temp.pkl'),P['good_indicies'])
+
+    P['Runs'] = Runs
+    
+
+
+
+
+
+
+
+
+def get_data_function(P):
+
+
+    Runs = P['Runs']
+
+    
+    while True:
+        
+        if True:#try:
+
+            g = len(P['good_indicies'])
+            r,ctr = P['good_indicies'][rndint(g)]
+            flip = 0
+
+            
+
+            Lists = {'input':[],'target':[]}
+
+            if not flip:
+                B = Runs[r]['original_timestamp_data']['left_image']['vals']
+            else:
+                assert(False)
+                B = Runs[r]['flip_images']['left_image_flip']['vals']
+            if ctr >= len(B):
+                continue
+
+
+
+            for k in ['input']:
+
+                if 'rgb' in P[k]:
+                    noise =0
+                    if P['noise'] > 0:
+                        noise = P['noise']*rnd(shape(B[ctr]))-P['noise']/2.
+                    Lists[k].append(B[ctr+P[k+'_offset']]+noise)
+                    if rnd() < P['drop'] and k == 'input':
+                        Lists[k][-1] *= 0
+                    if k == 'input' and 'rgb.noise' in P:
+                        if P['rgb.noise'] > 0:
+                            noise = P['rgb.noise'] * rnd(shape(C[ctr])) - P['rgb.noise']/2.
+                            noise = cv2.resize(noise,(168,94))
+                            Lists[k][-1] = Lists[k][-1]*1.0 + noise
+                    if 'drop.rgb' in P and rnd() < P['drop.rgb'] and k == 'input':
+                        Lists[k][-1] *= 0
+
+
+                if 'button' in P[k]:
+                    img = zeros((P['width'],P['height'],3))
+                    bn = int(Runs[r]['button_number'][ctr+P[k+'_offset']])
+                    if 'blue_center_button' in P:
+                        if bn == 1:
+                            bn = 1
+                        elif bn == 2:
+                            bn = 3
+                        elif bn == 3:
+                            bn = 2
+                    if 'original_Fire3_scaling' not in P:
+                        if bn in (1,2,3):
+                            img[:,:,bn-1] = 255        
+                        img[0,0,:] = 255
+                    else:
+                        if bn in (1,2,3):
+                            img[:,:,bn-1] = 1
+                    Lists[k].append(img)
+                    if rnd() < P['drop'] and k == 'input':
+                        Lists[k][-1] *= 0
+
+
+            if 'outer_contours' in P['target']:
+                Lists['target'].append(Runs[r]['rotated']['outer_countours_rotated_left'][ctr][:,0])
+                Lists['target'].append(Runs[r]['rotated']['outer_countours_rotated_right'][ctr][:,0])
+                Lists['target'].append(Runs[r]['rotated']['outer_countours_rotated_left'][ctr][:,1]/10.)
+                Lists['target'].append(Runs[r]['rotated']['outer_countours_rotated_right'][ctr][:,1]/10.)
+                Lists['target'].append(Runs[r]['rotated']['angles_left'][ctr][:]/10.)
+                Lists['target'].append(Runs[r]['rotated']['angles_right'][ctr][:]/10.)
+
+            if 'test22' in P['target']:
+                img = B[ctr+P[k+'_offset']]
+                line = img[:,:,1].mean(axis=1)
+                Lists['target'].append(line)
+
+
+            break
+        
+
+        
+
+    P['ctr'] = ctr
+
+    for k in ['input']:#sorted(Lists.keys()):
+        lst = Lists[k]
+        for l in rlen(lst):
+            e = lst[l]
+            e = e.astype(float)
+            if P['width'] and P['height']:
+                e = cv2.resize( e,(P['width'],P['height']))
+            e = e.transpose(2,1,0)
+            lst[l] = e
+
+    Concats = {}
+    for k in ['input','target']:#sorted(Lists.keys()):
+        lst = Lists[k]
+        for l in rlen(lst):
+            if k not in Concats:
+                Concats[k] = lst[l]
+            else:
+                Concats[k] = np.concatenate((Concats[k],lst[l]))
+
+    #cm(shape(Concats['target']),ra=1)
+
+    Data = {
+        'input':Concats['input'],
+        'target':Concats['target'],
+        'meta':zeros((10,41,22)),
+        'ctr':ctr,
+    }
+    #print(shape(Data['input']))
+    #print(shape(Data['target']))
+
+    return Data
+
+
+
+
+#EOF
---
+++ Learn_older/networks/__older/condecon_FS_no_A.py
@@ -0,0 +1,253 @@
+# https://github.com/pgtgrly/Convolution-Deconvolution-Network-Pytorch/blob/master/Neural_Network_Class.py
+
+from net import *
+exec(identify_file_str)
+
+class Smoke(nn.Module):
+    def __init__(
+        self,
+        inplanes,
+        squeeze_planes,
+        expand1x1_planes,
+        expand3x3_planes
+    ):
+        super(Smoke, self).__init__()
+        self.inplanes = inplanes
+        self.squeeze = nn.ConvTranspose2d(inplanes, squeeze_planes, kernel_size=1)
+        self.squeeze_activation = nn.ReLU(inplace=True)
+        self.expand1x1 = nn.ConvTranspose2d(squeeze_planes, expand1x1_planes,
+                                   kernel_size=1)
+        self.expand1x1_activation = nn.ReLU(inplace=True)
+        self.expand3x3 = nn.ConvTranspose2d(squeeze_planes, expand3x3_planes,
+                                   kernel_size=3, padding=1)
+        self.expand3x3_activation = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.squeeze_activation(self.squeeze(x))
+        return torch.cat([
+            self.expand1x1_activation(self.expand1x1(x)),
+            self.expand3x3_activation(self.expand3x3(x))
+        ], 1)
+
+aa = 8
+a = 16
+b = 32
+c = 64
+d = 128
+e = 256
+f = 512
+rgb = 3
+
+
+lateral = True
+
+class ConDecon_FS(Net):
+
+    def setup_layers(self,P):
+        self.A = {}
+        self.fire1 = Fire(P['NUM_INPUT_CHANNELS'],aa,b,b)
+        self.fire2 = Fire(c,a,c,c)
+        self.fire3 = Fire(d,b,d,d)
+        self.fire4=Fire(e,c,e,e)
+        self.smoke4=Smoke(f,c,d,d)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3,stride=2,return_indices=True,padding=0)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=3,stride=2,return_indices=True,padding=0)
+        self.maxpool3= nn.MaxPool2d(kernel_size=3,stride=2,return_indices=True,padding=0)
+
+        if lateral:
+            self.smoke3 = Smoke(e+e,b,c,c)
+            self.smoke2 = Smoke(d+d,a,b,b)
+            self.smoke1 = Smoke(c+c,aa,a,a)
+        else:
+            self.smoke3 = Smoke(e,b,c,c)
+            self.smoke2 = Smoke(d,a,b,b)
+            self.smoke1 = Smoke(c,aa,a,a)
+
+        self.maxunpool1 = nn.MaxUnpool2d(kernel_size=3,stride=2,padding=0)
+        self.maxunpool2 = nn.MaxUnpool2d(kernel_size=3,stride=2,padding=0)
+        self.maxunpool3 = nn.MaxUnpool2d(kernel_size=3,stride=2,padding=0)
+
+        self.final_deconv = nn.ConvTranspose2d(2*a, P['NUM_OUTPUTS'], kernel_size=1)
+        self.relu=nn.ReLU()
+
+        self.drop_layer = nn.Dropout(p=0.1)
+
+    def forward(self,Data):
+
+        self.optimizer.zero_grad()
+        Torch_data = self.data_to_torch(Data)
+        self.A['input'] = Torch_data['input']
+        x = self.A['input']
+        x = self.fire1(x)
+        f1 = x
+        size_fire1 = x.size()
+        x = self.drop_layer(x)
+        x,indices_fire1 = self.maxpool1(x)
+        x = self.fire2(x)
+        f2 = x
+        size_fire2 = x.size()
+        x = self.drop_layer(x)
+        x,indices_fire2=self.maxpool2(x)
+        x = self.fire3(x)
+        f3 = x
+        size_fire3 = x.size()
+        x = self.drop_layer(x)
+
+        if False: # these inner layers cause instability
+
+            x,indices_fire3=self.maxpool3(x)
+            #kprint(x.size(),"maxpool",ra=0)
+
+            x=self.fire4(x)
+            #kprint(x.size(),"fire4",ra=0)
+            size_fire4 = x.size()
+
+            x=self.smoke4(x)
+            #kprint(x.size(),"smoke4",ra=0)
+
+            x=self.maxunpool3(x,indices_fire3,size_fire3)
+
+        if lateral:
+            x = self.smoke3(torch.cat((x,f3),1))
+        else:
+            x = self.smoke3(x)
+
+        x = self.drop_layer(x)
+        x = self.maxunpool2(x,indices_fire2,size_fire2)
+
+        if lateral:
+            x = self.smoke2(torch.cat((x,f2),1))
+        else:
+            x = self.smoke2(x)
+
+        x = self.drop_layer(x)
+        x = self.maxunpool1(x,indices_fire1,size_fire1)
+
+        if lateral:
+            x = self.smoke1(torch.cat((x,f1),1))
+        else:
+            x = self.smoke1(x)
+
+        #x = self.drop_layer(x)
+        x = self.final_deconv(x)
+        x = self.relu(x)
+        #x = self.drop_layer(x)
+        #x = torch.clamp(x, 0., 15.)
+        self.A['output'] = x
+        self.A['target'] = Torch_data['target']
+
+        
+        self.loss = self.criterion(self.A['output'],self.A['target'])
+        self.losses_to_average.append(self.extract('loss'))
+        
+        if len(self.losses_to_average) >= self.num_losses_to_average:
+            self.losses.append( na(self.losses_to_average).mean() )
+            self.losses_to_average = []
+
+        return(self.A['output'])
+
+    
+
+    def forward_no_loss(self,Data):
+
+        
+        Torch_data = self.data_to_torch(Data)
+        self.A['input'] = Torch_data['input']
+        x = self.A['input']
+        x = self.fire1(x)
+        f1 = x
+        size_fire1 = x.size()
+        x = self.drop_layer(x)
+        x,indices_fire1 = self.maxpool1(x)
+        x = self.fire2(x)
+        f2 = x
+        size_fire2 = x.size()
+        x = self.drop_layer(x)
+        x,indices_fire2=self.maxpool2(x)
+        x = self.fire3(x)
+        f3 = x
+        size_fire3 = x.size()
+        x = self.drop_layer(x)
+
+        if False: # these inner layers cause instability
+
+            x,indices_fire3=self.maxpool3(x)
+            #kprint(x.size(),"maxpool",ra=0)
+
+            x=self.fire4(x)
+            #kprint(x.size(),"fire4",ra=0)
+            size_fire4 = x.size()
+
+            x=self.smoke4(x)
+            #kprint(x.size(),"smoke4",ra=0)
+
+            x=self.maxunpool3(x,indices_fire3,size_fire3)
+
+        if lateral:
+            x = self.smoke3(torch.cat((x,f3),1))
+        else:
+            x = self.smoke3(x)
+
+        x = self.drop_layer(x)
+        x = self.maxunpool2(x,indices_fire2,size_fire2)
+
+        if lateral:
+            x = self.smoke2(torch.cat((x,f2),1))
+        else:
+            x = self.smoke2(x)
+
+        x = self.drop_layer(x)
+        x = self.maxunpool1(x,indices_fire1,size_fire1)
+
+        if lateral:
+            x = self.smoke1(torch.cat((x,f1),1))
+        else:
+            x = self.smoke1(x)
+
+        #x = self.drop_layer(x)
+        x = self.final_deconv(x)
+        x = self.relu(x)
+        #x = self.drop_layer(x)
+        #x = torch.clamp(x, 0., 15.)
+        self.A['output'] = x
+        self.A['target'] = Torch_data['target']
+
+        return(self.A['output'])
+
+    
+
+
+    def setup_weights(self):
+        pass
+
+
+
+class Squeeze(nn.Module):
+    def __init__(self, inplanes, squeeze_planes,
+                 expand1x1_planes, expand3x3_planes):
+        super(Squeeze, self).__init__()
+        self.inplanes = inplanes
+        self.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
+        self.squeeze_activation = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.squeeze_activation(self.squeeze(x))
+        return x
+
+class Expand(nn.Module):
+    def __init__(self, inplanes, squeeze_planes,
+                 expand1x1_planes, expand3x3_planes):
+        super(Expand, self).__init__()
+        self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes,
+                                   kernel_size=1)
+        self.expand1x1_activation = nn.ReLU(inplace=True)
+        self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes,
+                                   kernel_size=3, padding=1)
+        self.expand3x3_activation = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        return torch.cat([
+            self.expand1x1_activation(self.expand1x1(x)),
+            self.expand3x3_activation(self.expand3x3(x))
+        ], 1)
+#EOF
