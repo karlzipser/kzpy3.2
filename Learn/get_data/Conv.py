6499c53c6976aa2b56cdce01d07a61e886071418
Karl Zipser <karlzipser@berkeley.edu>
2020-05-28 17:08:20 -0700

    gacp

--- Learn/get_data/Conv.py
+++ Learn/get_data/Conv.py
@@ -132,7 +132,7 @@ def get_data_function(P):
 
 
             if 'outer_contours' in P['target']:
-                Lists['target'].append(Runs[r]['outer_countours_rotated_left'][ctr][0,:])
+                Lists['target'].append(Runs[r]['rotated']['outer_countours_rotated_left'][ctr][0,:])
 
             if 'test22' in P['target']:
                 img = B[ctr+P[k+'_offset']]
