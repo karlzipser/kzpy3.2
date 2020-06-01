f838927efb36f5ae3e427daf407eaceca5b05029
Karl Zipser <karlzipser@berkeley.edu>
2020-05-31 21:07:25 -0700

    gacp

--- Learn/graphics/Conv.py
+++ Learn/graphics/Conv.py
@@ -226,8 +226,12 @@ def graphics_function(N,M,P):#,X):
         y = outer_countours_rotated_left[:,1]
         x = double_interp_array(x)
         x = double_interp_array(x)
+        x = double_interp_array(x)
+        x = double_interp_array(x)
+        x = double_interp_array(x)
+        x = double_interp_array(x)
         for i in rlen(outer_countours_rotated_left):
-            a = outer_countours_rotated_left[i,:]
+            a = x#outer_countours_rotated_left[i,:]
             b = fit3d.point_in_3D_to_point_in_2D(
                 a,
                 height_in_pixels = 94,
