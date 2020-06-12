c0004e5bbc87a9b66cb3cf40526a5ee174a1fbe1
Karl Zipser <karlzipser@berkeley.edu>
2020-06-12 08:41:58 -0700

    gacp

--- VT_net2__26March2020_for_no_ros/temp10.py
+++ VT_net2__26March2020_for_no_ros/temp10.py
@@ -3,7 +3,7 @@
 #,a
  
 Arguments = {
-    'run': 'tegra-ubuntu_12Oct18_11h11m30s',#'tegra-ubuntu_07Oct18_18h24m28s'#'tegra-ubuntu_17Oct18_12h11m22s'#'tegra-ubuntu_16Nov18_15h59m28s'
+    'run': 'tegra-ubuntu_07Oct18_18h24m28s'#'tegra-ubuntu_12Oct18_11h11m30s',#'tegra-ubuntu_07Oct18_18h24m28s'#'tegra-ubuntu_17Oct18_12h11m22s'#'tegra-ubuntu_16Nov18_15h59m28s'
     # 'tegra-ubuntu_12Nov18_20h56m16s',#'tegra-ubuntu_31Oct18_16h06m32s'#  ,
 }
 
@@ -37,6 +37,15 @@ def double_interp_2D_array(a):
 
 import kzpy3.Array.fit3d as fit3d
 
+def get_offshoot(A,B,r,theta):
+  m,b = curve_fit(f___,(A[0],B[0]),(A[1],B[1]))[0]
+  alpha = corrected_angle(m,B,A)
+  C = B + na([0,r])
+  D = rotatePoint(B,C,90-theta+alpha+0)
+  return D
+
+
+
 Colors = {'direct':'b','left':'r','right':'g'}
 
 if 'o' not in locals():
@@ -60,7 +69,7 @@ if 'O' not in o:
   o['xy'] = [na([0,0])]
 
   L = o['L']
-  for i in range(10000): #  rlen(L['motor']):
+  for i in rlen(L['motor']):
       a = vec(
           L['gyro_heading_x_meo'][i],
           L['encoder_meo'][i],
@@ -76,7 +85,7 @@ figure(1);clf();plt_square()
 XYR = {'left':None,'right':None}
 clf()
 plt_square()
-for i in range(2000,10000,1):
+for i in range(2800,5000):#rlen(L['motor']):
   if not L['drive_mode'][i]:
     cr(i,'d')
     continue
@@ -98,32 +107,48 @@ for i in range(2000,10000,1):
 
     
 
-    xy_r = rotatePolygon(xy - o['xy'][i],L['gyro_heading_x_meo'][i]) + o['xy'][i]
-    xy_r = xy_r[23:25,:]
+    xy_r_ = rotatePolygon(xy - o['xy'][i],L['gyro_heading_x_meo'][i]) + o['xy'][i]
+    xy_r = xy_r_[24:25,:]
 
     if XYR[s] is None:
       XYR[s] = xy_r
     else:
-      pass#XYR[s] = np.concatenate((XYR[s],xy_r))
+      XYR[s] = np.concatenate((XYR[s],xy_r))
 
     if True:#not i % 50:
-      pts_plot(xy_r,color=Colors[s],sym=',')
-      #pts_plot(XYR[s],color=Colors[s],sym=',')
+      pts_plot(xy_r,color=Colors[s],sym='.')
+      if False:
+        for s in ['left','right']:
+          pts_plot(XYR[s],color=Colors[s],sym=',')
 
     #plot(xy[:,0],xy[:,1],ms=3)
   ##plot(na([-10,10])+x,na([0,0])+y,'k:')
   ##plot(na([0,0])+x,na([-10,10])+y,'k:')
 
 
-  for s in ['left','right']:
-    for j in rlen(b['outer_countours_rotated_'+s]):
+
+  #for s in ['left','right']:
+    for j in [24]:#rlen(b['outer_countours_rotated_'+s]):
       a = b['angles_'+s][j]
       a = min(np.abs(a),400)
       marker_size = int(a/2.)
-      ##pts_plot(b['outer_countours_rotated_'+s][j] + o['xy'][i],Colors[s],sym='.',ms=marker_size)
+      #pts_plot(xy_r_[24],Colors[s],sym='.',ms=marker_size)
+      if True:#j > 0:
+        if s == 'left':
+          aa = 180-a
+        else:
+          aa = a-180
 
+        D = get_offshoot( 
+          xy_r_[24,:],
+          xy_r_[25,:],
+          np.abs(a)/10.,
+          aa,
+          )
+        pts_plot([xy_r[0],D],Colors[s],sym=':')
+        pts_plot(D,Colors[s],sym='x')
 
-
+    #pts_plot(xy_r,color='k',sym='.')
 
   if False:
 
@@ -161,7 +186,7 @@ for i in range(2000,10000,1):
             clp('Exception, shape(c) =',shape(c),'`wrb')
 
 
-  if not i % 50:
+  if False:#not i % 30*60:
     spause()
 
   #time.sleep(0.00005)
--- VT_net2__26March2020_for_no_ros/temp9.py
+++ VT_net2__26March2020_for_no_ros/temp9.py
@@ -1,9 +1,8 @@
 
-
 #,a
  
 Arguments = {
-    'run': 'tegra-ubuntu_12Oct18_11h11m30s',#'tegra-ubuntu_07Oct18_18h24m28s'#'tegra-ubuntu_17Oct18_12h11m22s'#'tegra-ubuntu_16Nov18_15h59m28s'
+    'run': 'tegra-ubuntu_07Oct18_18h24m28s'#'tegra-ubuntu_12Oct18_11h11m30s',#'tegra-ubuntu_17Oct18_12h11m22s'#'tegra-ubuntu_16Nov18_15h59m28s'
     # 'tegra-ubuntu_12Nov18_20h56m16s',#'tegra-ubuntu_31Oct18_16h06m32s'#  ,
 }
 
@@ -15,6 +14,27 @@ def vec(heading,encoder,motor,sample_frequency=30.,vel_encoding_coeficient=1.0/2
     a = array(rotatePoint([0,0],a,heading))
     a *= velocity/sample_frequency
     return array(a)
+
+def double_interp_1D_array(a):
+    b = []
+    for i in range(len(a)-1):
+        c = a[i]
+        d = a[i+1]
+        e = (c+d)/2.0
+        b.append(c)
+        b.append(e)
+    b.append(a[-1])
+    return na(b)
+
+def double_interp_2D_array(a):
+    b = double_interp_1D_array(a[:,0])
+    c = double_interp_1D_array(a[:,1])
+    d = zeros((len(b),2))
+    d[:,0] = b
+    d[:,1] = c
+    return d
+
+import kzpy3.Array.fit3d as fit3d
 
 Colors = {'direct':'b','left':'r','right':'g'}
 
@@ -39,7 +59,7 @@ if 'O' not in o:
   o['xy'] = [na([0,0])]
 
   L = o['L']
-  for i in range(10000): #  rlen(L['motor']):
+  for i in rlen(L['motor']):
       a = vec(
           L['gyro_heading_x_meo'][i],
           L['encoder_meo'][i],
@@ -50,10 +70,21 @@ if 'O' not in o:
   o['S'] = lo(opjD('Data/outer_contours/output_2_data',run_type,Arguments['run']))
 
 
+
+def get_offshoot(A,B,r,theta):
+  m,b = curve_fit(f___,(A[0],B[0]),(A[1],B[1]))[0]
+  alpha = corrected_angle(m,B,A)
+  C = B + na([0,r])
+  D = rotatePoint(B,C,90-theta+alpha+0)
+  return D
+
+
 figure(1)
 clf()
 plt_square()
-for i in range(7500,10000,1):
+for i in range(3200,5000,5):#rlen(L['motor']):
+  figure(1)
+  clf()
   if not L['drive_mode'][i]:
     cr(i,'d')
     continue
@@ -68,25 +99,71 @@ for i in range(7500,10000,1):
     x,y  = o['xy'][i][0],o['xy'][i][1]
   clf();plt_square();xylim(x-20,x+20,y-20,y+20)
   b = o['S'][i]
+  
   for s in ['left','right']:
     xy = b['outer_countours_rotated_'+s] + o['xy'][i]
     pts_plot(xy,color=Colors[s],sym='-')
     #plot(xy[:,0],xy[:,1],ms=3)
   plot(na([-10,10])+x,na([0,0])+y,'k:')
   plot(na([0,0])+x,na([-10,10])+y,'k:')
-
+  plot(x,y,'ko')
 
   for s in ['left','right']:
     for j in rlen(b['outer_countours_rotated_'+s]):
       a = b['angles_'+s][j]
       a = min(np.abs(a),400)
       marker_size = int(a/2.)
-      pts_plot(b['outer_countours_rotated_'+s][j] + o['xy'][i],Colors[s],sym='.',ms=marker_size)
+      #pts_plot(b['outer_countours_rotated_'+s][j] + o['xy'][i],Colors[s],sym='.',ms=marker_size)
+      if j > 0:
+        if s == 'left':
+          aa = 180-a
+        else:
+          aa = a-180
+        D = get_offshoot( 
+          b['outer_countours_rotated_'+s][j-1] + o['xy'][i],
+          b['outer_countours_rotated_'+s][j] + o['xy'][i],
+          np.abs(a)/10.,
+          aa,
+          )
+
+        pts_plot([b['outer_countours_rotated_'+s][j] + o['xy'][i],D],Colors[s],sym='-')
+        pts_plot(D,Colors[s])
+        
+  
+  #mci(o['O']['left_image']['vals'][i],title='left_image',scale=3.0)
+
+  figure(9)
+  mi(o['O']['left_image']['vals'][i],9)
 
 
+  for s in ['left','right']:
+
+      c = []
+      q = b['outer_countours_rotated_'+s]
+      w = double_interp_2D_array(q)#[:33,:])
+      w = double_interp_2D_array(w)
+      w = double_interp_2D_array(w)
+      q = w
+      #q = np.concatenate((w,q[33:,:]))
+
+      for i in rlen(q):
+          a = q[i,:]
+          bb = fit3d.point_in_3D_to_point_in_2D(
+              a,
+              height_in_pixels = 94,
+              width_in_pixels = 168,
+              backup_parameter=1,
+          )
+          if False not in bb:
+              c.append(bb)
+      c = na(c)
+      try:
+          pts_plot(c,color=Colors[s],sym='.')
+      except:
+          clp('Exception, shape(c) =',shape(c),'`wrb')
   spause()
-  mci(o['O']['left_image']['vals'][i],title='left_image',scale=3.0)
-  time.sleep(0.05)
+  time.sleep(0.00005)
+  #cm(a,ra=1)
 
 #,b
 
